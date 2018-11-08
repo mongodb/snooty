1d2a3af240f3619146c15501029bbb8e840b5779
Daniel Borowski <danielborowski@Daniels-MacBook-Pro-5.local>
2018-11-08 11:40:58 -0500

    homepage copy change and add OS tabs

--- front-end/src/templates/index.js
+++ front-end/src/templates/index.js
@@ -1,42 +1,30 @@
 import React, { Component} from 'react';
 import Card from '../components/Card';
 
-export default class Main extends Component {
+export default class Index extends Component {
 
   constructor(propsFromServer) {
     super(propsFromServer);
     this.state = {
       name: 'Guides',
       description: 'Getting Started',
-      guides: this.props.pageContext.__refDocMapping.index.ast.children[1].children[2].children,
-      languagesFilter: [],
-      operatingSystemsFilter: [],
-      platformsFilter: []
+      guides: this.props.pageContext.__refDocMapping.index.ast.children[1].children[2].children
     };
+    console.log(11, this.props.pageContext.__refDocMapping);
   }
 
   render() {
+    console.log(22, this.state.guides);
     const allCards = this.state.guides.map((card, index) => {
       return (
         <Card 
           card={ card } 
           key={ index }
           cardId={ index } 
+          refDocMapping={ this.props.pageContext.__refDocMapping }
         />
       ) 
     });
-    const filterBy = (key) => {
-      const keyName = key + 'Filter';
-      const allValues = this.state[keyName].map((filter, index) => {
-        return <option value={ filter } key={ index }>{ filter }</option> 
-      });
-      return (
-        <select style={{marginLeft:"3px"}}>
-          <option value="all" defaultValue>All { key }</option> 
-          { allValues }
-        </select>
-      )
-    }
     return ( 
       <div className="content">
         <div className="guide-category-list">
@@ -46,12 +34,6 @@ export default class Main extends Component {
                 { this.state.name }
                 <a className="headerlink" href="#guides" title="Permalink to this headline">¶</a>
               </h1>
-              <aside style={{width: "1010px", borderRadius: "5px", background: "#24282b", padding: "1px 18px", marginBottom: "15px", overflow: "auto"}} >
-                <h3 style={{color: "white", float: "left", paddingTop: "6px", marginRight: "12px"}}>Filter By: </h3>
-                <p style={{float: "left", marginRight: "12px"}}> { filterBy('languages') } </p>
-                <p style={{float: "left", marginRight: "12px"}}> { filterBy('operatingSystems') } </p>
-                <p style={{float: "left", marginRight: "12px"}}> { filterBy('platforms') } </p>
-              </aside>
               <section className="guide-category">
                 <div className="guide-category__title guide-category__title--getting-started">
                   { this.state.description }
