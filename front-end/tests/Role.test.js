8e58a66587c96808147d8f68496e52492149193c
Daniel Borowski <danielborowski@Daniels-MacBook-Pro-5.local>
2019-02-25 11:46:51 -0500

    lintfix

--- front-end/tests/Role.test.js
+++ front-end/tests/Role.test.js
@@ -3,15 +3,15 @@ import { shallow } from 'enzyme';
 import Role from '../src/components/Role';
 
 // data for this component
-import mockData_doc from './data/Role-doc.test.json';
-import mockData_ref from './data/Role-ref.test.json';
+import mockDataDoc from './data/Role-doc.test.json';
+import mockDataRef from './data/Role-ref.test.json';
 
 it('renders correctly role "doc"', () => {
-  const tree = shallow(<Role nodeData={ mockData_doc } />);
+  const tree = shallow(<Role nodeData={ mockDataDoc } />);
   expect(tree).toMatchSnapshot();
 });
 
 it('renders correctly role "ref"', () => {
-  const tree = shallow(<Role nodeData={ mockData_ref } />);
+  const tree = shallow(<Role nodeData={ mockDataRef } />);
   expect(tree).toMatchSnapshot();
 });
