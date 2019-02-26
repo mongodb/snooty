d581ff992bba9015668e366a6f9c80753fd6ded0
Daniel <dborowski27@gmail.com>
2019-02-26 16:21:01 -0500

    Create Test Data for React/Gatsby tests (#17)
    * test data
    
    * mock data for components
    
    * index page test
    
    * package lock
    
    * fix package lock
    
    * update to enzyme
    
    * update path in readme
    
    * remove path
    
    * testing all components
    
    * lintfix
    
    * fix required errors
    
    * test files
    
    * fix default prop types
    
    * update step snapshot
    
    * fix issues

--- front-end/tests/Paragraph.test.js
+++ front-end/tests/Paragraph.test.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Paragraph from '../src/components/Paragraph';
 
+// data for this component
+import mockData from './data/Paragraph.test.json';
+
 it('renders correctly', () => {
-  const tree = shallow(<Paragraph nodeData={{ children: [{ type: 'text', value: 'hello world' }] }} />);
+  const tree = shallow(<Paragraph nodeData={mockData} />);
   expect(tree).toMatchSnapshot();
 });
---
+++ front-end/tests/Section.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Section from '../src/components/Section';
+
+// data for this component
+import mockData from './data/Section.test.json';
+
+it('renders correctly', () => {
+  const tree = shallow(<Section nodeData={mockData} />);
+  expect(tree).toMatchSnapshot();
+});
