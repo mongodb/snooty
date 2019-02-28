554b61cf2f8bd2b2f90eeed9d656766e40cd3881
sophstad <srs2231@columbia.edu>
2019-02-28 14:53:46 -0500

    Add tests and handling for align

--- front-end/src/components/ListTable.js
+++ front-end/src/components/ListTable.js
@@ -2,18 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ListTable = ({ nodeData: { children, options } }) => {
-  const headerRowCount = parseInt(options['header-rows'], 10);
-  const stubColumnCount = parseInt(options['stub-columns'], 10);
+  const headerRowCount = parseInt(options['header-rows'], 10) || 0;
+  const stubColumnCount = parseInt(options['stub-columns'], 10) || 0;
   const headerRows = children[0].children[0].children.slice(0, headerRowCount);
   const bodyRows = children[0].children.slice(headerRowCount);
   const customWidth = options.width ? options.width : 'auto';
-  let widths = '';
-  if (options.widths) {
-    widths = options.widths === 'auto' ? 'colwidths-auto' : 'colwidths-given';
+  const customAlign = options.align ? `align-${options.align}` : '';
+
+  let widths = 'colwidths-auto';
+  if (options.widths && options.widths !== 'auto') {
+    widths = 'colwidths-given';
   }
 
   return (
-    <table className={`docutils ${options.class} ${widths}`} style={{ width: customWidth }}>
+    <table className={['docutils', options.class, widths, customAlign].join(' ')} style={{ width: customWidth }}>
       {widths === 'colwidths-given' && <Colgroup widths={options.widths.split(/[ ,]+/)} />}
       <ListTableHeader rows={headerRows} stubColumnCount={stubColumnCount} />
       <ListTableBody rows={bodyRows} headerRowCount={headerRowCount} stubColumnCount={stubColumnCount} />
@@ -23,8 +25,8 @@ const ListTable = ({ nodeData: { children, options } }) => {
 
 const Colgroup = ({ widths }) => (
   <colgroup>
-    {widths.map(width => (
-      <col width={`${width}%`} />
+    {widths.map((width, index) => (
+      <col width={`${width}%`} key={index} />
     ))}
   </colgroup>
 );
@@ -36,7 +38,7 @@ Colgroup.propTypes = {
 const ListTableHeader = ({ rows, stubColumnCount }) => (
   <thead valign="bottom">
     {rows.map((row, index) => (
-      <ListTableHeaderRow row={row.children} rowIndex={index} stubColumnCount={stubColumnCount} />
+      <ListTableHeaderRow row={row.children} rowIndex={index} stubColumnCount={stubColumnCount} key={index} />
     ))}
   </thead>
 );
@@ -53,7 +55,9 @@ ListTableHeader.propTypes = {
 const ListTableHeaderRow = ({ row, rowIndex, stubColumnCount }) => (
   <tr className={rowIndex % 2 === 0 ? 'row-odd' : 'row-even'}>
     {row.map((column, colIndex) => (
-      <th className={`head ${colIndex <= stubColumnCount - 1 && 'stub'}`}>{column.children[0].children[0].value}</th>
+      <th className={`head ${colIndex <= stubColumnCount - 1 && 'stub'}`} key={colIndex}>
+        {column.children[0].children[0].value}
+      </th>
     ))}
   </tr>
 );
@@ -80,6 +84,7 @@ const ListTableBody = ({ rows, headerRowCount, stubColumnCount }) => (
   <tbody valign="top">
     {rows.map((row, index) => (
       <ListTableBodyRow
+        key={index}
         row={row.children[0].children}
         rowIndex={index + headerRowCount}
         stubColumnCount={stubColumnCount}
@@ -101,7 +106,9 @@ ListTableBody.propTypes = {
 const ListTableBodyRow = ({ row, rowIndex, stubColumnCount }) => (
   <tr className={rowIndex % 2 === 0 ? 'row-odd' : 'row-even'}>
     {row.map((column, colIndex) => (
-      <td className={`${colIndex <= stubColumnCount - 1 && 'stub'}`}>{column.children[0].children[0].value}</td>
+      <td className={`${colIndex <= stubColumnCount - 1 && 'stub'}`} key={colIndex}>
+        {column.children[0].children[0].value}
+      </td>
     ))}
   </tr>
 );
