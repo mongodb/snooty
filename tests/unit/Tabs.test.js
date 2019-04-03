9cf0ae957d218d87b58ee10ed7827040118335ce
Daniel Borowski <danielborowski@daniels-mbp-5.lan>
2019-04-03 11:00:11 -0400

    test fixes

--- tests/unit/Tabs.test.js
+++ tests/unit/Tabs.test.js
@@ -6,24 +6,30 @@ import Tabs from '../../src/components/Tabs';
 // data for this component
 import mockData from './data/Tabs.test.json';
 
-const mountTabs = ({ activeTabs, mockCallback }) =>
-  mount(<Tabs nodeData={mockData} addTabset={mockCallback} setActiveTab={mockCallback} activeTabs={activeTabs} />);
+const mountTabs = ({ mockSetActiveTab, activeTabs }) =>
+  mount(<Tabs nodeData={mockData} addTabset={mockSetActiveTab} setActiveTab={mockSetActiveTab} activeTabs={activeTabs} />);
 
 describe('Tabs testing', () => {
   describe('Tabs selection', () => {
     let wrapper;
-    const mockCallback = jest.fn();
+    const mockSetActiveTab = jest.fn();
 
     beforeAll(() => {
-      wrapper = mountTabs({ mockCallback, activeTabs: { platforms: PLATFORMS[0] } });
+      wrapper = mountTabs({ mockSetActiveTab, activeTabs: { platforms: PLATFORMS[0] } });
     });
 
-    it('tabs container exists', () => {
+    it('tabs container exists with correct number of children', () => {
+      const tabCount = wrapper.props().nodeData.children.length;
       expect(wrapper.find('.tab-strip__element').exists()).toEqual(true);
+      expect(wrapper.find('.tab-strip__element')).toHaveLength(tabCount);
     });
 
-    it('active tab is set', () => {
+    it('active tab is set in DOM', () => {
       expect(wrapper.find('.tab-strip__element[aria-selected="true"]').exists()).toEqual(true);
+    });
+
+    it('active tab is correct value', () => {
+      expect(wrapper.props().activeTabs.platforms).toEqual(PLATFORMS[0]);
     });
 
     it('exists non-active tab', () => {
@@ -31,10 +37,10 @@ describe('Tabs testing', () => {
     });
 
     it('clicking new non-active tab calls function', () => {
-      mockCallback.mockClear();
+      mockSetActiveTab.mockClear();
       const nonactiveTab = wrapper.find('.tab-strip__element[aria-selected="false"]').first();
       nonactiveTab.simulate('click');
-      expect(mockCallback.mock.calls.length).toBe(1);
+      expect(mockSetActiveTab.mock.calls.length).toBe(1);
     });
   });
 });
