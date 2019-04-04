4b41cee2397dea6bcb22577203300bd6d15dc0c0
Sophie Stadler <sophstad@users.noreply.github.com>
2019-04-04 15:33:25 -0400

    DOCSP-5086: Fix Build (#33)
    remove modal code and fix localStorage

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,25 +28,29 @@ let stitchClient;
 
 const setupStitch = () => {
   return new Promise((resolve, reject) => {
-    stitchClient = Stitch.hasAppClient(STITCH_ID) ? Stitch.getAppClient(STITCH_ID) : Stitch.initializeAppClient(STITCH_ID);
-    stitchClient.auth.loginWithCredential(new AnonymousCredential()).then((user) => {
-      console.log('logged into stitch');
-      resolve();
-    });
+    stitchClient = Stitch.hasAppClient(STITCH_ID)
+      ? Stitch.getAppClient(STITCH_ID)
+      : Stitch.initializeAppClient(STITCH_ID);
+    stitchClient.auth
+      .loginWithCredential(new AnonymousCredential())
+      .then(user => {
+        console.log('logged into stitch');
+        resolve();
+      })
+      .catch(console.error);
   });
 };
 
 const saveAssetFile = async (name, objData) => {
   return new Promise((resolve, reject) => {
-    fs.writeFile(`static/${name}`, objData.data.buffer, 'binary', (err) => {
+    fs.writeFile(`static/${name}`, objData.data.buffer, 'binary', err => {
       if (err) console.log('ERROR with saving asset', err);
       resolve();
     });
   });
-}
+};
 
 exports.sourceNodes = async ({ actions }) => {
-
   const { createNode } = actions;
   const items = [];
 
@@ -54,29 +58,30 @@ exports.sourceNodes = async ({ actions }) => {
 
   // if running with test data
   if (USE_TEST_DATA) {
-
     // get data from test file
     try {
       const fullpath = path.join(TEST_DATA_PATH, USE_TEST_DATA);
       const fileContent = fs.readFileSync(fullpath, 'utf8');
-      RESOLVED_REF_DOC_MAPPING = JSON.parse(fileContent); 
+      RESOLVED_REF_DOC_MAPPING = JSON.parse(fileContent);
       console.log(`*** Using test data from "${fullpath}"`);
     } catch (e) {
-      throw Error (`ERROR with test data file: ${e}`);
+      throw Error(`ERROR with test data file: ${e}`);
     }
-
   } else {
-
     // start from index document
-    const query = { _id: `${ PREFIX.join('/') }/index` };
+    const query = { _id: `${PREFIX.join('/')}/index` };
     const documents = await stitchClient.callFunction('fetchDocuments', [NAMESPACE, query]);
 
     // set data for index page
-    RESOLVED_REF_DOC_MAPPING['index'] = (documents && documents.length > 0) ? documents[0] : {};
+    RESOLVED_REF_DOC_MAPPING['index'] = documents && documents.length > 0 ? documents[0] : {};
 
     // resolve references/urls to documents
-    RESOLVED_REF_DOC_MAPPING = await stitchClient.callFunction('resolveReferences', [PREFIX, NAMESPACE, documents, RESOLVED_REF_DOC_MAPPING]);
-
+    RESOLVED_REF_DOC_MAPPING = await stitchClient.callFunction('resolveReferences', [
+      PREFIX,
+      NAMESPACE,
+      documents,
+      RESOLVED_REF_DOC_MAPPING,
+    ]);
   }
 
   // separate references into correct types, e.g. pages, include files, assets, etc.
@@ -94,7 +99,7 @@ exports.sourceNodes = async ({ actions }) => {
 
   // get code examples for all github urls
   for (const url of GITHUB_CODE_EXAMPLES) {
-    const githubRawUrl = url.replace('https://github.com', 'https://raw.githubusercontent.com').replace('blob/', '')
+    const githubRawUrl = url.replace('https://github.com', 'https://raw.githubusercontent.com').replace('blob/', '');
     const codeFile = await stitchClient.callFunction('fetchReferenceUrlContent', [githubRawUrl]);
     RESOLVED_REF_DOC_MAPPING[url] = codeFile;
   }
@@ -117,36 +122,33 @@ exports.sourceNodes = async ({ actions }) => {
 
   // whenever we get latest data, always save latest version
   if (!USE_TEST_DATA) {
-    const fullpathLatest = path.join(TEST_DATA_PATH, ); `${TEST_DATA_PATH}/${LATEST_TEST_DATA_FILE}`;
-    fs.writeFile(fullpathLatest, JSON.stringify(RESOLVED_REF_DOC_MAPPING), 'utf8', (err) => {
+    const fullpathLatest = path.join(TEST_DATA_PATH);
+    `${TEST_DATA_PATH}/${LATEST_TEST_DATA_FILE}`;
+    fs.writeFile(fullpathLatest, JSON.stringify(RESOLVED_REF_DOC_MAPPING), 'utf8', err => {
       if (err) console.log(`ERROR saving test data into "${fullpathLatest}" file`, err);
       console.log(`** Saved test data into "${fullpathLatest}"`);
     });
   }
-
 };
 
 exports.createPages = ({ graphql, actions }) => {
-
   const { createPage } = actions;
 
   return new Promise((resolve, reject) => {
     for (const page of PAGES) {
-      const template = (page === 'index') ? 'index' : 'guide';
-      const pageUrl = (page === 'index') ? '/' : page;
+      const template = page === 'index' ? 'index' : 'guide';
+      const pageUrl = page === 'index' ? '/' : page;
       if (RESOLVED_REF_DOC_MAPPING[page] && Object.keys(RESOLVED_REF_DOC_MAPPING[page]).length > 0) {
         createPage({
           path: pageUrl,
-          component: path.resolve(`./src/templates/${ template }.js`),
+          component: path.resolve(`./src/templates/${template}.js`),
           context: {
             __refDocMapping: RESOLVED_REF_DOC_MAPPING,
-            __stitchID: STITCH_ID
-          }
+            __stitchID: STITCH_ID,
+          },
         });
       }
     }
     resolve();
   });
-
 };
-
