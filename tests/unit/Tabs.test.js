24112e3716387c57596a8ef343dd18317131c11b
Daniel Borowski <danielborowski@daniels-mbp-5.lan>
2019-04-04 15:36:58 -0400

    fix func in tabs

--- tests/unit/Tabs.test.js
+++ tests/unit/Tabs.test.js
@@ -6,22 +6,27 @@ import Tabs from '../../src/components/Tabs';
 // data for this component
 import mockData from './data/Tabs.test.json';
 
-const mountTabs = ({ mockSetActiveTab, activeTabs }) =>
-  mount(<Tabs nodeData={mockData} addTabset={mockSetActiveTab} setActiveTab={mockSetActiveTab} activeTabs={activeTabs} />);
+const mountTabs = ({ mockSetActiveTab, mockAddTabset, activeTabs }) =>
+  mount(<Tabs nodeData={mockData} setActiveTab={mockSetActiveTab} addTabset={mockAddTabset} activeTabs={activeTabs} />);
 
 describe('Tabs testing', () => {
   describe('Tabs selection', () => {
     let wrapper;
     const mockSetActiveTab = jest.fn();
+    const mockAddTabset = jest.fn();
 
     beforeAll(() => {
-      wrapper = mountTabs({ mockSetActiveTab, activeTabs: { platforms: PLATFORMS[0] } });
+      wrapper = mountTabs({ mockSetActiveTab, mockAddTabset, activeTabs: { platforms: PLATFORMS[0] } });
     });
 
     it('tabs container exists with correct number of children', () => {
       const tabCount = wrapper.props().nodeData.children.length;
       expect(wrapper.find('.tab-strip__element').exists()).toEqual(true);
       expect(wrapper.find('.tab-strip__element')).toHaveLength(tabCount);
+    });
+
+    it('added tabset on component load', () => {
+      expect(mockAddTabset.mock.calls.length).toBe(1);
     });
 
     it('active tab is set in DOM', () => {
@@ -37,7 +42,6 @@ describe('Tabs testing', () => {
     });
 
     it('clicking new non-active tab calls function', () => {
-      mockSetActiveTab.mockClear();
       const nonactiveTab = wrapper.find('.tab-strip__element[aria-selected="false"]').first();
       nonactiveTab.simulate('click');
       expect(mockSetActiveTab.mock.calls.length).toBe(1);
