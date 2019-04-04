b22962f0ed87bcf1ac6a77a76913b895a9e058ee
Sophie Stadler <sophstad@users.noreply.github.com>
2019-04-04 15:33:25 -0400

    DOCSP-5086: Fix Build (#33)
    remove modal code and fix localStorage

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,12 +1,18 @@
+const isBrowser = () => typeof window !== 'undefined';
+
 export const setLocalValue = (key, value) => {
-  const prevState = JSON.parse(localStorage.getItem('mongodb-docs'));
-  localStorage.setItem('mongodb-docs', JSON.stringify({ ...prevState, [key]: value }));
+  if (isBrowser()) {
+    const prevState = JSON.parse(window.localStorage.getItem('mongodb-docs'));
+    localStorage.setItem('mongodb-docs', JSON.stringify({ ...prevState, [key]: value }));
+  }
 };
 
 export const getLocalValue = key => {
-  const docsObj = JSON.parse(localStorage.getItem('mongodb-docs'));
-  if (docsObj) {
-    return docsObj[key];
+  if (isBrowser() && JSON.parse(window.localStorage.getItem('mongodb-docs'))) {
+    const docsObj = JSON.parse(window.localStorage.getItem('mongodb-docs'));
+    if (docsObj) {
+      return docsObj[key];
+    }
   }
   return undefined;
 };
--- src/templates/guide.js
+++ src/templates/guide.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Stitch, AnonymousCredential } from 'mongodb-stitch-browser-sdk';
 import TOC from '../components/TOC';
 import GuideSection from '../components/GuideSection';
 import GuideHeading from '../components/GuideHeading';
-import Modal from '../components/Modal';
 import { LANGUAGES, DEPLOYMENTS, REF_TARGETS } from '../constants';
 import { getLocalValue, setLocalValue } from '../localStorage';
 
@@ -22,37 +20,11 @@ export default class Guide extends Component {
       pageContext.__refDocMapping[
         this.props['*'] // eslint-disable-line react/destructuring-assignment
       ].ast.children[0].children;
-    this.stitchId = pageContext.__stitchID;
-    this.stitchClient = undefined;
-    this.DOMParser = undefined;
     this.validNames = ['prerequisites', 'check_your_environment', 'procedure', 'summary', 'whats_next', 'seealso'];
     this.admonitions = ['admonition', 'note', 'tip', 'important', 'warning'];
     this.state = {
       activeTabs: {},
-      modalPositionLeft: 0,
-      modalPositionTop: 0,
-      modalVisible: false,
-      modalContent: {
-        text: null,
-        example: null,
-      },
     };
-  }
-
-  componentDidMount() {
-    this.DOMParser = new DOMParser();
-    this.setupStitch();
-  }
-
-  setupStitch() {
-    const appName = this.stitchId;
-    if (!appName) return;
-    this.stitchClient = Stitch.hasAppClient(appName)
-      ? Stitch.defaultAppClient
-      : Stitch.initializeDefaultAppClient(appName);
-    this.stitchClient.auth.loginWithCredential(new AnonymousCredential()).then(user => {
-      console.log(`logged in as user ${user.id}`);
-    });
   }
 
   addTabset = (tabsetName, tabData) => {
@@ -83,87 +55,6 @@ export default class Guide extends Component {
     setLocalValue(tabsetName, value);
   };
 
-  // when a user hovers over a specific role
-  // first fetch the data and then show the modal with the content
-  modalFetchData = (event, href) => {
-    event.persist();
-    const findHashPart = href.substr(href.indexOf('#') + 1);
-    let contentObj;
-    this.stitchClient.callFunction('fetchReferenceUrlContent', [href]).then(response => {
-      if (!response) {
-        contentObj = {
-          text: 'Error fetching data...',
-        };
-      } else {
-        const parsed = this.DOMParser.parseFromString(response, 'text/html');
-        const mainContainer = parsed.getElementById(findHashPart)
-          ? parsed.getElementById(findHashPart).nextElementSibling
-          : null;
-        contentObj = {
-          text: mainContainer.getElementsByTagName('p')[0]
-            ? mainContainer.getElementsByTagName('p')[0].textContent.trim()
-            : 'FIX: no content found',
-          example: 'no code example',
-        };
-        // if syntax example is within first container
-        if (mainContainer.getElementsByClassName('button-code-block')[0]) {
-          contentObj.example = mainContainer.getElementsByClassName('copyable-code-block')[0].textContent.trim();
-        }
-      }
-      this.setState({
-        modalContent: contentObj,
-      });
-    });
-    this.modalShow(event);
-    const refElement = event.target;
-    this.modalBeginHidingInterval(refElement);
-  };
-
-  modalShow(event) {
-    const newX = event.target.offsetLeft + Math.floor(event.target.offsetWidth / 2);
-    let newY;
-    if (event.screenY < 400) {
-      newY = event.target.offsetTop + event.target.offsetHeight;
-    } else {
-      newY = event.target.offsetTop - 300; // height of modal
-    }
-    this.setState({
-      modalVisible: true,
-      modalPositionLeft: newX,
-      modalPositionTop: newY,
-    });
-  }
-
-  modalHide() {
-    this.setState({
-      modalVisible: false,
-    });
-  }
-
-  // TODO: use css instead??
-  // https://codepen.io/anon/pen/YJEaZo
-  modalBeginHidingInterval(refElement) {
-    let event;
-    const modalContainer = document.getElementsByClassName('__ref-modal')[0];
-    const saveEvent = e => {
-      event = e || window.event;
-    };
-    // watch mouse movements globally
-    document.addEventListener('mousemove', saveEvent);
-    // if not hovered over modal (or any element within the modal) remove it and end event listener
-    const interval = setInterval(() => {
-      if (
-        event.target.nodeName !== 'A' &&
-        !modalContainer.contains(event.target) &&
-        !refElement.contains(event.target)
-      ) {
-        document.removeEventListener('mousemove', saveEvent);
-        clearInterval(interval);
-        this.modalHide();
-      }
-    }, 1000);
-  }
-
   createSections() {
     const { pageContext } = this.props;
     const { activeTabs } = this.state;
@@ -175,10 +66,8 @@ export default class Guide extends Component {
           key={index}
           admonitions={this.admonitions}
           refDocMapping={pageContext ? pageContext.__refDocMapping : {}}
-          modal={this.modalFetchData}
           setActiveTab={this.setActiveTab}
           addTabset={this.addTabset}
-          stitchClient={this.stitchClient}
           activeTabs={activeTabs}
         />
       ));
@@ -186,7 +75,7 @@ export default class Guide extends Component {
 
   render() {
     const { pageContext } = this.props;
-    const { activeTabs, cloud, drivers, modalContent, modalPositionLeft, modalPositionTop, modalVisible } = this.state;
+    const { activeTabs, cloud, drivers } = this.state;
 
     return (
       <div className="content">
@@ -207,17 +96,7 @@ export default class Guide extends Component {
               addTabset={this.addTabset}
               admonitions={this.admonitions}
               refDocMapping={pageContext ? pageContext.__refDocMapping : {}}
-              modal={this.modalFetchData}
-              stitchClient={this.stitchClient}
               activeTabs={activeTabs}
-            />
-            <Modal
-              modalProperties={{
-                modalContent,
-                modalPositionLeft,
-                modalPositionTop,
-                modalVisible,
-              }}
             />
             {this.createSections()}
             <div className="footer">
@@ -239,6 +118,5 @@ Guide.propTypes = {
   '*': PropTypes.string.isRequired,
   pageContext: PropTypes.shape({
     __refDocMapping: PropTypes.objectOf(PropTypes.object).isRequired,
-    __stitchID: PropTypes.string.isRequired,
   }).isRequired,
 };
