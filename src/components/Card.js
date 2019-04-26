88c922bba9545135e366cbfe8b6ad976c775c1cd
sophstad <sophie.stadler@mongodb.com>
2019-04-26 17:31:38 -0400

    Improve pills

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,104 +4,109 @@ import { findKeyValuePair } from '../util';
 import { stringifyTab } from '../constants';
 import { setLocalValue } from '../localStorage';
 
-const CardPills = ({ isTruncated, pills, pillsetName }) => (
-  <ul className="guide__pills">
-    {pills.map((pill, index) => (
-      <li className="guide__pill" key={index}>
-        <span
-          onClick={() => {
-            setLocalValue(pillsetName, pill);
-          }}
-          role="button"
-          tabIndex={index}
-        >
-          {stringifyTab(pill)}
-        </span>
-      </li>
-    ))}
-    {isTruncated && <li className="guide__pill guide__pill--seeall">See All</li>}
-  </ul>
-);
+const CardPills = ({ pillsNode, pillsetName }) => {
+  const getPillTitle = node => node.children[0].children[0].value;
 
-CardPills.propTypes = {
-  isTruncated: PropTypes.bool,
-  pills: PropTypes.arrayOf(PropTypes.string).isRequired,
-  pillsetName: PropTypes.string.isRequired,
+  // Get the name of each pill.
+  // If the total character length of pills > 50, truncate them and add a "See All" pill.
+  const mapPills = node => {
+    let totalLength = 0;
+    let isTruncated = false;
+    const pills = [];
+    node.children[0].children.forEach(pillObj => {
+      const pill = getPillTitle(pillObj);
+      if (totalLength > 50) {
+        isTruncated = true;
+      } else {
+        pills.push(pill);
+      }
+      totalLength += stringifyTab(pill).length + 4;
+    });
+    return [pills, isTruncated];
+  };
+
+  const [pills, isTruncated] = mapPills(pillsNode);
+
+  return (
+    <ul className="guide__pills">
+      {pills.map((pill, index) => (
+        <li className="guide__pill" key={index}>
+          <span
+            onClick={() => {
+              setLocalValue(pillsetName, pill);
+            }}
+            role="button"
+            tabIndex={index}
+          >
+            {stringifyTab(pill)}
+          </span>
+        </li>
+      ))}
+      {isTruncated && <li className="guide__pill guide__pill--seeall">See All</li>}
+    </ul>
+  );
 };
 
-CardPills.defaultProps = {
-  isTruncated: false,
+CardPills.propTypes = {
+  pillsetName: PropTypes.string.isRequired,
+  pillsNode: PropTypes.shape({
+    name: PropTypes.oneOf(['languages']).isRequired,
+    children: PropTypes.arrayOf(
+      PropTypes.shape({
+        type: PropTypes.oneOf(['list']).isRequired,
+        children: PropTypes.arrayOf(
+          PropTypes.shape({
+            type: PropTypes.oneOf(['listItem']).isRequired,
+            children: PropTypes.arrayOf(
+              PropTypes.shape({
+                type: PropTypes.oneOf(['paragraph']).isRequired,
+                children: PropTypes.arrayOf(
+                  PropTypes.shape({
+                    type: PropTypes.oneOf(['text']).isRequired,
+                    value: PropTypes.string.isRequired,
+                  })
+                ).isRequired,
+              })
+            ).isRequired,
+          })
+        ).isRequired,
+      })
+    ).isRequired,
+  }).isRequired,
 };
 
 const Card = ({ card, refDocMapping, time }) => {
-  const getTitle = val => {
-    if (refDocMapping[val] && refDocMapping[val].ast) {
-      return refDocMapping[val].ast.children[0].children[0].children[0].value;
+  const getCardTitle = cardSlug => {
+    if (refDocMapping[cardSlug].ast) {
+      return refDocMapping[cardSlug].ast.children[0].children[0].children[0].value;
     }
     return 'Title not found';
   };
 
-  const getPillTitle = node => {
-    if (node.type !== 'listItem') {
-      return '';
-    }
-    if (!node.children || !Array.isArray(node.children) || !node.children[0] || node.children[0].type !== 'paragraph') {
-      return '';
-    }
-    if (
-      !node.children[0].children ||
-      !Array.isArray(node.children[0].children) ||
-      !node.children[0].children[0] ||
-      node.children[0].children[0].type !== 'text'
-    ) {
-      return '';
-    }
-    if (!node.children[0].children[0].value) {
-      return '';
-    }
-    return node.children[0].children[0].value;
-  };
-
   const getPills = cardSlug => {
-    const languagesNode = findKeyValuePair(refDocMapping[cardSlug].ast.children, 'name', 'languages');
-    if (languagesNode && languagesNode.children[0].type === 'list') {
-      let totalLength = 0;
-      let isTruncated = false;
-      const languages = [];
-      languagesNode.children[0].children.forEach(langObj => {
-        const lang = getPillTitle(langObj);
-        if (totalLength > 50) {
-          isTruncated = true;
-        } else {
-          languages.push(lang);
-        }
-        totalLength += stringifyTab(lang).length + 4;
-      });
-      return [languages, isTruncated];
-    }
-    return [undefined, undefined];
+    return findKeyValuePair(refDocMapping[cardSlug].ast.children, 'name', 'languages');
   };
 
   const cardContent = () => {
     const cardSlug = card.argument[0].value;
-    const [languages, isTruncated] = card.name !== 'multi-card' ? getPills(cardSlug) : [undefined, undefined];
+    const languagesNode = card.name !== 'multi-card' ? getPills(cardSlug) : undefined;
     const innerContent = (
       <React.Fragment>
-        <div className="guide__title">{card.name === 'card' ? getTitle(cardSlug) : cardSlug}</div>
+        <div className="guide__title">{card.name === 'card' ? getCardTitle(cardSlug) : cardSlug}</div>
         {card.name === 'multi-card' ? (
           <ul className="guide__body">
             {card.children[0].children.map((listItem, index) => (
               <li className="guide__entry" key={index}>
-                <a href={listItem.children[0].children[0].value}>{getTitle(listItem.children[0].children[0].value)}</a>
+                <a href={listItem.children[0].children[0].value}>
+                  {getCardTitle(listItem.children[0].children[0].value)}
+                </a>
               </li>
             ))}
           </ul>
         ) : (
           <div className="guide__body" />
         )}
-        {card.name !== 'multi-card' && languages && (
-          <CardPills pills={languages} pillsetName="drivers" isTruncated={isTruncated} />
-        )}
+        {card.name !== 'multi-card' && languagesNode && <CardPills pillsNode={languagesNode} pillsetName="drivers" />}
         {card.name === 'card' && <div className="guide__time">{time} min</div>}
       </React.Fragment>
     );
@@ -132,16 +137,43 @@ Card.propTypes = {
     ),
     name: PropTypes.string.isRequired,
   }).isRequired,
-  refDocMapping: PropTypes.shape({
-    index: PropTypes.shape({
-      ast: PropTypes.object,
+  refDocMapping: PropTypes.oneOfType([
+    PropTypes.shape({
+      index: PropTypes.shape({
+        ast: PropTypes.object,
+      }).isRequired,
+    }),
+    PropTypes.shape({
+      [PropTypes.string]: PropTypes.shape({
+        ast: PropTypes.object.isRequired,
+      }).isRequired,
     }).isRequired,
-  }).isRequired,
+  ]),
   time: PropTypes.string,
 };
 
 Card.defaultProps = {
+  refDoc: undefined,
   time: undefined,
 };
 
 export default Card;
+
+/* 
+    [PropTypes.string]: PropTypes.shape({
+      ast: PropTypes.shape({
+        children: PropTypes.arrayOf(
+          PropTypes.shape({
+            children: PropTypes.arrayOf(
+              PropTypes.shape({
+                children: PropTypes.arrayOf(
+                  PropTypes.shape({
+                    value: PropTypes.string.isRequired,
+                  })
+                ).isRequired,
+              })
+            ).isRequired,
+          })
+        ).isRequired,
+      }).isRequired,
+    }).isRequired, */
