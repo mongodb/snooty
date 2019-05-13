9a3c0d802644d8d166deba07e57062ac9c618753
sophstad <sophie.stadler@mongodb.com>
2019-05-13 15:30:16 -0400

    Fix failure caused by missing value property

--- src/components/Roles/Class.js
+++ src/components/Roles/Class.js
@@ -2,24 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const baseUrl = 'https://api.mongodb.com/python/current/api/pymongo/results.html#';
-const RoleClass = ({
-  nodeData: {
-    label: { value },
-    target,
-  },
-}) => (
-  <a href={`${baseUrl}${target}`} className="reference external">
-    <code className="xref py py-class docutils literal notranslate">
-      <span className="pre">{value}</span>
-    </code>
-  </a>
-);
+const RoleClass = ({ nodeData: { label, target } }) => {
+  const labelDisplay = label && label.value ? label.value : target;
+  return (
+    <a href={`${baseUrl}${target}`} className="reference external">
+      <code className="xref py py-class docutils literal notranslate">
+        <span className="pre">{labelDisplay}</span>
+      </code>
+    </a>
+  );
+};
 
 RoleClass.propTypes = {
   nodeData: PropTypes.shape({
     label: PropTypes.shape({
-      value: PropTypes.string.isRequired,
-    }).isRequired,
+      value: PropTypes.string,
+    }),
     target: PropTypes.string.isRequired,
   }).isRequired,
 };
