d6639d029e35ad22a57e217191257035b006f968
Sophie Stadler <sophstad@users.noreply.github.com>
2019-06-27 14:36:09 -0400

    DOCSP-5772: Implement replace functionality (#87)

--- src/templates/document.js
+++ src/templates/document.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import ComponentFactory from '../components/ComponentFactory';
 import DefaultLayout from '../components/layout';
 import Footer from '../components/Footer';
+import { getIncludeFile } from '../utils/get-include-file';
 import { getNestedValue } from '../utils/get-nested-value';
+import { findAllKeyValuePairs } from '../utils/find-all-key-value-pairs';
 
 const Document = props => {
   const {
@@ -11,6 +13,28 @@ const Document = props => {
     pageContext: { __refDocMapping },
   } = props;
   const pageNodes = getNestedValue([pageSlug || 'index', 'ast', 'children'], __refDocMapping) || [];
+
+  // Identify and save all substitutions as defined on this page and in its included files
+  const getSubstitutions = () => {
+    // Find substitutions on page
+    const pageSubstitutions = findAllKeyValuePairs(pageNodes, 'type', 'substitution_definition');
+
+    // Find all include nodes on the page, get each include's contents, and find all substitutions in each include
+    const includes = findAllKeyValuePairs(pageNodes, 'name', 'include');
+    const includeContents = includes
+      .map(include => getIncludeFile(__refDocMapping, getNestedValue(['argument', 0, 'value'], include)))
+      .flat();
+    const includeSubstitutions = findAllKeyValuePairs(includeContents, 'type', 'substitution_definition');
+
+    // Merge page and include substitutions.
+    // Create a map wherein each key is the word to be replaced, and each value is the nodes to replace it with.
+    const substitutions = pageSubstitutions.concat(includeSubstitutions);
+    return substitutions.reduce((map, sub) => {
+      map[sub.name] = sub.children; // eslint-disable-line no-param-reassign
+      return map;
+    }, {});
+  };
+
   return (
     <DefaultLayout>
       <div className="content">
@@ -23,7 +47,12 @@ const Document = props => {
               <div className="bodywrapper">
                 <div className="body">
                   {pageNodes.map((child, index) => (
-                    <ComponentFactory key={index} nodeData={child} refDocMapping={__refDocMapping} />
+                    <ComponentFactory
+                      key={index}
+                      nodeData={child}
+                      refDocMapping={__refDocMapping}
+                      substitutions={getSubstitutions()}
+                    />
                   ))}
                   <Footer />
                 </div>
