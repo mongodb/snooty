858b25a27d0671afbd8435cff4912d40814a307c
sophstad <sophie.stadler@mongodb.com>
2019-09-11 14:47:13 -0400

    Add PropTypes

--- src/components/CardGroup.js
+++ src/components/CardGroup.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withPrefix } from 'gatsby';
+import { getNestedValue } from '../utils/get-nested-value';
 
 const Card = ({ cardid, headline, image, link, type }) => (
-  <a href={link} className={`card card-${type}`} id="cardid">
+  <a href={link} className={`card card-${type}`} id={cardid}>
     <div className="card-image">
-      <img alt={image} src={withPrefix(image)} />
+      <img src={withPrefix(image)} alt={cardid} />
     </div>
     <div className="card-content">
       <div className="card-headline">{headline}</div>
@@ -13,18 +14,32 @@ const Card = ({ cardid, headline, image, link, type }) => (
   </a>
 );
 
+Card.propTypes = {
+  cardid: PropTypes.string.isRequired,
+  headline: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+};
+
 const CardGroup = ({ nodeData }) => {
-  console.log(nodeData);
-  const {
-    options: { type },
-  } = nodeData;
+  const type = getNestedValue(['options', 'type'], nodeData);
   return (
     <div className="card_group">
-      {nodeData.children.map((child, index) => {
-        return <Card key={index} type={type} {...child.options} />;
-      })}
+      {nodeData.children.map((child, index) => (
+        <Card key={index} type={type} {...child.options} />
+      ))}
     </div>
   );
+};
+
+CardGroup.propTypes = {
+  nodeData: PropTypes.shape({
+    children: PropTypes.arrayOf(PropTypes.object).isRequired,
+    options: PropTypes.shape({
+      type: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default CardGroup;
