272a1fe5bfbe12a26ecdb2151a1bdebf8f7f8a2c
Daniel Borowski <danielborowski@Daniels-MacBook-Pro-5.local>
2019-09-12 11:50:42 -0400

    add content branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,10 @@ require('dotenv').config({
   path: `.env.${runningEnv}`,
 });
 
+const getContentBranch = () => {
+  return process.env.GATSBY_CONTENT_BRANCH || getGitBranch();
+};
+
 const getGitBranch = () => {
   return execSync('git rev-parse --abbrev-ref HEAD')
     .toString('utf8')
@@ -15,16 +19,16 @@ const getGitBranch = () => {
 
 const getPathPrefix = () => {
   const user = userInfo().username;
-  const gitBranch = getGitBranch();
+  const branch = getContentBranch();
 
-  return runningEnv === 'production' ? `/${process.env.GATSBY_SITE}/${user}/${gitBranch}` : '/';
+  return runningEnv === 'production' ? `/${process.env.GATSBY_SITE}/${user}/${branch}` : '/';
 };
 
 module.exports = {
   pathPrefix: getPathPrefix(),
   plugins: ['gatsby-plugin-react-helmet'],
   siteMetadata: {
-    branch: getGitBranch(),
+    branch: getContentBranch(),
     project: process.env.GATSBY_SITE,
     title: 'MongoDB Guides',
     user: userInfo().username,
