80b93a8334f4250d4fea8b5a22a1476a24774d2b
sophstad <sophie.stadler@mongodb.com>
2019-09-23 13:11:01 -0400

    Pass tests

--- tests/unit/Figure.test.js
+++ tests/unit/Figure.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from 'enzyme';
 import Figure from '../../src/components/Figure';
 
 // data for this component
@@ -7,11 +7,11 @@ import mockData from './data/Figure.test.json';
 import lightboxData from './data/FigureLightbox.test.json';
 
 it('renders correctly', () => {
-  const tree = shallow(<Figure nodeData={mockData} />);
+  const tree = render(<Figure nodeData={mockData} />);
   expect(tree).toMatchSnapshot();
 });
 
 it('renders lightbox correctly when specified as an option', () => {
-  const tree = shallow(<Figure nodeData={lightboxData} />);
+  const tree = render(<Figure nodeData={lightboxData} />);
   expect(tree).toMatchSnapshot();
 });
