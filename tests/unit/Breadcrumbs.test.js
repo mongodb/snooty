1b54d35792f8ba9c4cf7b6050d907b302a3ea532
Sophie Stadler <sophstad@users.noreply.github.com>
2019-11-15 15:25:28 -0500

    DOCSP-7498: Implement breadcrumbs component (#125)

---
+++ tests/unit/Breadcrumbs.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Breadcrumbs from '../../src/components/Breadcrumbs';
+import slugTitleMapping from './data/ecosystem-slugTitleMapping.json';
+
+const mockData = ['drivers', 'drivers/php'];
+
+it('renders correctly', () => {
+  const tree = shallow(<Breadcrumbs parentPaths={mockData} slugTitleMapping={slugTitleMapping} />);
+  expect(tree).toMatchSnapshot();
+});
+
+it('fails gracefully', () => {
+  const tree = shallow(<Breadcrumbs parentPaths={null} slugTitleMapping={slugTitleMapping} />);
+  expect(tree).toMatchSnapshot();
+})
