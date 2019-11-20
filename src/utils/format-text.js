b438f38fef7d6a0597eafb269004ddf08f66677f
Sophie Stadler <sophstad@users.noreply.github.com>
2019-11-20 12:14:05 -0500

    DOCSP-7497: Update frontend to use formatted slug-title mapping (#131)
    * Update frontend to use formatted slug-title mapping
    
    * Tests passing
    
    * Nicer syntax

---
+++ src/utils/format-text.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ComponentFactory from '../components/ComponentFactory';
+
+/*
+ * Given either a string or an array of Snooty text nodes, return the appropriate text output.
+ */
+export const formatText = text =>
+  typeof text === 'string' ? text : text.map((e, index) => <ComponentFactory key={index} nodeData={e} />);
