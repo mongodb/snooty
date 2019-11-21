e5ed11462b2452dd4ba896b11a130ca632870d02
Sophie Stadler <sophstad@users.noreply.github.com>
2019-11-21 14:39:19 -0500

    DOCSP-7630: Internal page navigation (#132)

--- src/utils/get-page-slug.js
+++ src/utils/get-page-slug.js
@@ -1,6 +1,6 @@
 // Takes a look at the page name and returns the appropriate page url
-const getPageUrl = page => {
+const getPageSlug = page => {
   return page === 'index' ? '/' : page;
 };
 
-module.exports.getPageUrl = getPageUrl;
+module.exports.getPageSlug = getPageSlug;
