66487329a60f7949ea3c03f57e65f5972bcf2bf2
Sophie Stadler <sophstad@users.noreply.github.com>
2020-01-21 14:32:25 -0500

    DOCSP-7862: Ignore toctree directive (#144)

--- src/utils/generate-path-prefix.js
+++ src/utils/generate-path-prefix.js
@@ -1,6 +1,6 @@
 const generatePathPrefix = ({ parserBranch, project, snootyBranch, user }) => {
   const base = `/${project}/${user}/${parserBranch}`;
-  return process.env.SNOOTY_DEV ? `/${parserBranch}/${project}/${user}/${snootyBranch}` : base;
+  return process.env.GATSBY_SNOOTY_DEV ? `/${parserBranch}/${project}/${user}/${snootyBranch}` : base;
 };
 
 // TODO: switch to ES6 export syntax if Gatsby implements support for ES6 module imports
