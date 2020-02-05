7bb175542d7270e5763c0f01d26c702190a95db8
Nick Larew <nick.larew@10gen.com>
2020-02-05 13:33:39 -0500

    Remove unused packages & commented code

--- src/components/FeedbackWidget/FeedbackTab.js
+++ src/components/FeedbackWidget/FeedbackTab.js
@@ -5,19 +5,13 @@ import usePageSize from '../../hooks/usePageSize';
 
 import LeafygreenCard from '@leafygreen-ui/card';
 
-// const Container = styled.div`
-//   position: fixed;
-//   top: 256px;
-//   right: 35px;
-//   transform: rotate(-90deg);
-//   transform-origin: top right;
-//   user-select: none;
+export default function FeedbackTab(props) {
+  const { feedback, initializeFeedback } = useFeedbackState();
+  const { isTabletOrMobile } = usePageSize();
+  const shouldShowFeedbackTab = !feedback && !isTabletOrMobile;
+  return shouldShowFeedbackTab && <Container onClick={() => initializeFeedback()}>Give Feedback</Container>;
+}
 
-//   padding: 8px;
-//   border: 1px solid black;
-//   border-top-left-radius: 4px;
-//   border-top-right-radius: 4px;
-// `;
 const Container = styled(LeafygreenCard)`
   padding: 12px;
   position: fixed;
@@ -27,9 +21,3 @@ const Container = styled(LeafygreenCard)`
   transform-origin: top right;
   user-select: none;
 `;
-export default function FeedbackTab(props) {
-  const { feedback, initializeFeedback } = useFeedbackState();
-  const { isTabletOrMobile } = usePageSize();
-  const shouldShowFeedbackTab = !feedback && !isTabletOrMobile;
-  return shouldShowFeedbackTab && <Container onClick={() => initializeFeedback()}>Give Feedback</Container>;
-}
