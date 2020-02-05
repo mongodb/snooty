354b3c8459a701295a03dfb08c5a1d4b8e8c16f1
Nick Larew <nick.larew@10gen.com>
2020-02-05 13:55:15 -0500

    Use esm export rather than cjs export in browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 const React = require('react');
 const DefaultLayout = require('./src/components/layout').default;
 
-exports.wrapPageElement = ({ element, props }) => <DefaultLayout {...props}>{element}</DefaultLayout>;
+export const wrapPageElement = ({ element, props }) => <DefaultLayout {...props}>{element}</DefaultLayout>;
