358d3db4fbd7a6e2ef154e978e125e525ad0490d
tahiya-mongo <tahiya.chowdhury@10gen.com>
2020-03-26 21:22:39 -0400

    Removed unecessary renaming of variable

--- src/templates/guide.js
+++ src/templates/guide.js
@@ -21,8 +21,7 @@ export default class Guide extends Component {
     const {
       pageContext: {
         __refDocMapping,
-        // eslint-disable-next-line no-unused-vars
-        metadata: { slugToTitle: slugTitleMapping },
+        metadata: { slugToTitle },
       },
     } = this.props;
 
