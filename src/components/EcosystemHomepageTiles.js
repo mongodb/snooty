921cafdcbbff831317ad51cedeea77109da9218b
Daniel Borowski <danielborowski@Daniels-MBP-5.fios-router.home>
2020-03-31 12:31:25 -0400

    update linting

--- src/components/EcosystemHomepageTiles.js
+++ src/components/EcosystemHomepageTiles.js
@@ -4,79 +4,79 @@ import Card from '@leafygreen-ui/card';
 import Link from './Link';
 
 // language icons for tiles
-import IconC from '../../static/svg/icon-C';
-import IconCpp from '../../static/svg/icon-Cpp';
-import IconCsharp from '../../static/svg/icon-Csharp';
-import IconGo from '../../static/svg/icon-Go';
-import IconJava from '../../static/svg/icon-Java';
-import IconNode from '../../static/svg/icon-Node';
-import IconPHP from '../../static/svg/icon-PHP';
-import IconPython from '../../static/svg/icon-Python';
-import IconRuby from '../../static/svg/icon-Ruby';
-import IconPerl from '../../static/svg/icon-Perl';
-import IconScala from '../../static/svg/icon-Scala';
-import IconSwift from '../../static/svg/icon-Swift';
+import IconC from '../../static/svg/icons/icon-C';
+import IconCpp from '../../static/svg/icons/icon-Cpp';
+import IconCsharp from '../../static/svg/icons/icon-Csharp';
+import IconGo from '../../static/svg/icons/icon-Go';
+import IconJava from '../../static/svg/icons/icon-Java';
+import IconNode from '../../static/svg/icons/icon-Node';
+import IconPHP from '../../static/svg/icons/icon-PHP';
+import IconPython from '../../static/svg/icons/icon-Python';
+import IconRuby from '../../static/svg/icons/icon-Ruby';
+import IconPerl from '../../static/svg/icons/icon-Perl';
+import IconScala from '../../static/svg/icons/icon-Scala';
+import IconSwift from '../../static/svg/icons/icon-Swift';
 
 // does not take in any params because it is a hardcoded list
 const EcosystemHomepageTiles = () => {
   const tileValues = [
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/c/',
+      slug: '/drivers/c/',
       title: 'C',
       icon: <IconC />,
     },
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/cxx/',
+      slug: '/drivers/cxx/',
       title: 'C++',
       icon: <IconCpp />,
     },
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/csharp/',
+      slug: '/drivers/csharp/',
       title: 'C#',
       icon: <IconCsharp />,
     },
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/go/',
+      slug: '/drivers/go/',
       title: 'Go',
       icon: <IconGo />,
     },
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/java/',
+      slug: '/drivers/java/',
       title: 'Java',
       icon: <IconJava />,
     },
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/node/',
+      slug: '/drivers/node/',
       title: 'Node.js',
       icon: <IconNode />,
     },
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/perl/',
+      slug: '/drivers/perl/',
       title: 'Perl',
       icon: <IconPerl />,
     },
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/php/',
+      slug: '/drivers/php/',
       title: 'PHP',
       icon: <IconPHP />,
     },
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/python/',
+      slug: '/drivers/python/',
       title: 'Python',
       icon: <IconPython />,
     },
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/ruby/',
+      slug: '/ruby-driver/current/',
       title: 'Ruby',
       icon: <IconRuby />,
     },
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/scala/',
+      slug: '/drivers/scala/',
       title: 'Scala',
       icon: <IconScala />,
     },
     {
-      href: 'https://docs.mongodb.com/ecosystem/drivers/swift/',
+      slug: '/drivers/swift/',
       title: 'Swift',
       icon: <IconSwift />,
     },
@@ -85,7 +85,7 @@ const EcosystemHomepageTiles = () => {
   return (
     <ul className={tileStyles.tileOuter}>
       {tileValues.map((element, index) => (
-        <Link to={element.href} className={tileStyles.tileAnchor}>
+        <Link to={element.slug} className={tileStyles.tileAnchor}>
           <Card as="li" className={tileStyles.tile}>
             {element.icon}
             {element.title}
