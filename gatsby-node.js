7ca2a17a6b97a831d42756c0284b0992c4525cb5
sophstad <sophie.stadler@mongodb.com>
2020-04-08 13:37:29 -0400

    Query builds by patch ID

--- gatsby-node.js
+++ gatsby-node.js
@@ -16,11 +16,12 @@ const METADATA_COLLECTION = 'metadata';
 
 const constructPageIdPrefix = ({ project, parserUser, parserBranch }) => `${project}/${parserUser}/${parserBranch}`;
 
-const constructBuildFilter = ({ commitHash, ...rest }) => {
+const constructBuildFilter = ({ commitHash, patchId, ...rest }) => {
   const pageIdPrefix = constructPageIdPrefix(rest);
   return {
     page_id: { $regex: new RegExp(`^${pageIdPrefix}/*`) },
     commit_hash: commitHash || { $exists: false },
+    patch_id: patchId || { $exists: false },
   };
 };
 
