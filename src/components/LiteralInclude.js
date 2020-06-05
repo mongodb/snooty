51a51d5f1f9a2dcc8b9d5f632dc7a19fa0dd4ab6
mango-db <Claire.Goeckner-Wald@mongodb.com>
2020-06-05 18:16:43 -0400

    DOP-877: Properly render literal includes

--- src/components/LiteralInclude.js
+++ src/components/LiteralInclude.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { node } from 'prop-types';
 import ComponentFactory from './ComponentFactory';
 import { getNestedValue } from '../utils/get-nested-value';
 
@@ -7,24 +7,13 @@ export default class LiteralInclude extends Component {
   constructor(props) {
     super(props);
 
-    const { nodeData, refDocMapping } = this.props;
+    const { nodeData, _ } = this.props;
 
-    let key = getNestedValue(['argument', 0, 'value'], nodeData);
-    // fix for some includes
-    if (key && key.startsWith('/')) {
-      key = key.substr(1);
+    this.codeExample = '';
+
+    if (nodeData.children && nodeData.children.length) {
+      this.codeExample = nodeData.children[0].value;
     }
-    const startText = getNestedValue(['options', 'start-after'], nodeData);
-    const endText = getNestedValue(['options', 'end-before'], nodeData);
-    this.resolvedIncludeData = refDocMapping[key];
-
-    this.codeExample =
-      typeof this.resolvedIncludeData === 'string'
-        ? this.resolvedIncludeData.substring(
-            this.resolvedIncludeData.indexOf(startText) + startText.length,
-            this.resolvedIncludeData.indexOf(endText)
-          )
-        : '';
   }
 
   render() {
@@ -33,7 +22,7 @@ export default class LiteralInclude extends Component {
         {...this.props}
         nodeData={{
           type: 'code',
-          value: this.codeExample.substring(0, this.codeExample.lastIndexOf('\n')).trim(),
+          value: this.codeExample,
         }}
       />
     );
@@ -42,15 +31,7 @@ export default class LiteralInclude extends Component {
 
 LiteralInclude.propTypes = {
   nodeData: PropTypes.shape({
-    argument: PropTypes.arrayOf(
-      PropTypes.shape({
-        value: PropTypes.string,
-      })
-    ),
-    options: PropTypes.shape({
-      'start-after': PropTypes.string.isRequired,
-      'end-before': PropTypes.string.isRequired,
-    }),
+    children: PropTypes.arrayOf(PropTypes.node).isRequired,
   }).isRequired,
-  refDocMapping: PropTypes.objectOf(PropTypes.object).isRequired,
+  codeExample: PropTypes.string.isRequired,
 };
