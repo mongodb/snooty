3713110e1f10cff1b6db280156619e95b781dc58
Sophie Stadler <sophstad@users.noreply.github.com>
2020-06-16 12:03:54 -0400

    Revert "DOP-877: Properly render literal includes (#200)" (#207)
    This reverts commit da24dd96284d2ecc2554173c71b9bd0a3e8059a3.

--- src/components/LiteralInclude.js
+++ src/components/LiteralInclude.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ComponentFactory from './ComponentFactory';
+import { getNestedValue } from '../utils/get-nested-value';
 
-const LiteralInclude = ({ nodeData: { children }, ...rest }) => {
-  return children.map(child => <ComponentFactory {...rest} nodeData={child} />);
-};
+export default class LiteralInclude extends Component {
+  constructor(props) {
+    super(props);
+
+    const { nodeData, refDocMapping } = this.props;
+
+    let key = getNestedValue(['argument', 0, 'value'], nodeData);
+    // fix for some includes
+    if (key && key.startsWith('/')) {
+      key = key.substr(1);
+    }
+    const startText = getNestedValue(['options', 'start-after'], nodeData);
+    const endText = getNestedValue(['options', 'end-before'], nodeData);
+    this.resolvedIncludeData = refDocMapping[key];
+
+    this.codeExample =
+      typeof this.resolvedIncludeData === 'string'
+        ? this.resolvedIncludeData.substring(
+            this.resolvedIncludeData.indexOf(startText) + startText.length,
+            this.resolvedIncludeData.indexOf(endText)
+          )
+        : '';
+  }
+
+  render() {
+    return (
+      <ComponentFactory
+        {...this.props}
+        nodeData={{
+          type: 'code',
+          value: this.codeExample.substring(0, this.codeExample.lastIndexOf('\n')).trim(),
+        }}
+      />
+    );
+  }
+}
 
 LiteralInclude.propTypes = {
   nodeData: PropTypes.shape({
-    children: PropTypes.arrayOf(PropTypes.node).isRequired,
+    argument: PropTypes.arrayOf(
+      PropTypes.shape({
+        value: PropTypes.string,
+      })
+    ),
+    options: PropTypes.shape({
+      'start-after': PropTypes.string.isRequired,
+      'end-before': PropTypes.string.isRequired,
+    }),
   }).isRequired,
+  refDocMapping: PropTypes.objectOf(PropTypes.object).isRequired,
 };
-
-export default LiteralInclude;
