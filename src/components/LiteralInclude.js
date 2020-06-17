29d85b57c462ee9df4dd01a1bf8c9478cfae3a9f
Sophie Stadler <sophstad@users.noreply.github.com>
2020-06-17 16:33:38 -0400

    Revert "Revert "DOP-877: Properly render literal includes (#200)" (#207)" (#210)

--- src/components/LiteralInclude.js
+++ src/components/LiteralInclude.js
@@ -1,56 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ComponentFactory from './ComponentFactory';
-import { getNestedValue } from '../utils/get-nested-value';
 
-export default class LiteralInclude extends Component {
-  constructor(props) {
-    super(props);
-
-    const { nodeData, refDocMapping } = this.props;
-
-    let key = getNestedValue(['argument', 0, 'value'], nodeData);
-    // fix for some includes
-    if (key && key.startsWith('/')) {
-      key = key.substr(1);
-    }
-    const startText = getNestedValue(['options', 'start-after'], nodeData);
-    const endText = getNestedValue(['options', 'end-before'], nodeData);
-    this.resolvedIncludeData = refDocMapping[key];
-
-    this.codeExample =
-      typeof this.resolvedIncludeData === 'string'
-        ? this.resolvedIncludeData.substring(
-            this.resolvedIncludeData.indexOf(startText) + startText.length,
-            this.resolvedIncludeData.indexOf(endText)
-          )
-        : '';
-  }
-
-  render() {
-    return (
-      <ComponentFactory
-        {...this.props}
-        nodeData={{
-          type: 'code',
-          value: this.codeExample.substring(0, this.codeExample.lastIndexOf('\n')).trim(),
-        }}
-      />
-    );
-  }
-}
+const LiteralInclude = ({ nodeData: { children }, ...rest }) => {
+  return children.map(child => <ComponentFactory {...rest} nodeData={child} />);
+};
 
 LiteralInclude.propTypes = {
   nodeData: PropTypes.shape({
-    argument: PropTypes.arrayOf(
-      PropTypes.shape({
-        value: PropTypes.string,
-      })
-    ),
-    options: PropTypes.shape({
-      'start-after': PropTypes.string.isRequired,
-      'end-before': PropTypes.string.isRequired,
-    }),
+    children: PropTypes.arrayOf(PropTypes.node).isRequired,
   }).isRequired,
-  refDocMapping: PropTypes.objectOf(PropTypes.object).isRequired,
 };
+
+export default LiteralInclude;
