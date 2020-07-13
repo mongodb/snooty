33c6b9f436b79f62a43e07d9b77e32ce3d5f9fe5
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-07-13 16:45:33 -0400

    Implement basic expansion animation for mobile

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { css, keyframes } from '@emotion/core';
 import styled from '@emotion/styled';
 import Button from '@leafygreen-ui/button';
@@ -7,27 +7,12 @@ import { uiColors } from '@leafygreen-ui/palette';
 import TextInput from '@leafygreen-ui/text-input';
 import { theme } from '../../theme/docsTheme';
 import SearchDropdown from './SearchDropdown';
+import useScreenSize from '../../hooks/useScreenSize';
 
 const GO_BUTTON_COLOR = uiColors.green.light3;
 const GO_BUTTON_SIZE = theme.size.medium;
 const SEARCHBAR_DESKTOP_WIDTH = 372;
 const SEARCHBAR_HEIGHT = 36;
-
-const animationKeyframe = startingOpacity => keyframes`
-    0% {
-      opacity: ${startingOpacity};
-    }
-    100% {
-      opacity: 1;
-    }
-`;
-
-const fadeInAnimation = (startingOpacity, seconds) => css`
-  animation: ${animationKeyframe(startingOpacity)};
-  animation-duration: ${seconds};
-  animation-iteration-count: 1;
-  animation-timing-function: ease-in;
-`;
 
 const GoArrowIcon = styled(Icon)`
   left: 4px;
@@ -68,12 +53,13 @@ const SearchbarContainer = styled('div')`
   position: fixed;
   right: 16px;
   top: 5px;
-  width: ${SEARCHBAR_DESKTOP_WIDTH}px;
+  width: ${({ isExpanded }) => (isExpanded ? `${SEARCHBAR_DESKTOP_WIDTH}px` : '24px')};
   /* docs-tools navbar z-index is 9999 */
   z-index: 10000;
+  transition: width 150ms ease-in, opacity 300ms ease-in-out;
   :focus-within {
     opacity: 1;
-    ${fadeInAnimation(0.6, '0.3s')};
+    transition: opacity 300ms ease-in-out;
   }
 `;
 
@@ -94,11 +80,44 @@ const StyledTextInput = styled(TextInput)`
   }
 `;
 
+const ExpandButton = styled(Button)`
+  background-color: none;
+  border-radius: ${GO_BUTTON_SIZE};
+  height: ${GO_BUTTON_SIZE};
+  position: absolute;
+  right: 8px;
+  top: 6px;
+  width: ${GO_BUTTON_SIZE};
+  z-index: 1;
+  /* Below removes default hover effects from button */
+  background-image: none;
+  border: none;
+  box-shadow: none;
+  :before {
+    display: none;
+  }
+  :after {
+    display: none;
+  }
+`;
+
+const ExpandMagnifyingGlass = styled(Icon)`
+  position: absolute;
+  left: 4px;
+  top: 4px;
+  z-index: 1;
+`;
+
 const Searchbar = () => {
   const [value, setValue] = useState('');
   const onChange = useCallback(e => setValue(e.target.value), []);
   const [blurEvent, setBlurEvent] = useState(null);
   const [isFocused, setIsFocused] = useState(false);
+  const { isMediumScreen } = useScreenSize();
+  const [isExpanded, setIsExpanded] = useState(!isMediumScreen);
+  useEffect(() => {
+    setIsExpanded(!isMediumScreen);
+  }, [isMediumScreen]);
   // A user is searching if the text input is focused and it is not empty
   const isSearching = useMemo(() => !!value && isFocused, [isFocused, value]);
   const onFocus = useCallback(() => {
@@ -108,11 +127,19 @@ const Searchbar = () => {
   // The React onBlur event fires when tabbing between child elements
   const onBlur = useCallback(() => setBlurEvent(setTimeout(() => setIsFocused(false), 0)), []);
   return (
-    <SearchbarContainer onBlur={onBlur} onFocus={onFocus}>
-      <MagnifyingGlass glyph="MagnifyingGlass" fill="#061621" />
-      <StyledTextInput onChange={onChange} placeholder="Search Documentation" tabIndex="0" value={value} />
-      {!!value && <GoButton href="#" glyph={<GoArrowIcon glyph="ArrowRight" fill="#13AA52" />} />}
-      {isSearching && <SearchDropdown />}
+    <SearchbarContainer isMobile={isMediumScreen} isExpanded={isExpanded} onBlur={onBlur} onFocus={onFocus}>
+      {isExpanded ? (
+        <>
+          <MagnifyingGlass glyph="MagnifyingGlass" fill={uiColors.black} />
+          <StyledTextInput onChange={onChange} placeholder="Search Documentation" tabIndex="0" value={value} />
+          {!!value && <GoButton href="#" glyph={<GoArrowIcon glyph="ArrowRight" fill="#13AA52" />} />}
+          {isSearching && <SearchDropdown />}
+        </>
+      ) : (
+        <ExpandButton onClick={() => setIsExpanded(true)}>
+          <ExpandMagnifyingGlass glyph="MagnifyingGlass" fill={uiColors.gray.base} />
+        </ExpandButton>
+      )}
     </SearchbarContainer>
   );
 };
--- src/hooks/useScreenSize.js
+++ src/hooks/useScreenSize.js
@@ -3,6 +3,7 @@ import { useMediaQuery } from 'react-responsive';
 export default function useScreenSize() {
   const isSmallScreen = useMediaQuery({ query: '(max-width: 750px)' });
   const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+  const isMediumScreen = useMediaQuery({ query: 'only screen and (max-width: 767px)' });
   // const isDesktopOrLaptop = useMediaQuery({ query: '(min-device-width: 1224px)' });
   // const isBigScreen = useMediaQuery({ query: '(min-device-width: 1824px)' });
   // const isTabletOrMobileDevice = useMediaQuery({ query: '(max-device-width: 1224px)' });
@@ -10,6 +11,7 @@ export default function useScreenSize() {
   // const isRetina = useMediaQuery({ query: '(min-resolution: 2dppx)' });
 
   const screen = {
+    isMediumScreen,
     isTabletOrMobile,
     isSmallScreen,
   };
