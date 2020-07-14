1073f53c81093e5b5f90a306a623fd0f99bb5254
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-07-14 11:44:27 -0400

    [DOP-1219] Swap Navbar to Function Component (#225)
    * Add leafygreen text input
    
    * Build general searchbar structure
    
    * Fix blur animation issue
    
    * Clean up CSS
    
    * Switch Navbar to Function Component
    
    * Use memoized value with active on start

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,49 @@
-import React, { Component } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { withPrefix } from 'gatsby';
 import { isBrowser } from '../utils/is-browser';
 import { URL_SLUGS } from '../constants';
 import Searchbar from './Searchbar';
 
-export default class Navbar extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeLink: '',
-    };
-
-    // Static navprops by default
-    this.navprops = `{"links": [
-                            {"url": "https://docs.mongodb.com/manual/","text": "Server"},
-                            {"url": "https://docs.mongodb.com/drivers/","text": "Drivers"},
-                            {"url": "https://docs.mongodb.com/cloud/","text": "Cloud"},
-                            {"url": "https://docs.mongodb.com/tools/","text": "Tools"},
-                            {"url": "https://docs.mongodb.com/guides/","text": "Guides"}]}`;
+const getActiveSection = (slug, urlItems) => {
+  const urlMapping = Object.entries(urlItems).find(([, value]) => value.includes(slug));
+  if (urlMapping) {
+    return urlMapping[0];
   }
 
-  componentDidMount() {
+  if (isBrowser) {
+    switch (window.location.pathname) {
+      case 'tools':
+        return 'tools';
+      case 'cloud':
+        return 'cloud';
+      default:
+        return null;
+    }
+  }
+
+  return null;
+};
+
+const Navbar = () => {
+  const [activeLink, setActiveLink] = useState('');
+  const isActiveLink = useCallback(link => link.toLowerCase() === activeLink, [activeLink]);
+
+  // modify navprops
+  const modifyActiveLink = useMemo(
+    () =>
+      `{"links": [
+        {"url": "https://docs.mongodb.com/manual/","text": "Server", "active": ${isActiveLink('Server')}},
+        {"url": "https://docs.mongodb.com/drivers/","text": "Drivers", "active": ${isActiveLink('Drivers')}},
+        {"url": "https://docs.mongodb.com/cloud/","text": "Cloud", "active": ${isActiveLink('Cloud')}},
+        {"url": "https://docs.mongodb.com/tools/","text": "Tools", "active": ${isActiveLink('Tools')}},
+        {"url": "https://docs.mongodb.com/guides/","text": "Guides", "active": ${isActiveLink('Guides')}}
+      ]}`,
+    [isActiveLink]
+  );
+
+  const [navprops, setNavprops] = useState(modifyActiveLink);
+
+  useEffect(() => {
     // Add script to give navbar functionality and css
     const script = document.createElement('script');
     script.src = withPrefix('docs-tools/navbar.min.js');
@@ -29,58 +51,19 @@ export default class Navbar extends Component {
 
     document.body.appendChild(script);
 
-    this.setState({ activeLink: this.getActiveSection(process.env.GATSBY_SITE, URL_SLUGS) });
-  }
+    setActiveLink(getActiveSection(process.env.GATSBY_SITE, URL_SLUGS));
+  }, []);
 
-  getActiveSection = (slug, urlItems) => {
-    const urlMapping = Object.entries(urlItems).find(([, value]) => value.includes(slug));
-    if (urlMapping) {
-      return urlMapping[0];
-    }
+  useEffect(() => {
+    setNavprops(modifyActiveLink);
+  }, [activeLink, modifyActiveLink]);
 
-    if (isBrowser) {
-      switch (window.location.pathname) {
-        case 'tools':
-          return 'tools';
-        case 'cloud':
-          return 'cloud';
-        default:
-          return null;
-      }
-    }
+  return (
+    <>
+      <div tabIndex="0" id="navbar" className="navbar" data-navprops={navprops} />
+      <Searchbar />
+    </>
+  );
+};
 
-    return null;
-  };
-
-  isActiveLink = link => {
-    const { activeLink } = this.state;
-    return link.toLowerCase() === activeLink;
-  };
-
-  // Modify navprops
-  modifyActiveLink = () => {
-    return `{"links": [
-                    {"url": "https://docs.mongodb.com/manual/","text": "Server", "active": ${this.isActiveLink(
-                      'Server'
-                    )}},
-                    {"url": "https://docs.mongodb.com/drivers/","text": "Drivers", "active": ${this.isActiveLink(
-                      'Drivers'
-                    )}},
-                    {"url": "https://docs.mongodb.com/cloud/","text": "Cloud", "active": ${this.isActiveLink('Cloud')}},
-                    {"url": "https://docs.mongodb.com/tools/","text": "Tools", "active": ${this.isActiveLink('Tools')}},
-                    {"url": "https://docs.mongodb.com/guides/","text": "Guides", "active": ${this.isActiveLink(
-                      'Guides'
-                    )}}
-    ]}`;
-  };
-
-  render() {
-    this.navprops = this.modifyActiveLink();
-    return (
-      <>
-        <div id="navbar" className="navbar" data-navprops={this.navprops} tabIndex="0" />
-        <Searchbar />
-      </>
-    );
-  }
-}
+export default Navbar;
