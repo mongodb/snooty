2590acebba22242c2ead14e57b02ad287afa638d
Sophie Stadler <sophstad@users.noreply.github.com>
2020-07-21 10:18:10 -0400

    DOP-1244: Update admonition rendering (#229)

--- src/components/Admonition.js
+++ src/components/Admonition.js
@@ -1,64 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import Callout, { Variant } from '@leafygreen-ui/callout';
 import ComponentFactory from './ComponentFactory';
-import CSSWrapper from './CSSWrapper';
-import { getNestedValue } from '../utils/get-nested-value';
-import { makeId } from '../utils/make-id';
+import { getPlaintext } from '../utils/get-plaintext';
 
-// The classname associated with each admonition is illogical, so map them statically here
-const CLASSNAME_MAP = {
-  admonition: '',
-  example: 'admonition-example',
-  important: 'important',
-  note: 'note',
-  seealso: 'seealso',
-  tip: 'admonition-tip',
-  warning: 'warning',
+// TODO: Update with example variant once added to LeafyGreen
+export const admonitionMap = {
+  example: Variant.Note,
+  important: Variant.Important,
+  note: Variant.Note,
+  tip: Variant.Tip,
+  see: Variant.Tip,
+  seealso: Variant.Tip,
+  warning: Variant.Warning,
 };
 
-const Admonition = ({ nodeData, ...rest }) => {
-  const { name } = nodeData;
+const StyledCallout = styled(Callout)`
+  margin: 24px 0;
 
-  // If an admonition's contents are written on the same line as the directive in the rST, they will appear as node arguments.
-  // In this case, the admonition has no defined title, and we need to treat the argument property as the node's children.
-  const hasChildren = nodeData.children.length > 0;
-  const childElements = hasChildren ? nodeData.children : nodeData.argument;
-  let titleText;
-  if (hasChildren) {
-    titleText = getNestedValue(['argument', 0, 'value'], nodeData);
+  & p {
+    margin: 0 0 12.5px;
   }
+
+  & p:last-child {
+    margin: 0;
+  }
+`;
+
+const Admonition = ({ nodeData: { argument, children, name }, ...rest }) => {
+  let title = getPlaintext(argument);
+  if (name === 'see') {
+    title = `See: ${title}`;
+  } else if (name === 'seealso') {
+    title = `See Also: ${title}`;
+  } else if (name === 'example') {
+    title = `Example: ${title}`;
+  }
+
   return (
-    <div
-      className={[
-        'admonition',
-        CLASSNAME_MAP[name],
-        getNestedValue(['options', 'class'], nodeData),
-        titleText ? `admonition-${makeId(titleText)}` : null, // stringify title into slug
-      ].join(' ')}
-    >
-      <p className="first admonition-title">{titleText || (name === 'seealso' ? 'see also' : name)}</p>
-      {childElements.map((child, index) => {
-        // Apply "last" class to the last child element of admonition
-        if (index === nodeData.children.length - 1) {
-          return (
-            <CSSWrapper key={index} className="last">
-              <ComponentFactory {...rest} parentNode={null} nodeData={child} />
-            </CSSWrapper>
-          );
-        }
-        return <ComponentFactory {...rest} nodeData={child} key={index} />;
-      })}
-    </div>
+    <StyledCallout title={title} variant={admonitionMap[name] || Variant.Note}>
+      {children.map((child, i) => (
+        <ComponentFactory {...rest} key={i} nodeData={child} />
+      ))}
+    </StyledCallout>
   );
 };
 
 Admonition.propTypes = {
   nodeData: PropTypes.shape({
-    argument: PropTypes.arrayOf(
-      PropTypes.shape({
-        value: PropTypes.string,
-      })
-    ).isRequired,
+    argument: PropTypes.arrayOf(PropTypes.object),
     children: PropTypes.arrayOf(PropTypes.object).isRequired,
     name: PropTypes.string.isRequired,
   }).isRequired,
