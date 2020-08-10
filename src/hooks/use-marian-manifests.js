810c3dd5fc643d72a2eeb4e5372e84a1b99667e7
Jordan Stapinski <jordan.stapinski@mongodb.com>
2020-08-10 09:40:23 -0400

    Use marian PROD

--- src/hooks/use-marian-manifests.js
+++ src/hooks/use-marian-manifests.js
@@ -1,33 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { parseMarianManifests } from '../utils/parse-marian-manifests';
-
-// TODO: remove when Marian change is in prod
-const PARSED_MANIFESTS = {
-  Atlas: { master: 'atlas-master' },
-  'Atlas Open Service Broker': { current: 'atlas-open-service-broker-current' },
-  Charts: {
-    '19.06': 'charts-19.06',
-    '19.09': 'charts-19.09',
-    '19.12': 'charts-19.12',
-    current: 'charts-current',
-    master: 'charts-master',
-    'v0.10': 'charts-v0.10',
-    'v0.11': 'charts-v0.11',
-    'v0.12': 'charts-v0.12',
-    'v0.9': 'charts-v0.9',
-  },
-};
 
 export const useMarianManifests = () => {
   const [filters, setFilters] = useState({});
 
   useEffect(() => {
     async function fetchManifests() {
-      // TODO: Fix when Marian change is in production
-      // const result = await fetch('http://marian.mongodb.com/status');
-      // const jsonResult = await result.json();
-      // setFilters(parseMarianManifests(jsonResult.manifests));
-      setFilters(PARSED_MANIFESTS);
+      const result = await fetch('https://marian.mongodb.com/status');
+      const jsonResult = await result.json();
+      setFilters(parseMarianManifests(jsonResult.manifests));
     }
     fetchManifests();
   }, []);
