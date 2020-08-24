3954baf4c9c4034d23b9e29d01c06be8c84ff900
sophstad <sophie.stadler@mongodb.com>
2020-08-24 16:45:00 -0400

    Move leafygreen provider to root, as it does not depend on page

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,10 +4,10 @@ import LeafyGreenProvider from '@leafygreen-ui/leafygreen-provider';
 import { theme } from './src/theme/docsTheme';
 import DefaultLayout from './src/components/layout';
 
-export const wrapRootElement = ({ element }) => <ThemeProvider theme={theme}>{element}</ThemeProvider>;
-
-export const wrapPageElement = ({ element, props }) => (
-  <LeafyGreenProvider>
-    <DefaultLayout {...props}>{element}</DefaultLayout>
-  </LeafyGreenProvider>
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>
+    <LeafyGreenProvider>{element}</LeafyGreenProvider>
+  </ThemeProvider>
 );
+
+export const wrapPageElement = ({ element, props }) => <DefaultLayout {...props}>{element}</DefaultLayout>;
