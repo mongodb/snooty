ab5766c134546079b24911da2c33a6810cb76233
sophstad <sophie.stadler@mongodb.com>
2020-08-24 16:01:35 -0400

    Populate page title on server

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import { theme } from './src/theme/docsTheme';
+import DefaultLayout from './src/components/layout';
 
 export const wrapRootElement = ({ element }) => <ThemeProvider theme={theme}>{element}</ThemeProvider>;
+
+export const wrapPageElement = ({ element, props }) => <DefaultLayout {...props}>{element}</DefaultLayout>;
