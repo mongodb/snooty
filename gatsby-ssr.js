d51c342868eb4d23279b9a888074582137ad47b1
Sophie Stadler <sophstad@users.noreply.github.com>
2020-08-31 18:08:48 -0400

    Miscellaneous improvements (#265)

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
+import { renderStylesToString } from '@leafygreen-ui/emotion';
 import LeafyGreenProvider from '@leafygreen-ui/leafygreen-provider';
+import { renderToString } from 'react-dom/server';
 import { theme } from './src/theme/docsTheme';
 import DefaultLayout from './src/components/layout';
+
+// Support SSR for LeafyGreen components
+// https://github.com/mongodb/leafygreen-ui/tree/master/packages/emotion#server-side-rendering
+export const replaceRenderer = ({ replaceBodyHTMLString, bodyComponent }) =>
+  replaceBodyHTMLString(renderStylesToString(renderToString(bodyComponent)));
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>
