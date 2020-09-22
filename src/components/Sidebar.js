80af986cc01f55782d14553040ae377f3773a7d9
Sophie Stadler <sophstad@users.noreply.github.com>
2020-09-22 10:56:34 -0400

    DOP-1006: Fix version selector and title in dropdown (#275)

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import Link from './Link';
 import VersionDropdown from './VersionDropdown';
 import TableOfContents from './TableOfContents';
@@ -7,20 +7,30 @@ import style from '../styles/sidebar.module.css';
 
 const Sidebar = ({ slug, publishedBranches, toctreeData, toggleLeftColumn }) => {
   const { title } = toctreeData;
+
+  // Calculate height of the fixed header so that the TOC can occupy the rest of the vertical space.
+  const [fixedHeight, setFixedHeight] = useState(0);
+  const fixedHeading = useRef(null);
+  useLayoutEffect(() => {
+    setFixedHeight(fixedHeading.current.clientHeight);
+  }, []);
+
   return (
     <aside className={`sidebar ${style.sidebar}`} id="sidebar">
       <div className={`sphinxsidebar ${style.sphinxsidebar}`} id="sphinxsidebar">
         <div id="sphinxsidebarwrapper" className="sphinxsidebarwrapper">
-          <span className="closeNav" id="closeNav" onClick={toggleLeftColumn} style={{ cursor: 'pointer' }}>
-            Close ×
-          </span>
-          <h3>
-            <Link className="index-link" to="/">
-              {formatText(title)}
-            </Link>
-          </h3>
-          {publishedBranches && <VersionDropdown slug={slug} publishedBranches={publishedBranches} />}
-          <TableOfContents toctreeData={toctreeData} />
+          <div ref={fixedHeading}>
+            <span className="closeNav" id="closeNav" onClick={toggleLeftColumn} style={{ cursor: 'pointer' }}>
+              Close ×
+            </span>
+            <h3>
+              <Link className="index-link" to="/">
+                {formatText(title)}
+              </Link>
+            </h3>
+            {publishedBranches && <VersionDropdown slug={slug} publishedBranches={publishedBranches} />}
+          </div>
+          <TableOfContents toctreeData={toctreeData} height={fixedHeight} />
         </div>
       </div>
     </aside>
