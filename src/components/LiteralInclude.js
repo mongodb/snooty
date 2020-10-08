785c6ad9ea8a56329f97c089cc70c2d60c053bb0
Sophie Stadler <sophstad@users.noreply.github.com>
2020-10-08 13:06:28 -0400

    DOP-1529 (1): Simplify tabs and remove pill strips (#293)

--- src/components/LiteralInclude.js
+++ src/components/LiteralInclude.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import ComponentFactory from './ComponentFactory';
 
 const LiteralInclude = ({ nodeData: { children }, ...rest }) => {
-  return children.map(child => <ComponentFactory {...rest} nodeData={child} />);
+  return children.map((child, i) => <ComponentFactory {...rest} key={i} nodeData={child} />);
 };
 
 LiteralInclude.propTypes = {
   nodeData: PropTypes.shape({
-    children: PropTypes.arrayOf(PropTypes.node).isRequired,
+    children: PropTypes.arrayOf(PropTypes.object).isRequired,
   }).isRequired,
 };
 
