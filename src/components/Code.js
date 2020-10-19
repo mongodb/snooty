7671770a98c15e3ff4ae831b8abc502433653f48
rayangler <rayangler@users.noreply.github.com>
2020-10-19 13:27:18 -0400

    DOP-1604: Allow code blocks to render with caption (#299)

--- src/components/Code.js
+++ src/components/Code.js
@@ -40,7 +40,7 @@ const getLanguage = lang => {
 };
 
 const Code = ({
-  nodeData: { copyable, emphasize_lines: emphasizeLines, lang, linenos, value },
+  nodeData: { copyable, caption, emphasize_lines: emphasizeLines, lang, linenos, value },
   uriWriter: { cloudURI, localURI },
 }) => {
   const { activeTabs } = useContext(TabContext);
@@ -72,6 +72,8 @@ const Code = ({
         highlightLines={emphasizeLines}
         language={getLanguage(lang)}
         showLineNumbers={linenos}
+        showWindowChrome={caption ? true : false}
+        chromeTitle={caption ? caption : ''}
       >
         {code}
       </CodeBlock>
@@ -86,6 +88,7 @@ Code.propTypes = {
     lang: PropTypes.string,
     linenos: PropTypes.bool,
     value: PropTypes.string.isRequired,
+    caption: PropTypes.string,
   }).isRequired,
   uriWriter: PropTypes.shape({
     cloudURI: PropTypes.object,
