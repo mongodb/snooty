07dd458a85053da74781c16c90d6e8b9338edc16
Sophie Stadler <sophstad@users.noreply.github.com>
2020-10-22 10:08:36 -0400

    DOP-1530: Update versionmodified directives (#302)

--- src/components/VersionChanged.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import VersionModified from './VersionModified';
-
-const VersionChanged = props => <VersionModified {...props} introText="Changed in version" />;
-
-export default VersionChanged;
