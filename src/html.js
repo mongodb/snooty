82bb5216c509e9e20bcbd994aadc77a4b61ecbfa
Sophie Stadler <sophstad@users.noreply.github.com>
2020-11-06 10:52:01 -0500

    Remove segment script (#314)

--- src/html.js
+++ src/html.js
@@ -38,7 +38,6 @@ const HTML = ({ body, bodyAttributes, headComponents, htmlAttributes, preBodyCom
       {headComponents}
     </head>
     <body {...bodyAttributes}>
-      <script async src={withPrefix('scripts/segment.js')} />
       {preBodyComponents}
       {/* eslint-disable-next-line react/no-danger */}
       <div key="body" id="___gatsby" dangerouslySetInnerHTML={{ __html: body }} />
