3d85c7c4ef07ec5b31dd05b51b8c53208c31a6f8
rayangler <raymund.rodriguez@mongodb.com>
2020-11-11 13:06:22 -0500

    Update passing children as array for landing

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -65,7 +65,7 @@ const DefaultLayout = props => {
       <Global styles={globalCSS} />
       <SiteMetadata siteTitle={title} />
       <TabProvider selectors={getNestedValue(['ast', 'options', 'selectors'], page)}>
-        <Template {...props}>{Array.isArray(children) ? children : [children]}</Template>
+        <Template {...props}>{template === 'landing' ? [children] : children}</Template>
       </TabProvider>
       <Navbar />
     </>
