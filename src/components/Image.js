5c4d09b89aff0848d51a4c261d7d7850d288d13a
mango-db <Claire.Goeckner-Wald@mongodb.com>
2020-12-03 18:02:09 -0500

    DOP-1579: Add bordered-figure styling to Image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withPrefix } from 'gatsby';
+import { css } from '@emotion/core';
 import { getNestedValue } from '../utils/get-nested-value';
 
 export default class Image extends Component {
@@ -45,6 +46,13 @@ export default class Image extends Component {
       ? `align-${getNestedValue(['options', 'align'], nodeData)}`
       : '';
 
+    const hasBorder = getNestedValue(['options', 'border'], nodeData);
+    const border = css`
+      border: 0.5px solid #babdbe;
+      width: 100%;
+      border-radius: 4px;
+    `;
+
     const buildStyles = () => {
       const { height, width } = this.state;
       return {
@@ -62,6 +70,7 @@ export default class Image extends Component {
         style={nodeData.options ? buildStyles() : {}}
         onLoad={this.handleLoad}
         ref={this.imgRef}
+        css={hasBorder ? border : ''}
       />
     );
   }
