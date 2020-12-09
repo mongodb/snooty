b5bd62cd9b05b13dd518f6e1f9ede3f4846682ac
Nick Larew <nick.larew@mongodb.com>
2020-12-09 15:31:49 -0500

    Allow users with adblockers to open the feedback widget

--- src/utils/segment.js
+++ src/utils/segment.js
@@ -1,13 +1,21 @@
 export function getSegmentUserId() {
-  const user = window.analytics.user();
-  // Get the user's persistent Segment ID, if they have one
-  const segmentId = user.id();
-  return {
-    // If the user doesn't have a persistent Segment ID then they're anonymous
-    isAnonymous: !Boolean(segmentId),
-    // Return the Persistent ID or Anonymous ID as a string
-    id: segmentId ? segmentId.toString() : user.anonymousId().toString(),
-  };
+  if (window.analytics) {
+    // Get the user's persistent Segment ID, if they have one
+    const user = window.analytics.user();
+    const segmentId = user.id();
+    return {
+      // If the user doesn't have a persistent Segment ID then they're anonymous
+      isAnonymous: !Boolean(segmentId),
+      // Return the Persistent ID or Anonymous ID as a string
+      id: segmentId ? segmentId.toString() : user.anonymousId().toString(),
+    };
+  } else {
+    // The user has an ad/tracker blocker enabled
+    return {
+      isAnonymous: true,
+      id: '<notrack>',
+    };
+  }
 }
 
 export function sendAnalytics(eventName, eventObj) {
@@ -18,7 +26,10 @@ export function sendAnalytics(eventName, eventObj) {
     } else {
       eventObj.segmentUID = id;
     }
-    window.analytics.track(eventName, eventObj);
+    if (window.analytics) {
+      // If the user doesn't block trackers, track the event
+      window.analytics.track(eventName, eventObj);
+    }
   } catch (err) {
     console.error(err);
   }
