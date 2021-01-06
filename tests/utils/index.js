5ea498924cf571c7dabaeb4731daf92f4e8e89db
Sophie Stadler <sophstad@users.noreply.github.com>
2021-01-06 10:01:34 -0500

    DOP-1749: Fix walkabout, refactor screen size hooks (#328)

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -1,5 +1,5 @@
 import { act } from 'react-dom/test-utils';
-import { Context as ResponsiveContext } from 'react-responsive';
+import { theme } from '../../src/theme/docsTheme';
 
 export async function tick({ waitFor = 0, wrapper } = {}) {
   await act(async () => {
@@ -25,17 +25,26 @@ export function mockSegmentAnalytics() {
   };
 }
 
-export function withScreenSize(size) {
-  const screenSizes = {
-    // Simulate a 1920px x 1080px desktop monitor
-    desktop: { width: 1920, height: 1080 },
-    // Simulate a 10.5" iPad Pro (1668px x 2224px @ 2x scale)
-    'ipad-pro': { width: 834, height: 1112 },
-    // Simulate an iPhone X (1125px x 2436px @ 3x scale)
-    'iphone-x': { width: 375, height: 812 },
-  };
-  return {
-    wrappingComponent: ResponsiveContext.Provider,
-    wrappingComponentProps: { value: screenSizes[size] },
-  };
-}
+const {
+  screenSize: { largeAndUp, upToLarge, upToSmall },
+} = theme;
+
+const setMatchMedia = (...queries) => {
+  window.matchMedia = media => ({
+    addListener: () => {},
+    removeListener: () => {},
+    matches: queries.some(query => media === query),
+  });
+};
+
+export const setMobile = () => {
+  setMatchMedia(upToSmall, upToLarge);
+};
+
+export const setTablet = () => {
+  setMatchMedia(upToLarge);
+};
+
+export const setDesktop = () => {
+  setMatchMedia(largeAndUp);
+};
