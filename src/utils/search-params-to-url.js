78881df6503de12a67f5095688492a12316b1d6f
rayangler <raymund.rodriguez@mongodb.com>
2021-01-13 11:43:57 -0500

    Fix test, update constants

--- src/utils/search-params-to-url.js
+++ src/utils/search-params-to-url.js
@@ -1,8 +1,8 @@
+import { DOCS_URL, MARIAN_URL } from '../constants';
+
 // Search helper function to generate marian URL from params and filters
-const MARIAN_URL = 'https://marian.mongodb.com/';
-const DOCS_URL = 'https://docs.mongodb.com/';
 export const searchParamsToURL = (searchQuery, searchFilters, isMarian = true) => {
   const queryParams = `?q=${searchQuery}${searchFilters ? `&searchProperty=${searchFilters}` : ''}`;
   const url = isMarian ? MARIAN_URL : DOCS_URL;
-  return `${url}search${queryParams}`;
+  return `${url}/search${queryParams}`;
 };
