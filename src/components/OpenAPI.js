8960154cb7b91ba1344b140dfb11589a22026f51
rayangler <raymund.rodriguez@mongodb.com>
2021-01-22 17:57:05 -0500

    DOP-1896: Render OpenAPI content using Swagger React UI

--- src/components/OpenAPI.js
+++ src/components/OpenAPI.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
 import ComponentFactory from './ComponentFactory';
+import { getNestedValue } from '../utils/get-nested-value';
 
-const OpenAPI = ({ nodeData: { children }, ...rest }) => (
-  <>
-    {children.map((node, i) => (
-      <ComponentFactory {...rest} key={i} nodeData={node} />
-    ))}
-  </>
-);
+const OpenAPI = ({ nodeData: { children, options = {} }, ...rest }) => {
+  const swagger_parse = getNestedValue(['swagger-parse'], options);
+  if (swagger_parse) {
+    if (children.length !== 1) {
+      return null;
+    }
+    const spec = JSON.parse(children[0].value);
+    return <SwaggerUI spec={spec} />;
+  }
+
+  return (
+    <>
+      {children.map((node, i) => (
+        <ComponentFactory {...rest} key={i} nodeData={node} />
+      ))}
+    </>
+  );
+};
 
 OpenAPI.propTypes = {
   nodeData: PropTypes.shape({
     children: PropTypes.arrayOf(PropTypes.object).isRequired,
+    options: PropTypes.shape({
+      swagger_parse: PropTypes.bool,
+    }).isRequired,
   }).isRequired,
 };
 
