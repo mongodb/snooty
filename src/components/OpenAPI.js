d50b1bb54c7722c95dae6b3af81d85a9a0655f27
rayangler <27821750+rayangler@users.noreply.github.com>
2021-02-17 09:52:53 -0500

    DOP-1896: Render OpenAPI content using Redoc (#338)

--- src/components/OpenAPI.js
+++ src/components/OpenAPI.js
@@ -1,18 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { RedocStandalone } from 'redoc';
 import ComponentFactory from './ComponentFactory';
 
-const OpenAPI = ({ nodeData: { children }, ...rest }) => (
-  <>
-    {children.map((node, i) => (
-      <ComponentFactory {...rest} key={i} nodeData={node} />
-    ))}
-  </>
-);
+const OpenAPI = ({ nodeData: { argument, children, options = {} }, ...rest }) => {
+  const usesRST = options?.['uses-rst'];
+
+  if (usesRST) {
+    return (
+      <>
+        {children.map((node, i) => (
+          <ComponentFactory {...rest} key={i} nodeData={node} />
+        ))}
+      </>
+    );
+  }
+
+  // Check for JSON string spec first
+  const spec = children[0]?.value;
+  const specOrUrl = spec ? JSON.parse(spec) : argument[0]?.refuri;
+  if (!specOrUrl) {
+    return null;
+  }
+
+  return (
+    <RedocStandalone
+      options={{
+        maxDisplayedEnumValues: 5,
+      }}
+      spec={specOrUrl}
+      specUrl={specOrUrl}
+    />
+  );
+};
 
 OpenAPI.propTypes = {
   nodeData: PropTypes.shape({
+    argument: PropTypes.arrayOf(PropTypes.object).isRequired,
     children: PropTypes.arrayOf(PropTypes.object).isRequired,
+    options: PropTypes.shape({
+      uses_rst: PropTypes.bool,
+    }).isRequired,
   }).isRequired,
 };
 
