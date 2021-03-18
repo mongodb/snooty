f81df7828d117680e70e390862a8bbf699ab7448
rayangler <raymund.rodriguez@mongodb.com>
2021-03-18 16:23:26 -0400

    Fix import and comment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { css } from '@emotion/core';
+import styled from '@emotion/styled';
 import Loadable from '@loadable/component';
 import Navbar from './Navbar';
-import styled from '@emotion/styled';
 
 // Prevents Stitch functions in Banner from erroring when starting local development
 const Banner = Loadable(() => import('./Banner'));
 
 // Set Header children to be the highest z-index. Assigning this to the Header component itself
-// causes the search dropdown to appear underneath the header instead of on top of the navbar.
+// causes the search dropdown to appear underneath the navbar instead of on top of it.
 const setHighestZIndexCss = css`
   position: relative;
   z-index: 9999;
