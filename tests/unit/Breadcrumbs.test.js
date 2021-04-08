7b12d80f9753f22591653856ebe83e351ae6a9a6
Sophie Stadler <sophstad@users.noreply.github.com>
2021-04-08 13:29:46 -0400

    DOP-1975: Fix breadcrumb schema bug (#380)

--- tests/unit/Breadcrumbs.test.js
+++ tests/unit/Breadcrumbs.test.js
@@ -5,11 +5,11 @@ import Breadcrumbs from '../../src/components/Breadcrumbs';
 import mockData from './data/Breadcrumbs.test.json';
 
 it('renders correctly', () => {
-  const tree = shallow(<Breadcrumbs parentPaths={mockData} siteTitle="MongoDB Compass" />);
+  const tree = shallow(<Breadcrumbs parentPaths={mockData} siteTitle="MongoDB Compass" slug="documents/view" />);
   expect(tree).toMatchSnapshot();
 });
 
 it('fails gracefully', () => {
-  const tree = shallow(<Breadcrumbs parentPaths={null} siteTitle="untitled" />);
+  const tree = shallow(<Breadcrumbs parentPaths={null} siteTitle="untitled" slug="/" />);
   expect(tree).toMatchSnapshot();
 });
