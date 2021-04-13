885cdd818cc9f8facd58e011e099cf84a03b62c5
rayangler <raymund.rodriguez@mongodb.com>
2021-04-13 11:20:44 -0400

    Fix test

--- tests/unit/ProductsList.test.js
+++ tests/unit/ProductsList.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { matchers } from 'jest-emotion';
 import ProductsList from '../../src/components/ProductsList';
 
 const mockProducts = [
@@ -17,19 +18,16 @@ jest.mock('../../src/hooks/useAllProducts', () => ({
   useAllProducts: () => mockProducts,
 }));
 
+expect.extend(matchers);
+
 describe('ProductsList', () => {
   it('renders with products', async () => {
     let wrapper = mount(<ProductsList />);
     expect(wrapper).toMatchSnapshot();
 
-    // Show products list
+    // Display products list
     wrapper.find('ProductsListHeading').simulate('click');
-    const products = wrapper.find('a');
-
-    expect(products).toHaveLength(2);
-    expect(products.at(0).props()).toHaveProperty('href', 'https://www.docs.mongodb.com/drivers/');
-    expect(products.at(0).children().text()).toEqual('MongoDB Drivers');
-    expect(products.at(1).props()).toHaveProperty('href', 'https://www.docs.atlas.mongodb.com/');
-    expect(products.at(1).children().text()).toEqual('MongoDB Atlas');
+    expect(wrapper.find('Products').props()).toHaveProperty('isOpen', true);
+    expect(wrapper.find('Products')).not.toHaveStyleRule('display', 'none');
   });
 });
