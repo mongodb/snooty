33e1e4141e2e9708a4160cb26d9eb889c05e3adf
mango-db <Claire.Goeckner-Wald@mongodb.com>
2021-04-18 23:56:27 -0400

    DOP-2063: Original Card changes

--- src/components/landing/Card.js
+++ src/components/landing/Card.js
@@ -4,6 +4,7 @@ import { withPrefix } from 'gatsby';
 import styled from '@emotion/styled';
 import Box from '@leafygreen-ui/box';
 import LeafyGreenCard from '@leafygreen-ui/card';
+import { theme } from '../../theme/docsTheme';
 import ComponentFactory from '../ComponentFactory';
 import Link from '../Link';
 import Tag from '../Tag';
@@ -12,16 +13,24 @@ const StyledCard = styled(LeafyGreenCard)`
   display: flex;
   flex-direction: column;
   height: 100%;
-  padding: ${({ theme }) => `${theme.size.large}`};
+  padding: ${theme.size.large};
+`;
+
+const CompactCard = styled(LeafyGreenCard)`
+  padding: ${theme.size.large};
 `;
 
 const CardIcon = styled('img')`
-  width: ${({ theme }) => `${theme.size.medium}`};
+  width: ${theme.size.medium};
+`;
+
+const CompactIcon = styled('img')`
+  width: 48px;
 `;
 
 const H4 = styled('h4')`
   letter-spacing: 0.5px;
-  margin: ${({ theme }) => `${theme.size.medium} 0 ${theme.size.small} 0`};
+  margin: ${theme.size.medium} 0 ${theme.size.small} 0;
 `;
 
 const CTA = styled(Link)`
@@ -37,21 +46,41 @@ const FlexTag = styled(Tag)`
 const Card = ({
   nodeData: {
     children,
-    options: { cta, headline, icon, 'icon-alt': iconAlt, tag, url },
+    options: { cta, headline, icon, style, 'icon-alt': iconAlt, tag, url },
   },
-}) => (
-  <Box href={url}>
-    <StyledCard>
-      {icon && <CardIcon src={withPrefix(icon)} alt={iconAlt} />}
-      {tag && <FlexTag text={tag} />}
-      <H4>{headline}</H4>
-      {children.map((child, i) => (
-        <ComponentFactory nodeData={child} key={i} />
-      ))}
-      <CTA to={url}>{cta}</CTA>
-    </StyledCard>
-  </Box>
-);
+  page,
+}) => {
+  // CHECK ON STYLE NOT ON TEMPLATE
+  const isCompact = style === 'product-landing';
+  console.log(isCompact);
+  return (
+    <Box href={url}>
+      <>
+        {isCompact ? (
+          <CompactCard>
+            {icon && <CompactIcon src={withPrefix(icon)} alt={iconAlt} />}
+            {tag && <FlexTag text={tag} />}
+            <H4>{headline}</H4>
+            {children.map((child, i) => (
+              <ComponentFactory nodeData={child} key={i} />
+            ))}
+            <CTA to={url}>{cta}</CTA>
+          </CompactCard>
+        ) : (
+          <StyledCard>
+            {icon && <CardIcon src={withPrefix(icon)} alt={iconAlt} />}
+            {tag && <FlexTag text={tag} />}
+            <H4>{headline}</H4>
+            {children.map((child, i) => (
+              <ComponentFactory nodeData={child} key={i} />
+            ))}
+            <CTA to={url}>{cta}</CTA>
+          </StyledCard>
+        )}
+      </>
+    </Box>
+  );
+};
 
 Card.propTypes = {
   nodeData: PropTypes.shape({
--- src/components/landing/CardGroup.js
+++ src/components/landing/CardGroup.js
@@ -2,32 +2,33 @@ import React from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 import ComponentFactory from '../ComponentFactory';
+import { theme } from '../../theme/docsTheme';
 
 const getColumnValue = props => props.columns || React.Children.count(props.children);
 
 const StyledGrid = styled('div')`
   align-items: stretch;
   display: grid;
-  grid-column: 1/-1 !important;
-  grid-column-gap: ${({ theme }) => theme.size.medium};
-  grid-row-gap: ${({ theme }) => theme.size.medium};
+  grid-column: 1 / -1 !important;
+  grid-column-gap: ${theme.size.medium};
+  grid-row-gap: ${theme.size.medium};
   grid-template-columns: ${props => `repeat(${getColumnValue(props)}, 1fr)`};
-  margin: ${({ theme }) => theme.size.large} 0;
+  margin: ${theme.size.large} 0;
 
-  @media ${({ theme }) => theme.screenSize.upToLarge} {
+  @media ${theme.screenSize.upToLarge} {
     grid-template-columns: repeat(2, 1fr);
   }
 
-  @media ${({ theme }) => theme.screenSize.upToMedium} {
-    grid-gap: ${({ theme }) => `calc(${theme.size.medium} * 0.75)`};
+  @media ${theme.screenSize.upToMedium} {
+    grid-gap: ${`calc(${theme.size.medium} * 0.75)`};
     grid-template-columns: ${({ children, theme }) =>
       `calc(${theme.size.medium} / 2) repeat(${React.Children.count(children)}, calc(75% - calc( 2 * ${
         theme.size.medium
       }))) calc(${theme.size.medium} / 2)`};
     grid-template-rows: minmax(150px, 1fr);
-    margin: ${({ theme }) => theme.size.medium} 0;
+    margin: ${theme.size.medium} 0;
     overflow-x: scroll;
-    padding-bottom: ${({ theme }) => `calc(${theme.size.medium} / 2)`};
+    padding-bottom: ${`calc(${theme.size.medium} / 2)`};
     scroll-snap-type: x proximity;
 
     &:before,
@@ -37,19 +38,49 @@ const StyledGrid = styled('div')`
   }
 `;
 
+const LandingGrid = styled('div')`
+  display: grid;
+  grid-column-gap: ${theme.size.default};
+  grid-row-gap: ${theme.size.default};
+  grid-template-columns: repeat(3, 1fr);
+  margin: ${theme.size.large} 0;
+
+  @media ${theme.screenSize.upToLarge} {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media ${theme.screenSize.upToMedium} {
+    grid-template-columns: 1fr;
+  }
+`;
+
 const CardGroup = ({
   nodeData: {
     children,
     options: { columns },
   },
+  page,
   ...rest
-}) => (
-  <StyledGrid columns={columns} noMargin={true}>
-    {children.map(child => (
-      <ComponentFactory nodeData={child} {...rest} />
-    ))}
-  </StyledGrid>
-);
+}) => {
+  const isProductLanding = page?.options?.template === 'product-landing';
+  return (
+    <>
+      {isProductLanding ? (
+        <LandingGrid>
+          {children.map(child => (
+            <ComponentFactory nodeData={child} page={page} {...rest} />
+          ))}
+        </LandingGrid>
+      ) : (
+        <StyledGrid columns={columns} noMargin={true}>
+          {children.map(child => (
+            <ComponentFactory nodeData={child} {...rest} />
+          ))}
+        </StyledGrid>
+      )}
+    </>
+  );
+};
 
 CardGroup.propTypes = {
   nodeData: PropTypes.shape({
