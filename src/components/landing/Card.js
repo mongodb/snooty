5a7b84c5a82c6664c98231d0378843f1ee159f55
sophstad <sophie.stadler@mongodb.com>
2021-04-22 10:43:40 -0400

    Add compact card config

--- src/components/landing/Card.js
+++ src/components/landing/Card.js
@@ -4,6 +4,7 @@ import { withPrefix } from 'gatsby';
 import styled from '@emotion/styled';
 import LeafyGreenCard from '@leafygreen-ui/card';
 import ComponentFactory from '../ComponentFactory';
+import ConditionalWrapper from '../ConditionalWrapper';
 import Link from '../Link';
 import Tag from '../Tag';
 
@@ -12,6 +13,11 @@ const StyledCard = styled(LeafyGreenCard)`
   flex-direction: column;
   height: 100%;
   padding: ${({ theme }) => `${theme.size.large}`};
+
+  p:last-of-type {
+    margin-top: auto;
+    margin-bottom: 0;
+  }
 `;
 
 const CardIcon = styled('img')`
@@ -20,39 +26,63 @@ const CardIcon = styled('img')`
 
 const H4 = styled('h4')`
   letter-spacing: 0.5px;
-  margin: ${({ theme }) => `${theme.size.medium} 0 ${theme.size.small} 0`};
+  margin: ${({ isExtraCompact, theme }) =>
+    isExtraCompact ? `0 0 ${theme.size.small}` : `${theme.size.medium} 0 ${theme.size.small} 0`};
 `;
 
 const CTA = styled(Link)`
   font-weight: bold;
-  margin-top: auto;
-  margin-bottom: 0;
 `;
 
 const FlexTag = styled(Tag)`
   margin-right: auto;
 `;
 
+const CompactCard = styled(StyledCard)`
+  align-items: flex-start;
+  display: flex;
+  flex-direction: row;
+`;
+
+const CompactWrapper = styled('div')`
+  margin-left: ${({ theme }) => theme.size.default};
+`;
+
 const Card = ({
+  isCompact,
+  isExtraCompact,
+  nodeData,
   nodeData: {
     children,
     options: { cta, headline, icon, 'icon-alt': iconAlt, tag, url },
   },
-}) => (
-  <StyledCard
-    onClick={() => {
-      window.location.href = url;
-    }}
-  >
-    {icon && <CardIcon src={withPrefix(icon)} alt={iconAlt} />}
-    {tag && <FlexTag text={tag} />}
-    <H4>{headline}</H4>
-    {children.map((child, i) => (
-      <ComponentFactory nodeData={child} key={i} />
-    ))}
-    <CTA to={url}>{cta}</CTA>
-  </StyledCard>
-);
+}) => {
+  const Card = isCompact || isExtraCompact ? CompactCard : StyledCard;
+  return (
+    <Card
+      onClick={() => {
+        window.location.href = url;
+      }}
+    >
+      {icon && <CardIcon src={withPrefix(icon)} alt={iconAlt} />}
+      <ConditionalWrapper
+        condition={isCompact || isExtraCompact}
+        wrapper={(children) => <CompactWrapper>{children}</CompactWrapper>}
+      >
+        {tag && <FlexTag text={tag} />}
+        <H4 isExtraCompact={isExtraCompact}>{headline}</H4>
+        {children.map((child, i) => (
+          <ComponentFactory nodeData={child} key={i} />
+        ))}
+        {cta && (
+          <p>
+            <CTA to={url}>{cta}</CTA>
+          </p>
+        )}
+      </ConditionalWrapper>
+    </Card>
+  );
+};
 
 Card.propTypes = {
   nodeData: PropTypes.shape({
