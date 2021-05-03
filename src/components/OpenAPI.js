e0b91b44a59b47d7ee64efc441826ecf4bb4e773
rayangler <raymund.rodriguez@mongodb.com>
2021-05-03 15:28:10 -0400

    Attempt using theming on redoc component

--- src/components/OpenAPI.js
+++ src/components/OpenAPI.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { RedocStandalone } from 'redoc';
+import { RedocStandalone, styled } from 'redoc';
 import ComponentFactory from './ComponentFactory';
+
+const StyledRedocComponent = styled('div')`
+  .token.string {
+    color: #000000;
+  }
+
+  span.token.boolean {
+    color: #00ff00;
+  }
+`;
 
 const OpenAPI = ({ nodeData: { argument, children, options = {} }, ...rest }) => {
   const usesRST = options?.['uses-rst'];
@@ -24,13 +34,60 @@ const OpenAPI = ({ nodeData: { argument, children, options = {} }, ...rest }) =>
   }
 
   return (
-    <RedocStandalone
-      options={{
-        maxDisplayedEnumValues: 5,
-      }}
-      spec={specOrUrl}
-      specUrl={specOrUrl}
-    />
+    <StyledRedocComponent>
+      <RedocStandalone
+        options={{
+          maxDisplayedEnumValues: 5,
+          hideLoading: true,
+          theme: {
+            typography: {
+              fontSize: '16px',
+              fontFamily: 'Akzidenz',
+              headings: 'Akzidenz',
+              code: {
+                fontSize: '14px',
+                fontFamily: 'Source Code Pro',
+                color: '#800020',
+                backgroundColor: '#FFFFFF',
+              },
+              links: {
+                color: '#007CAD',
+                visited: '#1A567E',
+              },
+            },
+            sidebar: {
+              width: '268px',
+              backgroundColor: '#F9FBFA',
+              textColor: '#21313C',
+              activeTextColor: '#0B3B35',
+            },
+            rightPanel: {
+              backgroundColor: '#061621',
+            },
+            colors: {
+              responses: {
+                success: {
+                  color: 'black',
+                  backgroundColor: 'red',
+                  tabTextColor: 'blue',
+                },
+              },
+              warning: {
+                main: 'green',
+              },
+              http: {
+                get: 'pink',
+              },
+            },
+            codeBlock: {
+              backgroundColor: 'orange',
+            },
+          },
+        }}
+        spec={specOrUrl}
+        specUrl={specOrUrl}
+      />
+    </StyledRedocComponent>
   );
 };
 
