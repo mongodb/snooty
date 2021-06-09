40e01f16e09291f4d11b84ca305877ec76e1432f
mango-db <Claire.Goeckner-Wald@mongodb.com>
2021-06-09 15:06:45 -0400

    DOP-2249: Update test

--- tests/unit/StepYAML.test.js
+++ tests/unit/StepYAML.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Step from '../../src/components/StepYAML';
+import StepYAML from '../../src/components/StepYAML';
 
 // data for this component
 import mockData from './data/Step.test.json';
 
 it('renders correctly', () => {
-  const tree = shallow(<Step nodeData={mockData} />);
+  const tree = shallow(<StepYAML nodeData={mockData} />);
   expect(tree).toMatchSnapshot();
 });
