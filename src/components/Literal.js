b24af55d57b21e1df0a0da238aaa62c7a018415c
Carolina Obregon <carolinao@Carolinas-MacBook-Pro.local>
2021-06-11 15:45:36 -0400

    Changed css class to emotion styled code in Literal component

--- src/components/Literal.js
+++ src/components/Literal.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ComponentFactory from './ComponentFactory';
+import styled from '@emotion/styled';
 import { InlineCode } from '@leafygreen-ui/typography';
+import ComponentFactory from './ComponentFactory';
+
+const StyledInlineCode = styled(InlineCode)`
+  /* Unset font size so it inherits it from its context */
+  font-size: unset;
+`;
 
 const Literal = ({ nodeData: { children } }) => (
-  <InlineCode className="literal-comp">
+  <StyledInlineCode>
     {children.map((node, i) => (
       <ComponentFactory nodeData={node} key={i} />
     ))}
-  </InlineCode>
+  </StyledInlineCode>
 );
 
 Literal.propTypes = {
