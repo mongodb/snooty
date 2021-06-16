18bb0eaa8cc19e71cd316eebec3eee0c03c6ee1a
mango-db <Claire.Goeckner-Wald@mongodb.com>
2021-06-16 18:51:22 -0400

    DOP-2215: Move landing-domain components and tests

--- tests/unit/Procedure.test.js
+++ tests/unit/Procedure.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Procedure from '../../src/components/landing/Procedure';
+import Procedure from '../../src/components/Procedure';
 
 // data for this component
-import mockData from './data/landing/Procedure.test.json';
+import mockData from './data/Procedure.test.json';
 
 it('renders correctly', () => {
   const tree = shallow(<Procedure nodeData={mockData} />);
