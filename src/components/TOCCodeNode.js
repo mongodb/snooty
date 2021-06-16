af0ae16a4a838b21b30120d93138c098610bbb4f
carooobregon <carooobregon@gmail.com>
2021-06-16 11:42:43 -0400

    Changed span to code element, added emotion style

--- src/components/TOCCodeNode.js
+++ src/components/TOCCodeNode.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 
-const styleTOCCodeNode = {
-  fontFamily: 'Source Code Pro',
-};
+const StyledTOCCode = styled('code')`
+  font-family: 'Source Code Pro';
+  color: #333;
+`;
 
-const TOCCodeNode = ({ nodeData: { children } }) => <span style={styleTOCCodeNode}>{children[0].value}</span>;
+const TOCCodeNode = ({ nodeData: { children } }) => <StyledTOCCode>{children[0].value}</StyledTOCCode>;
 
 TOCCodeNode.propTypes = {
   nodeData: PropTypes.shape({
