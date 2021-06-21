a8883622f759af7368f5f6eebd6d2bf332f41284
mango-db <Claire.Goeckner-Wald@mongodb.com>
2021-06-21 15:16:37 -0400

    DOP-2215: Update Card test, add CardGroup test

--- tests/unit/Card.test.js
+++ tests/unit/Card.test.js
@@ -1,45 +1,11 @@
 import React from 'react';
-import { mount, render } from 'enzyme';
-import Card from '../../src/components/LandingPage/Card';
+import { shallow } from 'enzyme';
+import Card from '../../src/components/Card';
 
-import cardData from './data/Card.test.json';
-import multiCardData from './data/MultiCard.test.json';
-import mockGuidesMetadata from './data/guidesPageMetadata.json';
+// data for this component
+import mockData from './data/Card.test.json';
 
-const mountCard = ({ card }) => mount(<Card card={card} guidesMetadata={mockGuidesMetadata} />);
-
-const renderCard = ({ card }) => render(<Card card={card} guidesMetadata={mockGuidesMetadata} />);
-
-describe('Card component', () => {
-  let wrapper;
-  let render;
-  describe('when a standard card is mounted', () => {
-    beforeAll(() => {
-      wrapper = mountCard({ card: cardData });
-      render = renderCard({ card: cardData });
-    });
-
-    it('renders correctly', () => {
-      expect(render).toMatchSnapshot();
-    });
-
-    it('displays a time estimate', () => {
-      expect(wrapper.find('.guide__time')).toHaveLength(1);
-    });
-  });
-
-  describe('when a multi card is mounted', () => {
-    beforeAll(() => {
-      wrapper = mountCard({ card: multiCardData });
-      render = renderCard({ card: multiCardData });
-    });
-
-    it('renders correctly', () => {
-      expect(render).toMatchSnapshot();
-    });
-
-    it('does not display a time estimate', () => {
-      expect(wrapper.find('.guide__time')).toHaveLength(0);
-    });
-  });
+it('renders correctly', () => {
+  const tree = shallow(<Card nodeData={mockData} />);
+  expect(tree).toMatchSnapshot();
 });
