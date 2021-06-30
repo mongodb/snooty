075f921053fdf8ca813317fe92697cc303ced96c
Cassidy Schaufele <cassidyschaufele@gmail.com>
2021-06-30 12:12:50 -0600

    Re-compartmentalize session management logic

--- src/clients/auth.js
+++ src/clients/auth.js
@@ -8,8 +8,16 @@ if (isBrowser) {
     clientId: process.env.AUTH_CLIENT_ID,
     redirectUri: process.env.REDIRECT_URI,
     issuer: process.env.AUTH_BASE_URL,
+    tokenManager: {
+      storage: 'cookie',
+    },
   });
 }
+
+export const signupWithRedirect = () => {
+  const signupUrl = `${process.env.AUTH_BASE_URL}/account/login?fromURI=https%3A%2F%2Fdocs.mongodb.com`;
+  window.location = signupUrl;
+};
 
 export const loginWithRedirect = () => {
   const loginUrl = `${process.env.AUTH_BASE_URL}/account/login?fromURI=https%3A%2F%2Fdocs.mongodb.com`;
@@ -21,11 +29,27 @@ export const logoutWithRedirect = () => {
   window.location = logoutUrl;
 };
 
+const initializeOktaApplicationSession = async () => {
+  return;
+};
+
+// Handles parsing and storage of idTokens after auth'ing via redirectUri pattern
+// If or when access tokens are needed, highly consider adding them
+// to the token manager within this function
+const authorize = async () => {
+  return authClient.token.parseFromUrl().then((data) => {
+    const { idToken } = data.tokens;
+    authClient.tokenManager.add('idToken', idToken);
+    authClient.handleLoginRedirect();
+  });
+};
+
 export const checkOktaSession = async () => {
   if (isBrowser && authClient) {
     authClient.session.exists().then(function (exists) {
       if (exists) {
         console.log('logged in');
+        initializeOktaApplicationSession();
       } else {
         console.log('logged out');
       }
@@ -36,11 +60,7 @@ export const checkOktaSession = async () => {
 export const getUserProfileFromJWT = async () => {
   if (isBrowser && authClient) {
     if (authClient.isLoginRedirect()) {
-      return authClient.token.parseFromUrl().then((data) => {
-        const { idToken } = data.tokens;
-        authClient.tokenManager.add('idToken', idToken);
-        authClient.handleLoginRedirect();
-      });
+      return authorize();
     } else {
       return authClient.tokenManager.get('idToken').then((idToken) => idToken);
     }
