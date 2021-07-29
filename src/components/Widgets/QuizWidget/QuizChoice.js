4ffa09c9807a908fa1f5d71cbe3291d628fc6930
carooobregon <carooobregon@gmail.com>
2021-07-29 15:52:44 -0400

    DOP-2367 - Refactor various QuizWidget pieces of code (#457)

---
+++ src/components/Widgets/QuizWidget/QuizChoice.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import { css, cx } from '@leafygreen-ui/emotion';
+import Card from '@leafygreen-ui/card';
+import { uiColors } from '@leafygreen-ui/palette';
+import Icon from '@leafygreen-ui/icon';
+import ComponentFactory from '../../ComponentFactory';
+import { theme } from '../../../theme/docsTheme';
+import { getPlaintext } from '../../../utils/get-plaintext';
+
+const submittedChoiceStyle = css`
+  pointer-events: none;
+  transition: unset !important;
+`;
+
+const Dot = styled('span')`
+  height: 10px;
+  width: 10px;
+  background-color: ${(props) => (props.isSelected ? `${uiColors.gray.dark3}` : `white`)};
+  border-color: black;
+  border-radius: 50%;
+  display: inline-block;
+  border-style: solid;
+  border-width: thin;
+  margin-right: ${theme.size.default};
+`;
+
+const StyledIcon = styled(Icon)`
+  margin-right: 12px;
+`;
+
+const DescriptionBody = styled('div')`
+  p {
+    padding-left: 26px;
+    margin-top: ${theme.size.small} !important;
+    margin-bottom: 0 !important;
+  }
+`;
+
+const getCardStyling = ({ isSelected, isSubmitted, isCorrect }) => css`
+  box-shadow: none;
+  margin: auto auto ${theme.size.default} auto;
+  padding: ${theme.size.default};
+  ${isSelected && `border-color: ${uiColors.black}`};
+  ${isSubmitted ? submittedChoiceStyle : `&:hover { border-color: black; }`}
+  ${isSubmitted && (isCorrect ? `border: 2px solid ${uiColors.green.base};` : `opacity: 0.5;`)}}
+`;
+
+const AnswerDescription = ({ description }) => {
+  return (
+    <DescriptionBody>
+      {description.map((node, i) => (
+        <ComponentFactory nodeData={node} key={i} />
+      ))}
+    </DescriptionBody>
+  );
+};
+
+const correctIcon = <StyledIcon glyph="Checkmark" fill={uiColors.green.base} size="small" />;
+const incorrectIcon = <StyledIcon glyph="X" fill={uiColors.gray.base} size="small" />;
+const UnsubmittedIcon = ({ isSelected }) => <Dot isSelected={isSelected} />;
+
+const ChoiceIconFactory = ({ isSubmitted, isSelected, isCorrect }) => {
+  if (isSubmitted) return isCorrect ? correctIcon : incorrectIcon;
+  else return <UnsubmittedIcon isSelected={isSelected} />;
+};
+
+const createSelectedResponseObj = (idx, isCorrect, argument) => {
+  return {
+    index: idx,
+    isCorrect: isCorrect,
+    choiceText: getPlaintext(argument),
+  };
+};
+
+const QuizChoice = ({
+  nodeData: { argument, children, options },
+  selectedResponseIdx,
+  setSelectedResponse,
+  idx,
+  isSubmitted,
+}) => {
+  const isCorrect = !!options?.['is-true'];
+  const isSelected = selectedResponseIdx === idx;
+  return (
+    <Card
+      className={cx(getCardStyling({ isSelected, isSubmitted, isCorrect }))}
+      onClick={() =>
+        !isSelected ? setSelectedResponse(createSelectedResponseObj(idx, isCorrect, argument)) : setSelectedResponse()
+      }
+    >
+      <ChoiceIconFactory isSubmitted={isSubmitted} isSelected={isSelected} isCorrect={isCorrect} />
+      {argument.map((node, i) => (
+        <ComponentFactory nodeData={node} key={i} />
+      ))}
+      {isSubmitted && children && <AnswerDescription description={children} />}
+    </Card>
+  );
+};
+
+QuizChoice.propTypes = {
+  nodeData: PropTypes.shape({
+    children: PropTypes.arrayOf(PropTypes.object).isRequired,
+  }).isRequired,
+  selectedResponseIdx: PropTypes.number,
+  setSelectedResponse: PropTypes.func,
+  idx: PropTypes.number,
+  isSubmitted: PropTypes.bool,
+};
+
+export default QuizChoice;
--- src/components/Widgets/QuizWidget/QuizWidget.js
+++ src/components/Widgets/QuizWidget/QuizWidget.js
@@ -5,10 +5,10 @@ import Button from '@leafygreen-ui/button';
 import Card from '@leafygreen-ui/card';
 import { uiColors } from '@leafygreen-ui/palette';
 import Icon from '@leafygreen-ui/icon';
-import ComponentFactory from './ComponentFactory';
-import { theme } from '../theme/docsTheme';
-import { useSiteMetadata } from '../hooks/use-site-metadata';
-import { getPlaintext } from '../utils/get-plaintext';
+import ComponentFactory from '../../ComponentFactory';
+import { theme } from '../../../theme/docsTheme';
+import { useSiteMetadata } from '../../../hooks/use-site-metadata';
+import { getPlaintext } from '../../../utils/get-plaintext';
 
 const StyledCard = styled(Card)`
   background-color: ${uiColors.gray.light3};
@@ -17,7 +17,7 @@ const StyledCard = styled(Card)`
 `;
 
 const QuizTitle = styled('p')`
-  margin: 0 0 32px 0 !important;
+  margin: 0 0 ${theme.size.large} 0 !important;
   font-weight: 500;
   font-size: 18px;
 `;
@@ -30,28 +30,26 @@ const QuizHeader = styled('div')`
 
 const QuizSubtitle = styled('p')`
   font-weight: 500;
-  font-size: 14px;
+  font-size: ${theme.fontSize.small};
   line-height: 16.59px;
   color: ${uiColors.gray.base};
   margin: 0 !important;
 `;
 
 const QuizQuestion = styled('p')`
-  margin: 8px 0 24px 0 !important;
+  margin: ${theme.size.small} 0 ${theme.size.medium} 0 !important;
 `;
 
 const StyledButton = styled(Button)`
-  font-size: 16px;
+  font-size: ${theme.fontSize.default};
 `;
 
-const QuizCompleteHeader = () => {
-  return (
-    <QuizHeader>
-      <Icon glyph="CheckmarkWithCircle" fill={uiColors.green.base} size="xlarge" />
-      <QuizTitle>Check your understanding</QuizTitle>
-    </QuizHeader>
-  );
-};
+const quizCompleteHeader = (
+  <QuizHeader>
+    <Icon glyph="CheckmarkWithCircle" fill={uiColors.green.base} size="xlarge" />
+    <QuizTitle>Check your understanding</QuizTitle>
+  </QuizHeader>
+);
 
 const QuizCompleteSubtitle = ({ question }) => {
   return (
@@ -66,12 +64,12 @@ const QuizCompleteSubtitle = ({ question }) => {
   );
 };
 
-const SubmitButton = ({ setHasSubmitted, selectedResponse, quizResponseObj }) => {
+const SubmitButton = ({ setIsSubmitted, selectedResponse, quizResponseObj }) => {
   const handleChoiceClick = useCallback(() => {
     if (selectedResponse) {
-      setHasSubmitted(true);
+      setIsSubmitted(true);
     }
-  }, [setHasSubmitted, selectedResponse]);
+  }, [setIsSubmitted, selectedResponse]);
 
   return (
     <StyledButton onClick={handleChoiceClick} variant="default">
@@ -92,13 +90,13 @@ const createQuizResponseObj = (question, quizId, selectedResponse, project) => {
 const QuizWidget = ({ nodeData: { children, options } }) => {
   const [question, ...choices] = children;
   const [selectedResponse, setSelectedResponse] = useState();
-  const [hasSubmitted, setHasSubmitted] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const quizId = options?.['quiz-id'];
   const { project } = useSiteMetadata();
   return (
     question?.type === 'paragraph' && (
       <StyledCard>
-        <QuizCompleteHeader />
+        {quizCompleteHeader}
         <QuizCompleteSubtitle question={question.children} />
         {choices.map((node, i) => (
           <ComponentFactory
@@ -107,12 +105,12 @@ const QuizWidget = ({ nodeData: { children, options } }) => {
             idx={i}
             selectedResponseIdx={selectedResponse?.index}
             setSelectedResponse={setSelectedResponse}
-            hasSubmitted={hasSubmitted}
+            isSubmitted={isSubmitted}
           />
         ))}
-        {!hasSubmitted && (
+        {!isSubmitted && (
           <SubmitButton
-            setHasSubmitted={setHasSubmitted}
+            setIsSubmitted={setIsSubmitted}
             selectedResponse={selectedResponse}
             quizResponseObj={createQuizResponseObj(question, quizId, selectedResponse, project)}
           />
