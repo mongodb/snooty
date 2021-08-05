f0c022ce2d9dabc87a568af276c99993a6f8bb51
Cassidy Schaufele <cassidyschaufele@gmail.com>
2021-08-05 08:30:58 -0600

    Bump consistentnav package version, add initial Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import loadable from '@loadable/component';
+
 import PropTypes from 'prop-types';
 const FeedbackFooter = loadable(() => import('./Widgets/FeedbackWidget/FeedbackFooter'));
 
 const Footer = ({ disableFeedback }) => (
   <>
     {!disableFeedback && <FeedbackFooter />}
-    <div className="footer">
-      <div className="copyright">
-        <p>© MongoDB, Inc 2008-present. MongoDB, Mongo, and the leaf logo are registered trademarks of MongoDB, Inc.</p>
+    {!process.env.GATSBY_FEATURE_FLAG_CONSISTENT_NAVIGATION && (
+      <div className="footer">
+        <div className="copyright">
+          <p>
+            © MongoDB, Inc 2008-present. MongoDB, Mongo, and the leaf logo are registered trademarks of MongoDB, Inc.
+          </p>
+        </div>
       </div>
-    </div>
+    )}
   </>
 );
 
