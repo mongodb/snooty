23821f7fbb745cbf61d0963786a5735972966220
rayangler <27821750+rayangler@users.noreply.github.com>
2021-08-06 14:55:34 -0400

    DOP-2380: Add mobile support for the side nav with consistent nav enabled (#464)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import SiteBanner from './SiteBanner';
+import { UnifiedNav } from '@mdb/consistent-nav';
 import Navbar from './Navbar';
+import { SidenavMobileMenuDropdown } from './Sidenav';
+import SiteBanner from './SiteBanner';
 
 const StyledHeaderContainer = styled.header`
   grid-area: header;
@@ -12,7 +14,16 @@ const Header = () => {
   return (
     <StyledHeaderContainer>
       <SiteBanner />
-      <Navbar />
+      {/* TODO: Remove this flag after consistent-nav is officially released */}
+      {process.env.GATSBY_FEATURE_FLAG_CONSISTENT_NAVIGATION ? (
+        <>
+          {/* UnifiedNav currently looks wonky on grid layout; this should be changed in a future update */}
+          <UnifiedNav position="relative" />
+          <SidenavMobileMenuDropdown />
+        </>
+      ) : (
+        <Navbar />
+      )}
     </StyledHeaderContainer>
   );
 };
