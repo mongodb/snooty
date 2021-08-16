c96ad1a29644b8b8b01da0c721d2facd3534cca2
carooobregon <carooobregon@gmail.com>
2021-08-16 16:09:13 -0400

    adding constant sizes

--- src/components/Widgets/FeedbackWidget/views/SentimentView.js
+++ src/components/Widgets/FeedbackWidget/views/SentimentView.js
@@ -4,66 +4,67 @@ import Emoji from '../components/Emoji';
 import styled from '@emotion/styled';
 import { uiColors } from '@leafygreen-ui/palette';
 import { useFeedbackState } from '../context';
+import { theme } from '../../../../theme/docsTheme';
 
-const sentimentChoices = ['happy', 'upset', 'suggesting']
+const sentimentChoices = ['happy', 'upset', 'suggesting'];
 
 const ViewHeader = styled('h3')`
   font-weight: 600;
-  font-size: 14px;
+  font-size: ${theme.fontSize.small};
   text-align: center;
   margin-top: 10px;
   margin-bottom: 10px;
 `;
 
 const getCopy = (sentiment) => {
-  switch(sentiment){
+  switch (sentiment) {
     case 'happy':
       return 'Yes, it did';
     case 'upset':
-      return 'No, I have feedback.'
+      return 'No, I have feedback.';
     case 'suggesting':
-      return 'I have a suggestion.'
+      return 'I have a suggestion.';
     default:
-      return 'noemoji';
+      return '';
   }
-}
+};
 
 const StyledSentiment = styled('div')`
   width: 100%;
 `;
 const StyledSentimentOption = styled('h4')`
-  font-weight: 200!important;
-  font-size: 16px!important;
-  color: ${uiColors.gray.dark1}!important;
-  margin:0px -24px !important;
-  padding: 10px 24px!important;
+  font-weight: 200 !important;
+  font-size: ${theme.fontSize.default} !important;
+  color: ${uiColors.gray.dark1} !important;
+  margin: 0px -24px !important;
+  padding: 10px ${theme.size.medium} !important;
   cursor: pointer;
-  :hover{
+  :hover {
     background-color: rgba(0, 0, 0, 0.1);
   }
 `;
 
-const SentimentOption = ({sentiment}) => {
+const SentimentOption = ({ sentiment }) => {
   const { feedback, setSentiment } = useFeedbackState();
   return (
     <StyledSentiment onClick={() => setSentiment()}>
       <StyledSentimentOption>
-        <Emoji sentiment={sentiment}/>
+        <Emoji sentiment={sentiment} />
         {getCopy(sentiment)}
       </StyledSentimentOption>
     </StyledSentiment>
-  )
-}
+  );
+};
 
-const SentimentView = (props)  => {
+const SentimentView = (props) => {
   return (
     <Layout>
       <ViewHeader>Did this page help?</ViewHeader>
       {sentimentChoices.map((sentiment) => (
-        <SentimentOption sentiment={sentiment}/>
+        <SentimentOption sentiment={sentiment} />
       ))}
     </Layout>
   );
-}
+};
 
 export default SentimentView;
