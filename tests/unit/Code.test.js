4b0eba58739a42e6a117fb562814464d7cdadea2
rayangler <27821750+rayangler@users.noreply.github.com>
2021-10-06 13:40:52 -0400

    DOP-2136: Add LanguageSwitcher to code blocks (#498)

--- tests/unit/Code.test.js
+++ tests/unit/Code.test.js
@@ -1,25 +1,105 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Code from '../../src/components/Code';
-import { TabContext } from '../../src/components/tab-context';
+import { CodeProvider } from '../../src/components/code-context';
+import { TabProvider } from '../../src/components/tab-context';
+import * as browserStorage from '../../src/utils/browser-storage';
+import { tick } from '../utils';
 
 // data for this component
 import mockData from './data/Code.test.json';
 
-const shallowCode = ({ data, activeTabs }) => {
-  return shallow(
-    <TabContext.Provider value={{ activeTabs }}>
-      <Code nodeData={data} />
-    </TabContext.Provider>
+const mockSelectors = {
+  drivers: {
+    nodejs: [
+      {
+        type: 'text',
+        position: {
+          start: {
+            line: 0,
+          },
+        },
+        value: 'Node.js',
+      },
+    ],
+    python: [
+      {
+        type: 'text',
+        position: {
+          start: {
+            line: 0,
+          },
+        },
+        value: 'Python',
+      },
+    ],
+    shell: [
+      {
+        type: 'text',
+        position: {
+          start: {
+            line: 0,
+          },
+        },
+        value: 'MongoDB Shell',
+      },
+    ],
+  },
+};
+
+const shallowCode = ({ data }) => {
+  return shallow(<Code nodeData={data} />);
+};
+
+const mountCodeWithSelector = ({ data }) => {
+  return mount(
+    <TabProvider selectors={mockSelectors}>
+      <CodeProvider>
+        <Code nodeData={data} />
+      </CodeProvider>
+    </TabProvider>
   );
 };
 
 it('renders correctly', () => {
-  const tree = shallowCode({ data: mockData, activeTabs: { cloud: 'cloud' } });
-  expect(tree).toMatchSnapshot();
+  const wrapper = shallowCode({ data: mockData.testCode, activeTabs: { cloud: 'cloud' } });
+  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find('LanguageSwitcher').exists()).toBeFalsy();
 });
 
-it('renders with javascript disabled correctly', () => {
-  const tree = shallowCode({ data: mockData, activeTabs: {} });
-  expect(tree).toMatchSnapshot();
+describe('when rendering with selectors', () => {
+  jest.useFakeTimers();
+
+  const testData = mockData.testWithSelectors;
+  let mockGetLocalValue;
+
+  beforeAll(() => {
+    const mockActiveTabs = {
+      drivers: 'shell',
+    };
+    mockGetLocalValue = jest.spyOn(browserStorage, 'getLocalValue').mockImplementation(() => mockActiveTabs);
+  });
+
+  afterAll(() => {
+    mockGetLocalValue.mockClear();
+  });
+
+  it('renders with the correct active tab', () => {
+    const wrapper = mountCodeWithSelector({ data: testData });
+    const codeComponent = wrapper.find('Code').last();
+    expect(codeComponent.props().language).toEqual('MongoDB Shell');
+    expect(wrapper.find('LanguageSwitcher').exists()).toBeTruthy();
+  });
+
+  it('changes the selected driver', async () => {
+    const wrapper = mountCodeWithSelector({ data: testData });
+    let codeComponent = wrapper.find('Code').last();
+    // Assume that the LG component propagates events upwards successfully
+    codeComponent.invoke('onChange')({ id: 'nodejs' });
+
+    await tick({ wrapper });
+
+    codeComponent = wrapper.find('Code').last();
+    expect(codeComponent.props().language).toEqual('Node.js');
+  });
 });
