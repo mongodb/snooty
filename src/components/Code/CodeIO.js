9766e108939f947d236b409180c6d2d72bc80257
Grace Chong <grace.chong@mongodb.com>
2022-01-19 17:19:08 -0500

    remove print statements, put Code files in its own directory

--- src/components/Code/CodeIO.js
+++ src/components/Code/CodeIO.js
@@ -2,11 +2,19 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ComponentFactory from '../ComponentFactory';
 import { cx, css as LeafyCss } from '@leafygreen-ui/emotion';
+<<<<<<< HEAD
 import Icon from '@leafygreen-ui/icon';
 import Button from '@leafygreen-ui/button';
 import { css } from '@emotion/core';
 import styled from '@emotion/styled';
 import { baseCodeStyle, borderCodeStyle } from './styles/codeStyle';
+=======
+import { css } from '@emotion/core';
+import Icon from '@leafygreen-ui/icon';
+import Button from '@leafygreen-ui/button';
+import styled from '@emotion/styled';
+import { theme } from '../../theme/docsTheme';
+>>>>>>> 74f96a4 (remove print statements, put Code files in its own directory)
 
 const outputButtonStyling = LeafyCss`
   padding: 0px;
@@ -17,6 +25,7 @@ const outputButtonStyling = LeafyCss`
 `;
 
 const CodeIO = ({ nodeData: { children }, ...rest }) => {
+<<<<<<< HEAD
   const [showOutput, setShowOutput] = useState(false);
   const [buttonText, setButtonText] = useState('VIEW OUTPUT');
   const [arrow, setArrow] = useState('ChevronDown');
@@ -29,6 +38,20 @@ const CodeIO = ({ nodeData: { children }, ...rest }) => {
       setArrow('ChevronDown');
     } else {
       setShowOutput(true);
+=======
+  const [showOutput, setShowOutput] = useState('none');
+  const [buttonText, setButtonText] = useState('VIEW OUTPUT');
+  const [arrow, setArrow] = useState('ChevronDown');
+  let outputExists = showOutput === 'none' ? '4px' : '0px';
+
+  const handleClick = (e) => {
+    if (showOutput === 'inline') {
+      setShowOutput('none');
+      setButtonText('VIEW OUTPUT');
+      setArrow('ChevronDown');
+    } else {
+      setShowOutput('inline');
+>>>>>>> 74f96a4 (remove print statements, put Code files in its own directory)
       setButtonText('HIDE OUTPUT');
       setArrow('ChevronUp');
     }
@@ -37,16 +60,31 @@ const CodeIO = ({ nodeData: { children }, ...rest }) => {
   return (
     <div
       css={css`
+<<<<<<< HEAD
         ${baseCodeStyle}
+=======
+        display: table;
+        margin: ${theme.size.default} 0;
+        min-width: 150px;
+        table-layout: fixed;
+        width: 100%;
+>>>>>>> 74f96a4 (remove print statements, put Code files in its own directory)
 
         // Inner div of LG component has a width set to 700px. Unset this as part of our
         // override for docs when the language switcher is being used.
         > div > div {
+<<<<<<< HEAD
           border-bottom-right-radius: 0px;
           border-bottom-left-radius: 0px;
         }
 
         // Controls output code block style
+=======
+          width: unset;
+          border-radius: 0px;
+        }
+
+>>>>>>> 74f96a4 (remove print statements, put Code files in its own directory)
         > div {
           border-top-right-radius: 0px;
           border-top-left-radius: 0px;
@@ -54,6 +92,7 @@ const CodeIO = ({ nodeData: { children }, ...rest }) => {
           border-bottom-left-radius: ${outputExists};
           margin: 0px;
         }
+<<<<<<< HEAD
       `}
     >
       {children.length === 2 && (
@@ -73,13 +112,39 @@ const CodeIO = ({ nodeData: { children }, ...rest }) => {
         </>
       )}
       {showOutput && <ComponentFactory {...rest} nodeData={children[1]} />}
+=======
+
+        // Override default LG Code language switcher font size
+        button > div > div {
+          font-size: ${theme.fontSize.default};
+        }
+      `}
+    >
+      <ComponentFactory {...rest} nodeData={children[0]} />
+      <IOToggle>
+        <Button
+          className={cx(outputButtonStyling)}
+          darkMode={false}
+          disabled={false}
+          onClick={handleClick}
+          leftGlyph={<Icon glyph={arrow} fill="#FF0000" />}
+        >
+          {buttonText}
+        </Button>
+      </IOToggle>
+      {showOutput === 'inline' && <ComponentFactory {...rest} nodeData={children[1]} />}
+>>>>>>> 74f96a4 (remove print statements, put Code files in its own directory)
       {children.length !== 2 && children.map((child, i) => <ComponentFactory {...rest} key={i} nodeData={child} />)}
     </div>
   );
 };
 
 const IOToggle = styled.div`
+<<<<<<< HEAD
   ${borderCodeStyle}
+=======
+  border: 1px solid #e7eeec;
+>>>>>>> 74f96a4 (remove print statements, put Code files in its own directory)
   border-top: none;
 `;
 
