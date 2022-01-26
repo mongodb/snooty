8e553b8ef67000e418463dde1e22cd3682b3c646
Grace Chong <grace.chong@mongodb.com>
2022-01-26 17:07:52 -0500

    fix rendering problem

--- src/components/Banner/CTABanner.js
+++ src/components/Banner/CTABanner.js
@@ -1,64 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
 import { css } from '@emotion/core';
-import { cx, css as LeafyCSS } from '@leafygreen-ui/emotion';
-import LeafyBanner, { Variant as LeafyVariant } from '@leafygreen-ui/banner';
 import { uiColors } from '@leafygreen-ui/palette';
 import Icon from '@leafygreen-ui/icon';
 import ComponentFactory from '../ComponentFactory';
 import { baseBannerStyle } from './styles/bannerItemStyle';
 
-const StyledBanner = styled((props) => <LeafyBanner {...props} />)`
-  ${baseBannerStyle}
-  border-left-color: ${uiColors.blue.light2};
-`;
+const videoBannerStyling = css`
+  ${baseBannerStyle};
+  background-color: ${uiColors.blue.light3};
+  border: 1px solid ${uiColors.blue.light2};
+  border-radius: 6px;
+  display: flex;
+  position: relative;
+  font-size: 14px;
+  padding: 9px 12px 9px 20px;
+  color: ${uiColors.blue.dark2};
 
-const videoBannerStyling = LeafyCSS`
-  border-left-width: 1px;
+  > p {
+    margin-left: 35px;
+  }
 
   svg {
     height: 15px;
     margin-left: 3px;
-    margin-bottom: -5px;
-  }
-
-  &:before {
-    background-color: transparent;
+    margin-bottom: -3px;
   }
 `;
 
 const playIconStyling = css`
   border: 1px solid ${uiColors.blue.base};
   border-radius: 20px;
-  border-width: 1px;
-  margin-left: -5px;
   width: 25px;
   height: 25px;
   background-color: ${uiColors.blue.light2};
-  margin-top: 0px;
-  margin-bottom: 0px;
-  > div {
-    fill: ${uiColors.blue.base};
-  }
+  margin-left: -5px;
+  position: absolute;
 `;
 
 const CTABanner = ({ nodeData: { children, options }, ...rest }) => {
   return (
-    <a href={options.url}>
-      <StyledBanner
-        className={cx(videoBannerStyling)}
-        variant={LeafyVariant.Info}
-        image={
-          <div css={playIconStyling}>
-            <Icon glyph="Play" />
-          </div>
-        }
-      >
+    <a href={options?.url}>
+      <div css={videoBannerStyling}>
+        <div css={playIconStyling}>
+          <Icon glyph="Play" fill={uiColors.blue.base} />
+        </div>
         {children.map((child, i) => (
           <ComponentFactory {...rest} key={i} nodeData={child} />
         ))}
-      </StyledBanner>
+      </div>
     </a>
   );
 };
