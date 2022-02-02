52c8dbef43aa1a26e09fc5e391932b6cc08fe10f
gjchong25 <gjchong25@gmail.com>
2022-02-02 12:53:21 -0600

    DOP-2605: Fix styling for permalink and apply it to Target (#532)
    * DOP-2605: Fix styling for permalink and apply it to Target
    
    * generalize repeated code and address small fixes
    
    * add prop types and make buffer a prop
    
    * simplify logic
    
    Co-authored-by: Grace Chong <grace.chong@mongodb.com>

---
+++ src/components/Permalink.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { withPrefix } from 'gatsby';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+import { cx, css as LeafyCSS } from '@leafygreen-ui/emotion';
+import Tooltip from '@leafygreen-ui/tooltip';
+import { isBrowser } from '../utils/is-browser';
+import { theme } from '../theme/docsTheme';
+import useCopyClipboard from '../hooks/useCopyClipboard';
+
+const tooltipStyle = LeafyCSS` 
+  padding: 2px 8px;
+  font-size: ${theme.fontSize.xsmall};
+
+  > div {
+    font-size: ${theme.fontSize.tiny};
+  }
+`;
+
+const LinkIcon = styled.img`
+  border-radius: 0 !important;
+  display: initial !important;
+  margin: initial !important;
+`;
+
+const headingStyle = (copied) => css`
+  ${!!copied && 'visibility: visible !important;'}
+  align-self: center;
+  padding: 0 10px;
+`;
+
+const Permalink = ({ id, description, buffer }) => {
+  const [copied, setCopied] = useState(false);
+  const [headingNode, setHeadingNode] = useState(null);
+  const url = isBrowser ? window.location.href.split('#')[0] + '#' + id : '';
+  const bufferSpace = buffer || '-175px';
+
+  useCopyClipboard(copied, setCopied, headingNode, url);
+
+  const handleClick = (e) => {
+    setCopied(true);
+  };
+
+  const HeaderBuffer = styled.div`
+    margin-top: ${bufferSpace};
+    position: absolute;
+  `;
+
+  return (
+    <>
+      <a
+        className="headerlink"
+        ref={setHeadingNode}
+        css={headingStyle(copied)}
+        href={`#${id}`}
+        title={'Permalink to this ' + description}
+        onClick={handleClick}
+      >
+        <LinkIcon src={withPrefix('assets/link.svg')} />
+        <Tooltip
+          className={cx(tooltipStyle)}
+          triggerEvent="click"
+          open={copied}
+          align="top"
+          justify="middle"
+          darkMode={true}
+        >
+          {'copied'}
+        </Tooltip>
+      </a>
+      <HeaderBuffer id={id} />
+    </>
+  );
+};
+
+Permalink.propTypes = {
+  id: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  buffer: PropTypes.string,
+};
+
+export default Permalink;
