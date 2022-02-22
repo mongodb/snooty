2edfd73a12479bf8bbb9110e1d49941ac2395d87
rayangler <27821750+rayangler@users.noreply.github.com>
2022-02-22 17:41:26 -0500

    DOP-2763: Render nested steps with correct step numbers (#559)

--- tests/unit/Step.test.js
+++ tests/unit/Step.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Step from '../../src/components/Step';
+import Step from '../../src/components/Procedure/Step';
 
 // data for this component
 import mockData from './data/Step.test.json';
