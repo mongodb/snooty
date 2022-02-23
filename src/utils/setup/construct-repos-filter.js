23c6a3fb9900fd9737993e83a678d07018545077
rayangler <27821750+rayangler@users.noreply.github.com>
2022-02-23 15:01:56 -0500

    Update src/utils/setup/construct-repos-filter.js
    Co-authored-by: Cassidy Schaufele <cassidyschaufele@gmail.com>

--- src/utils/setup/construct-repos-filter.js
+++ src/utils/setup/construct-repos-filter.js
@@ -1,8 +1,6 @@
 // Returns the filter to use when querying for a site's versions/branches.
 const constructReposFilter = (project) => {
-  const filter = {
-    project: project,
-  };
+  const filter = { project };
 
   // Handles case where public and internal docs share the same project name.
   // REPO_NAME should be passed in by the autobuilder as an env variable
