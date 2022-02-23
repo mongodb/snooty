34553a3854067dfbc34d089363f882f9f17154d4
Grace Chong <grace.chong@mongodb.com>
2022-02-23 14:46:00 -0500

    DOP-2755: Add URL validation for videos

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
+import ReactPlayer from 'react-player/youtube';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
@@ -7,9 +7,9 @@ import { theme } from '../../theme/docsTheme';
 import VideoPlayButton from './VideoPlayButton';
 
 const ReactPlayerWrapper = styled('div')`
-  margin-bottom: ${theme.size.articleContent};
   position: relative;
   padding-top: 56.25%;
+  margin: ${theme.size.medium} 0px;
 `;
 
 const StyledReactPlayer = styled(ReactPlayer)`
@@ -29,28 +29,39 @@ const videoStyling = css`
 `;
 
 const Video = ({ nodeData: { argument }, ...rest }) => {
+  const playable = ReactPlayer.canPlay(argument[0]['refuri']);
+
+  if (!playable) {
+    console.warn(`Invalid URL: ${argument[0]['refuri']} has been passed into the Video component`);
+    return null;
+  }
+
   return (
-    <ReactPlayerWrapper>
-      <StyledReactPlayer
-        css={videoStyling}
-        config={{
-          youtube: {
-            playerVars: {
-              autohide: 1,
-              modestbranding: 1,
-              rel: 0,
-            },
-          },
-        }}
-        controls
-        url={argument[0]['refuri']}
-        width="100%"
-        height="100%"
-        playing
-        playIcon={<VideoPlayButton />}
-        light={<VideoPlayButton />}
-      />
-    </ReactPlayerWrapper>
+    <>
+      {playable && (
+        <ReactPlayerWrapper>
+          <StyledReactPlayer
+            css={videoStyling}
+            config={{
+              youtube: {
+                playerVars: {
+                  autohide: 1,
+                  modestbranding: 1,
+                  rel: 0,
+                },
+              },
+            }}
+            controls
+            url={argument[0]['refuri']}
+            width="100%"
+            height="100%"
+            playing
+            playIcon={<VideoPlayButton />}
+            light={<VideoPlayButton />}
+          />
+        </ReactPlayerWrapper>
+      )}
+    </>
   );
 };
 
