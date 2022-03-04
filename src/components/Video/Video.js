9c8e607d981820bc6d08d9f734355257d0d2d9ab
Grace Chong <grace.chong@mongodb.com>
2022-03-04 17:10:27 -0500

    update import

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import ReactPlayer from 'react-player';
+import ReactPlayer from 'react-player/youtube';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
