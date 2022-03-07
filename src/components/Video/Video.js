24cf374cfed7f2b3c61f8202a6941d0ae123191d
Cassidy Schaufele <cassidyschaufele@gmail.com>
2022-03-07 09:59:27 -0700

    Hard version pin subdependencies from react-player

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactPlayer from 'react-player';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
-import { css } from '@emotion/core';
+import { css } from '@emotion/react';
 import { theme } from '../../theme/docsTheme';
 import VideoPlayButton from './VideoPlayButton';
 
