27e047b41e750f402341bd2586211230a83e1cb9
rayangler <27821750+rayangler@users.noreply.github.com>
2022-03-08 18:44:55 -0500

    Add comment for lint-staged fix

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,12 @@
 const { CLIEngine } = require('eslint');
 
+// lint-staged seems to disregard eslint ignore rules if we have the eslint
+// "--max-warnings 0" option set. This will potentially break our linting process
+// if we are staging files that we want ignored (such as .eslintrc.js).
+
+// This was the recommended fix according to lint-staged's README:
+// https://github.com/okonet/lint-staged/blob/fa15d686deb90b7ffddfbcf644d56ed05fcd8a38/README.md#how-can-i-ignore-files-from-eslintignore
+
 module.exports = {
   '*.js': (files) => {
     const cli = new CLIEngine({});
