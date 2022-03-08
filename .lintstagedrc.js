52491afa74a5e537d188b116aa95464b97340f8e
rayangler <27821750+rayangler@users.noreply.github.com>
2022-03-08 18:24:53 -0500

    Fix format and lint commands

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,7 @@ const { CLIEngine } = require('eslint');
 module.exports = {
   '*.js': (files) => {
     const cli = new CLIEngine({});
-    return 'eslint --max-warnings=0 ' + files.filter((file) => !cli.isPathIgnored(file)).join(' ');
+    const filesToLint = files.filter((file) => !cli.isPathIgnored(file)).join(' ');
+    return ['npm run format:fix', `npm run lint:fix -- ${filesToLint}`];
   },
-}
+};
