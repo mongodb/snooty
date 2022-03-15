294e40a773e4623b66682d2caa9e74bd16c5555f
Cassidy Schaufele <cassidyschaufele@gmail.com>
2022-03-15 11:49:21 -0600

    DOP-2819: Remove duplicate prefix from HTML Breadcrumbs (#580)
    * DOP-2819: Add prefix toggle to dotcomify logic, optionize parameters
    
    * DOP-2819: Update test values
    
    * DOP-2819: Add drillthrough of needsPrefix to getSiteUrl w/ testsuite

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { UnifiedNav } from '@mdb/consistent-nav';
 import { SidenavMobileMenuDropdown } from './Sidenav';
 import SiteBanner from './Banner/SiteBanner';
 import { useSiteMetadata } from '../hooks/use-site-metadata';
-import { isDotCom, DOTCOM_BASE_URL } from '../utils/dotcom';
+import { isDotCom, DOTCOM_BASE_URL, DOTCOM_BASE_PREFIX } from '../utils/dotcom';
 
 const StyledHeaderContainer = styled.header`
   grid-area: header;
@@ -26,7 +26,7 @@ const Header = ({ sidenav }) => {
   const shouldSearchRealm = project === 'realm' || searchProperty === 'realm-master';
   const unifiedNavProperty = shouldSearchRealm ? 'REALM' : 'DOCS';
 
-  const docsBaseUrl = isDotCom() ? `https://${DOTCOM_BASE_URL}` : null;
+  const docsBaseUrl = isDotCom() ? `https://${DOTCOM_BASE_URL}/${DOTCOM_BASE_PREFIX}` : null;
 
   return (
     <StyledHeaderContainer>
