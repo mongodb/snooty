adc06b190d14b8394fdcc1fa5979b21dff3d98f6
rayangler <27821750+rayangler@users.noreply.github.com>
2022-03-16 17:32:26 -0400

    Transfer footnote CSS

--- src/components/Footnote.js
+++ src/components/Footnote.js
@@ -1,9 +1,31 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
 import ComponentFactory from './ComponentFactory';
 import { getNestedValue } from '../utils/get-nested-value';
 import { intersperse } from '../utils/intersperse';
 import FootnoteContext from './footnote-context';
+
+const tableStyling = css`
+  border: 0;
+  border-collapse: collapse;
+  margin: 24px 0;
+  font-size: 14px;
+  line-height: 24px;
+
+  tbody tr td div.highlight pre {
+    background-color: inherit;
+  }
+
+  :target {
+    background-color: #ffa;
+  }
+`;
+
+const tdStyling = css`
+  border: 0;
+  padding: 11px 5px 12px;
+`;
 
 const Footnote = ({ nodeData: { children, id, name }, ...rest }) => {
   const { footnotes } = useContext(FootnoteContext);
@@ -12,29 +34,22 @@ const Footnote = ({ nodeData: { children, id, name }, ...rest }) => {
   const uid = name ? `${name}-` : '';
   const footnoteReferences = footnotes && footnotes[ref] ? footnotes[ref].references : [];
   const footnoteReferenceNodes = footnoteReferences.map((footnote, index) => (
-    <a className="fn-backref" href={`#ref-${uid}${footnote}`} key={index}>
+    <a href={`#ref-${uid}${footnote}`} key={index}>
       {index + 1}
     </a>
   ));
   return (
-    <table className="docutils footnote" frame="void" id={`footnote-${ref}`} rules="none">
+    <table css={tableStyling} frame="void" id={`footnote-${ref}`} rules="none">
       <colgroup>
-        <col className="label" />
+        <col />
       </colgroup>
       <tbody valign="top">
         <tr>
-          <td className="label">
-            [
-            {footnoteReferenceNodes.length !== 1 ? (
-              label
-            ) : (
-              <a className="fn-backref" href={`#ref-${uid}${footnoteReferences[0]}`}>
-                {label}
-              </a>
-            )}
+          <td css={tdStyling}>
+            [{footnoteReferenceNodes.length !== 1 ? label : <a href={`#ref-${uid}${footnoteReferences[0]}`}>{label}</a>}
             ]
           </td>
-          <td>
+          <td css={tdStyling}>
             {footnoteReferenceNodes.length > 1 && <em>({intersperse(footnoteReferenceNodes)})</em>}{' '}
             {children.map((child, index) => (
               <ComponentFactory {...rest} nodeData={child} key={index} parentNode="footnote" />
