a2375870b99a89a941ce172d6395303ee7811b94
rayangler <27821750+rayangler@users.noreply.github.com>
2022-03-16 17:44:16 -0400

    Transfer some guilabel CSS

--- src/components/Roles/GUILabel.js
+++ src/components/Roles/GUILabel.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from '@emotion/core';
 import ComponentFactory from '../ComponentFactory';
 
+const guiLabelStyle = css`
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: 700;
+`;
+
 const RoleGUILabel = ({ nodeData: { children } }) => (
-  <span className="guilabel">
+  // Keep "guilabel" className for styling when this component is inside of a Heading.
+  <span className="guilabel" css={guiLabelStyle}>
     {children.map((node, i) => (
       <ComponentFactory key={i} nodeData={node} />
     ))}
