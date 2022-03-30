9b1e04f14b3c941c4df08ca16738d7471a31ce10
rayangler <27821750+rayangler@users.noreply.github.com>
2022-03-30 10:27:11 -0400

    Fix emotion imports for transferred css

--- src/components/Footnote.js
+++ src/components/Footnote.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { css } from '@emotion/core';
+import { css } from '@emotion/react';
 import ComponentFactory from './ComponentFactory';
 import { getNestedValue } from '../utils/get-nested-value';
 import { intersperse } from '../utils/intersperse';
