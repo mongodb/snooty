c84d915c10e8298a860c4cf56255632e64aff517
Grace Chong <grace.chong@mongodb.com>
2022-03-30 11:10:31 -0400

    add tests for LG Link component

--- tests/unit/Link.test.js
+++ tests/unit/Link.test.js
@@ -13,6 +13,25 @@ describe('Link component renders a variety of strings correctly', () => {
   it('external URL', () => {
     const tree = setup({ to: 'http://mongodb.com', text: 'MongoDB Company' });
     expect(tree.asFragment()).toMatchSnapshot();
+    expect(tree.container.getElementsByClassName('leafygreen-ui-16bw3aj').length).toBe(1);
+  });
+
+  it('external MDB docs URL', () => {
+    const tree = setup({ to: 'https://www.mongodb.com/docs/atlas/', text: 'MongoDB Atlas Docs' });
+    expect(tree.asFragment()).toMatchSnapshot();
+    expect(tree.container.getElementsByClassName('leafygreen-ui-16bw3aj').length).toBe(0);
+  });
+
+  it('external MDB non-docs URL', () => {
+    const tree = setup({ to: 'https://account.mongodb.com/account/', text: 'MongoDB Atlas Registration Page' });
+    expect(tree.asFragment()).toMatchSnapshot();
+    expect(tree.container.getElementsByClassName('leafygreen-ui-16bw3aj').length).toBe(1);
+  });
+
+  it('external non-MDB docs URL', () => {
+    const tree = setup({ to: 'https://safety.google/authentication/', text: 'Google 2-Step Verification' });
+    expect(tree.asFragment()).toMatchSnapshot();
+    expect(tree.container.getElementsByClassName('leafygreen-ui-16bw3aj').length).toBe(1);
   });
 
   it('internal link', () => {
