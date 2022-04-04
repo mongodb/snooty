17859f035ba069f68d25a217c9fb10272808a0d4
rayangler <27821750+rayangler@users.noreply.github.com>
2022-04-04 12:59:46 -0400

    Transfer FA icons to separate stylesheet

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@emotion/react';
 import LeafyGreenProvider from '@leafygreen-ui/leafygreen-provider';
 import { theme } from './src/theme/docsTheme';
 import './src/styles/mongodb-docs.css';
+import './src/styles/icons.css';
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>
