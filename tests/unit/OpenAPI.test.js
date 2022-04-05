eac163b0842a1c000c77840aef1e13fc2b402f02
Cassidy Schaufele <cassidyschaufele@gmail.com>
2022-04-05 10:15:49 -0600

    DOP-2773: Fix test input

--- tests/unit/OpenAPI.test.js
+++ tests/unit/OpenAPI.test.js
@@ -94,7 +94,7 @@ describe('OpenAPI', () => {
     global.window = Object.create(window);
     Object.defineProperty(window, 'location', {
       value: {
-        search: `?src=this_is_a_test_url`,
+        search: `?src=https://this_is_a_test_url.com`,
       },
       writable: true,
     });
