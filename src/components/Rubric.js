d9f4625507094a830bc4b33fa72550aa634f4164
rayangler <27821750+rayangler@users.noreply.github.com>
2022-04-07 16:44:34 -0400

    DOP-2806: Separate component-specific legacy CSS into respective components (#592)

--- src/components/Rubric.js
+++ src/components/Rubric.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from '@emotion/react';
 import ComponentFactory from './ComponentFactory';
 
+const rubricStyle = css`
+  font-weight: 700;
+`;
+
 const Rubric = ({ nodeData: { argument }, ...rest }) => (
-  <p className="rubric">
+  <p css={rubricStyle}>
     {argument.map((node, i) => (
       <ComponentFactory {...rest} key={i} nodeData={node} />
     ))}
