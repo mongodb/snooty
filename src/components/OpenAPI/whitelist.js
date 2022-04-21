bbb1d330d90d1109ae1d0aac805775baff849f83
Allison Reinheimer Moore <allison@mongodb.com>
2022-04-21 12:36:02 -0400

    Update copy on unsupported URL OpenAPI admonition

--- src/components/OpenAPI/whitelist.js
+++ src/components/OpenAPI/whitelist.js
@@ -7,7 +7,7 @@ export const isLinkInWhitelist = (link) => HOST_WHITELIST.includes(new URL(link)
 export const WhitelistErrorCallout = (props) => {
   return (
     <Callout variant="warning" {...props}>
-      The link you're trying to preview isn't currently whitelisted - if this is a mistake, please raise a pull request
+      The link you're trying to preview isn't currently permitted - if this is a mistake, please raise a pull request
       to add this source{' '}
       <a href="https://github.com/mongodb/snooty/blob/master/src/components/OpenAPI/whitelist.js#L4">here</a> for our
       consideration.
