3e81a54cf97029af8f7f26bd1214463a36f70885
rayangler <27821750+rayangler@users.noreply.github.com>
2022-04-29 13:45:48 -0400

    Rename index files for DefitionList and FieldList

--- tests/unit/FieldList.test.js
+++ tests/unit/FieldList.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import FieldList from '../../src/components/FieldList/FieldList';
+import FieldList from '../../src/components/FieldList';
 import { theme } from '../../src/theme/docsTheme';
 import { ThemeProvider } from '@emotion/react';
 // data for this component
