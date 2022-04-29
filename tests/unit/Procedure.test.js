8145ed11e690488e0a52ac1dfdff419e1fcc8f28
rayangler <27821750+rayangler@users.noreply.github.com>
2022-04-29 14:18:09 -0400

    DOP-2700: Organize related components (#609)

--- tests/unit/Procedure.test.js
+++ tests/unit/Procedure.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Procedure from '../../src/components/Procedure/Procedure';
+import Procedure from '../../src/components/Procedure';
 
 // data for this component
 import mockData from './data/Procedure.test.json';
