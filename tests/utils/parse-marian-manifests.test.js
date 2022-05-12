29f8d9877d9fc3c3b54e6b3bf65dd2c11ce2cee3
rayangler <27821750+rayangler@users.noreply.github.com>
2022-05-12 18:14:50 -0400

    Remove hardcoded mapping

--- tests/utils/parse-marian-manifests.test.js
+++ tests/utils/parse-marian-manifests.test.js
@@ -1,8 +1,4 @@
-import {
-  getSortedBranchesForProperty,
-  parseMarianManifest,
-  parseMarianManifests,
-} from '../../src/utils/parse-marian-manifests';
+import { getSortedBranchesForProperty, parseMarianManifests } from '../../src/utils/parse-marian-manifests';
 import mockInputData from './data/marian-manifests.json';
 import mockResponseData from './data/parsed-marian-manifests.json';
 
@@ -19,19 +15,4 @@ it('should properly sort branches for a property with version numbers', () => {
     'Version 7.4 (current)',
     'Version 7.3',
   ]);
-});
-
-it('should properly parse a single marian manifest', () => {
-  // This function is covered by testing parseMarianManifests but it is good to show a use case
-  expect(parseMarianManifest('bi-connector-v2.0')).toStrictEqual({
-    property: 'BI Connector',
-    branch: 'v2.0',
-  });
-  // master has been mapped to "Latest"
-  expect(parseMarianManifest('atlas-master')).toStrictEqual({
-    property: 'Atlas',
-    branch: 'Latest',
-  });
-  // current has been mapped to "Stable"
-  expect(parseMarianManifest('atlas-current')).toStrictEqual({ property: 'Atlas', branch: 'Stable' });
 });
