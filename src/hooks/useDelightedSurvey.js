23e2ac3720e2d4f3ed4d5c435a7422f7fd08830e
Cassidy Schaufele <cassidyschaufele@gmail.com>
2022-08-03 10:22:37 -0600

    DOP-3169: Add dotcomprd to Delighted accepted envs

--- src/hooks/useDelightedSurvey.js
+++ src/hooks/useDelightedSurvey.js
@@ -2,12 +2,13 @@ import { useEffect } from 'react';
 import { useSiteMetadata } from './use-site-metadata';
 
 const ENABLED_SITES = new Set(['cloud-docs', 'datalake', 'docs', 'node', 'realm']);
+const ENABLED_ENVS = new Set(['dotcomprd', 'production']);
 
 export const useDelightedSurvey = (slug) => {
   const { parserBranch, project, snootyEnv } = useSiteMetadata();
 
   useEffect(() => {
-    if (snootyEnv === 'production' && ENABLED_SITES.has(project)) {
+    if (ENABLED_ENVS.has(snootyEnv) && ENABLED_SITES.has(project)) {
       let projectName = project;
       if (project === 'docs') {
         projectName = 'manual';
