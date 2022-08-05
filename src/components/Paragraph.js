2b785021546c42f04cf2f16798f896c1f313edbf
Matt Meigs <matt.meigs@mongodb.com>
2022-08-05 11:00:45 -0400

    renamed file to match util function renaming

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ComponentFactory from './ComponentFactory';
-import { appendTrailingPunctuation } from '../utils/append-dangling-siblings';
+import { appendTrailingPunctuation } from '../utils/append-trailing-punctuation';
 
 const SKIP_P_TAGS = new Set(['caption', 'footnote', 'field']);
 
