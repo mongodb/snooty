d564fac3a2b8e642f846d05163b7f871c7a50241
Allison Reinheimer Moore <allison@mongodb.com>
2022-08-16 12:25:52 -0400

    Revert "DOP-3167: dangling punctuation after Link (#661)"
    This reverts commit ee2e40cb0ab2da04486fc77d8e1d543019bedc8c.

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ComponentFactory from './ComponentFactory';
-import { appendTrailingPunctuation } from '../utils/append-trailing-punctuation';
 import { Body } from '@leafygreen-ui/typography';
 import styled from '@emotion/styled';
 
@@ -12,13 +11,10 @@ const StyledParagraph = styled(Body)`
 `;
 
 const Paragraph = ({ nodeData, parentNode, skipPTag, ...rest }) => {
-  const children = appendTrailingPunctuation(nodeData.children);
-
   // For paragraph nodes that appear inside certain containers, skip <p> tags and just render their contents
   if (skipPTag || SKIP_P_TAGS.has(parentNode)) {
-    return children.map((element, index) => <ComponentFactory {...rest} nodeData={element} key={index} />);
+    return nodeData.children.map((element, index) => <ComponentFactory {...rest} nodeData={element} key={index} />);
   }
-
   return (
     <StyledParagraph>
       {nodeData.children.map((element, index) => (
