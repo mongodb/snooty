ad482d35439d070e80c5230e1d5be482a689f6c4
mmeigs <matt.meigs@mongodb.com>
2022-08-29 11:38:35 -0400

    DOP-3167: fixed terrifying multiplication of punctuation (#676)
    * DOP-3167: fixed terrifying multiplication of punctuation
    
    Rather than emptying the value of the text node, the fix now deeply copies the children of the reference node to remove the continuing reference to extra punctuation
    
    * updated snapshot
    
    * added dangling punctuation fix to ref_role and omitted newline characters
    
    * PR review feedback
    
    * return copy of node in all cases

--- tests/unit/Paragraph.test.js
+++ tests/unit/Paragraph.test.js
@@ -4,8 +4,18 @@ import Paragraph from '../../src/components/Paragraph';
 
 // data for this component
 import mockData from './data/Paragraph.test.json';
+import mockDataFormat from './data/Paragraph-Format.test.json';
 
-it('renders correctly', () => {
-  const tree = render(<Paragraph nodeData={mockData} />);
-  expect(tree.asFragment()).toMatchSnapshot();
+describe('Paragraph unit tests', () => {
+  it('renders correctly', () => {
+    const tree = render(<Paragraph nodeData={mockData} />);
+    expect(tree.asFragment()).toMatchSnapshot();
+  });
+
+  it('handles formatting dangling punctuation after Links and no extra multiplying on rerenders', () => {
+    const tree = render(<Paragraph nodeData={mockDataFormat} />);
+    expect(tree.asFragment()).toMatchSnapshot();
+    const treeRerender = render(<Paragraph nodeData={mockDataFormat} />);
+    expect(treeRerender.asFragment()).toMatchSnapshot();
+  });
 });
