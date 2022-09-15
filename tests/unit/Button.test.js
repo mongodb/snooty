d4878b4fe5efcf8c1016d21643a7cf6a79a6bd9b
Seung Park <seungpark@users.noreply.github.com>
2022-09-15 10:46:25 -0400

    DOP-3222 follow up: Add unit tests for button uri (#688)
    * DOP-2579: update banner component (#682)
    
    * update banner component to v4. match typography with figma design
    
    * minor. fix comment
    
    * DOP-2581 (#683)
    
    * update lg/badge to v5
    
    * commit package lock file
    
    * DOP-2581: update tests (#685)
    
    update tests for operations
    
    * fix button href
    
    * remove redundant else
    
    * update snapshot
    
    * update color logic on button. add test for button to use link
    
    * update snapsho
    
    * minor. remove important styling
    
    * remove debug console

--- tests/unit/Button.test.js
+++ tests/unit/Button.test.js
@@ -4,8 +4,23 @@ import Button from '../../src/components/Button';
 
 // data for this component
 import mockData from './data/Button.test.json';
+import Link from '../../src/components/Link';
 
-it('renders correctly', () => {
-  const tree = render(<Button nodeData={mockData} />);
-  expect(tree.asFragment()).toMatchSnapshot();
+jest.mock('../../src/components/Link');
+
+describe('button component', () => {
+  beforeEach(() => {
+    Link.mockImplementation(jest.requireActual('../../src/components/Link').default);
+  });
+
+  it('renders correctly', () => {
+    const tree = render(<Button nodeData={mockData} />);
+    expect(tree.asFragment()).toMatchSnapshot();
+  });
+
+  it('converts to a link when passed an uri', () => {
+    Link.mockImplementation(() => <div>this is a link</div>);
+    const tree = render(<Button nodeData={mockData} />);
+    expect(tree.getByText('this is a link')).toBeTruthy();
+  });
 });
