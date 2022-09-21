5893d2c9ff937412098d89055ce10417a6e72b4d
Seung Park <seungpark@users.noreply.github.com>
2022-09-21 15:37:56 -0400

    DOP-3250: add no-return-await to eslint config (#699)
    * add version context for current+available versions for product+associated products
    
    * init unit tests for version context
    
    * fetch documents for associated products on server build
    
    * incorporate fetch on context for run time fetches
    
    * minor updates. remove assoc products from static query. filter associated productss branches when fetched
    
    * update unit tests. remove testing logs
    
    * update unit tests
    
    * add call to make realm fn to get repo branches of associated + parent repo
    
    * update unit tests. add mock for realm call
    
    * add feature flag
    
    * add no return await rule as warning for eslint config
    
    * merge conflicts

--- src/utils/realm.js
+++ src/utils/realm.js
@@ -20,29 +20,29 @@ const fetchData = async (funcName, ...argsList) => {
   if (!app.currentUser) {
     await loginAnonymous();
   }
-  return await app.currentUser.callFunction(funcName, ...argsList);
+  return app.currentUser.callFunction(funcName, ...argsList);
 };
 
 export const fetchBanner = async (snootyEnv) => {
-  return await fetchData('getBanner', snootyEnv === 'development');
+  return fetchData('getBanner', snootyEnv === 'development');
 };
 
 export const fetchProjectParents = async (database, project) => {
-  return await fetchData('fetchProjectParents', database, project);
+  return fetchData('fetchProjectParents', database, project);
 };
 
 export const fetchSearchPropertyMapping = async (snootyEnv) => {
-  return await fetchData('fetchSearchPropertyMapping', snootyEnv);
+  return fetchData('fetchSearchPropertyMapping', snootyEnv);
 };
 
 export const fetchOASFile = async (apiName, database) => {
-  return await fetchData('fetchOASFile', apiName, database);
+  return fetchData('fetchOASFile', apiName, database);
 };
 
 export const fetchDocument = async (database, collectionName, query) => {
-  return await fetchData('fetchDocument', database, collectionName, query);
+  return fetchData('fetchDocument', database, collectionName, query);
 };
 
 export const fetchDocuments = async (database, collectionName, query) => {
-  return await fetchData('fetchDocuments', database, collectionName, query);
+  return fetchData('fetchDocuments', database, collectionName, query);
 };
