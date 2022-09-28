55f8737338e38217f330eed27fe250b936189c69
Cassidy Schaufele <cassidyschaufele@gmail.com>
2022-09-28 09:44:01 -0600

    Fix typo :100:

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -12,7 +12,7 @@ export const onRenderBody = ({ setHeadComponents }) => {
     // GTM Pathway
     <script
       type="text/javascript"
-      dangerouslySetInnerHtml={{
+      dangerouslySetInnerHTML={{
         __html: `!function(e,n){var t=document.createElement("script"),o=null,x="pathway";t.async=!0,t.src='https://'+x+'.mongodb.com/'+(e?x+'-debug.js':''),document.head.append(t),t.addEventListener("load",function(){o=window.pathway.default,(n&&o.configure(n)),o.createProfile("mongodbcom").load(),window.segment=o})}();`,
       }}
     />,
