d65d469d77102c5a75201ab9f0a990ea5b1cf696
Seung Park <seungpark@users.noreply.github.com>
2022-10-04 10:57:00 -0400

    DOP-3225: add version context (#702)
    * add Toc context that initializes with data from metadata + version context
    
    * add test data. add project to test data
    
    * do not mutate original toctree from staticquery
    
    * update logic for filtering. TODO: client side fetch for metadata
    
    * add client side fetch for toc metadata
    
    * test with snooty_dev data
    
    * add testing code
    
    * clean up context. update reference to gitbranchname
    
    * add toc context unit test
    
    * add note for mutation. update tests

---
+++ src/context/toc-context.js
@@ -0,0 +1,101 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { METADATA_COLLECTION } from '../build-constants';
+import { useSiteMetadata } from '../hooks/use-site-metadata';
+import { fetchDocument } from '../utils/realm';
+import useSnootyMetadata from '../utils/use-snooty-metadata';
+import { VersionContext } from './version-context';
+
+const TocContext = createContext({
+  activeToc: {}, // table of contents, represented by head node
+});
+
+/**
+ *
+ * @param {object} tocTree toc tree head node
+ * @param {object} currentVersion see version-context currentVersion{}
+ * @param {object} availableVersions see version-context availableVersions{}
+ * @modifies [tocTree.children[], tocTree.children[].options.versions[], tocTree.children[].options.versions]
+ *
+ * @returns tocTree toc tree head node, with nodes filtered by current and available versions
+ */
+const filterTocByVersion = function (tocTree = {}, currentVersion = {}, availableVersions = {}) {
+  const shallowCloneToc = { ...tocTree };
+  if (Object.keys(tocTree).length === 0) {
+    return shallowCloneToc;
+  }
+  shallowCloneToc.children =
+    shallowCloneToc.children?.filter((tocNode) => {
+      if (!tocNode?.options?.versions) {
+        return true;
+      }
+      const nodeProject = tocNode.options.project;
+      const availableVersionNames =
+        availableVersions[nodeProject]?.map((branchObj) => branchObj['gitBranchName']) || [];
+      // filter ToC version options by version context
+      tocNode.options.versions = tocNode.options.versions.filter((version) => availableVersionNames.includes(version));
+      // if ToC and version context don't match, don't show versioned ToC children
+      if (!tocNode.options.versions.length) {
+        return false;
+      }
+
+      let targetVersion = currentVersion[nodeProject];
+      // if current version is not part of merged ToC, fallback to last
+      if (!targetVersion || !tocNode.options.versions.includes(targetVersion)) {
+        targetVersion = tocNode.options.versions[tocNode.options.version.length - 1];
+      }
+      tocNode.children = tocNode.children?.filter(
+        (versionedChild) => versionedChild.options?.version === targetVersion
+      );
+      return tocNode;
+    }) || [];
+
+  return shallowCloneToc;
+};
+
+const getTocMetadata = async (db, project, parserUser, parserBranch, manifestTree) => {
+  try {
+    let filter = {
+      page_id: `${project}/${parserUser}/${parserBranch}`,
+    };
+    if (process.env.GATSBY_TEST_EMBED_VERSIONS && project === 'cloud-docs') {
+      // TODO: remove testing code
+      db = 'snooty_dev';
+      filter = {
+        page_id: 'cloud-docs/spark/DOP-3225',
+      };
+    }
+    const metadata = await fetchDocument(db, METADATA_COLLECTION, filter);
+    return metadata.toctree;
+  } catch (e) {
+    console.error(e);
+    return manifestTree;
+  }
+};
+
+// ToC context that provides ToC content in form of *above*
+// filters all available ToC by currently selected version via VersionContext
+const TocContextProvider = ({ children }) => {
+  const { activeVersions, availableVersions } = useContext(VersionContext);
+  const { toctree } = useSnootyMetadata();
+  const { database, project, parserUser, parserBranch } = useSiteMetadata();
+  const [activeToc, setActiveToc] = useState({});
+
+  useEffect(() => {
+    getTocMetadata(database, project, parserUser, parserBranch, toctree).then((toctreeResponse) => {
+      const filtered = filterTocByVersion(toctreeResponse, activeVersions, availableVersions);
+      setActiveToc(filtered);
+    });
+  }, [
+    activeVersions,
+    availableVersions, // variable dependencies
+    database,
+    parserBranch,
+    parserUser,
+    project,
+    toctree, // build time constants
+  ]);
+
+  return <TocContext.Provider value={{ activeToc }}>{children}</TocContext.Provider>;
+};
+
+export { TocContext, TocContextProvider };
