d73c3e2ae900ba27d0d7fda6c6358e6aa76367dd
rayangler <27821750+rayangler@users.noreply.github.com>
2022-11-01 19:18:04 -0400

    Create mapping of OpenAPI spec stores

---
+++ src/utils/setup/construct-openapi-page-mapping.js
@@ -0,0 +1,62 @@
+const { createStore } = require('redoc');
+const fetch = require('node-fetch');
+
+const fetchData = async (url) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(res.statusText);
+  }
+  return res.text();
+};
+
+/**
+ * @param {string} specString
+ * @returns Serialized Redoc spec store
+ */
+const createSerializedStore = async (specString) => {
+  const spec = JSON.parse(specString);
+  const store = await createStore(spec);
+  return store.toJS();
+};
+
+/**
+ * @param {string} apiKeyword
+ * @returns Serialized Redoc spec store specific to Atlas APIs
+ */
+const getAtlasSourceStore = async (apiKeyword) => {
+  // Currently, the only expected API fetched programmatically is the Cloud Admin API,
+  // but it's possible to have more in the future with varying processes.
+  if (apiKeyword !== 'cloud') {
+    return null;
+  }
+
+  const versionURL = 'https://cloud.mongodb.com/version';
+  const gitHash = await fetchData(versionURL);
+  const oasFileURL = `https://mongodb-mms-prod-build-server.s3.amazonaws.com/openapi/${gitHash}.json`;
+  const oasFileContent = await fetchData(oasFileURL);
+
+  return createSerializedStore(oasFileContent);
+};
+
+/**
+ * @param {object} openapiMetadata
+ * @returns {object} A mapping of OpenAPI content pages and their parsed spec stores
+ */
+const constructOpenAPIPageMapping = async (openapiMetadata) => {
+  const mapping = {};
+  const entries = Object.entries(openapiMetadata);
+
+  for (const [slug, data] of entries) {
+    const { source_type: sourceType, source } = data;
+
+    if (sourceType === 'url' || sourceType === 'local') {
+      mapping[slug] = await createSerializedStore(source);
+    } else if (sourceType === 'atlas') {
+      mapping[slug] = await getAtlasSourceStore(source);
+    }
+  }
+
+  return mapping;
+};
+
+module.exports = { constructOpenAPIPageMapping };
