e9db108e893a8b088aeb995aad75e1e7172e379a
Seung Park <seung.park@mongodb.com>
2022-11-08 12:52:42 -0500

    minor. fix styling on "on this page" label

--- src/components/Contents/ContentsList.js
+++ src/components/Contents/ContentsList.js
@@ -2,16 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { Label } from '../Select';
+import { palette } from '@leafygreen-ui/palette';
 
 const List = styled('ul')`
   list-style-type: none;
   padding: 0;
 `;
 
+const StyledLabel = styled(Label)`
+  font-weight: 500;
+  color: ${palette.gray.dark1};
+`;
+
 const ContentsList = ({ children, label }) => {
   return (
     <>
-      <Label>{label}</Label>
+      <StyledLabel>{label}</StyledLabel>
       <List>{children}</List>
     </>
   );
