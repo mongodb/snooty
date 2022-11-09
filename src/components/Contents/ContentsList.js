c6167881d67873013a81eb5d5b50a781225fcd44
Seung Park <seungpark@users.noreply.github.com>
2022-11-09 09:34:24 -0500

    DOP-2591: fix styling on 'on this page' (#724)
    * minor. fix styling on "on this page" label
    
    * move import

--- src/components/Contents/ContentsList.js
+++ src/components/Contents/ContentsList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import { palette } from '@leafygreen-ui/palette';
 import { Label } from '../Select';
 
 const List = styled('ul')`
@@ -8,10 +9,15 @@ const List = styled('ul')`
   padding: 0;
 `;
 
+const StyledLabel = styled(Label)`
+  font-weight: 500;
+  color: ${palette.gray.dark1};
+`;
+
 const ContentsList = ({ children, label }) => {
   return (
     <>
-      <Label>{label}</Label>
+      <StyledLabel>{label}</StyledLabel>
       <List>{children}</List>
     </>
   );
