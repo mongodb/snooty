8c4316577ee2c56f7ba02ba43d5c3eac5b204a91
Allison Reinheimer Moore <allison@mongodb.com>
2022-11-14 19:35:12 -0500

    DOP-2863: escape HTML characters in search preview

---
+++ src/utils/escape-reserved-html-characters.js
@@ -0,0 +1,13 @@
+/**
+ * Replaces HTML-reserved characters with safe equivalents when
+ * dangerously setting inner HTML (e.g. for search previews).
+ * See DOP-2863 for rationale.
+ */
+export const escapeHtml = (unsafe) => {
+  return unsafe
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#039;');
+};
