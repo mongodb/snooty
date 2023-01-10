51c65c0158ec2f61d5cc918e301e037f9f88187a
Matt Meigs <matt.meigs@mongodb.com>
2023-01-10 15:56:01 -0500

    added index-no-nav file for test, added comment to index in src/layouts

---
+++ src/components/Header/index-no-nav.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+// import { UnifiedNav } from '@mdb/consistent-nav';
+import { SidenavMobileMenuDropdown } from '../Sidenav';
+import SiteBanner from '../Banner/SiteBanner';
+import { useSiteMetadata } from '../../hooks/use-site-metadata';
+import { isBrowser } from '../../utils/is-browser';
+
+const StyledHeaderContainer = styled.header`
+  grid-area: header;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`;
+
+const Header = ({ sidenav }) => {
+  const { project } = useSiteMetadata();
+
+  let searchProperty;
+  if (isBrowser) {
+    searchProperty = new URL(window.location).searchParams.get('searchProperty');
+  }
+
+  const shouldSearchRealm = project === 'realm' || searchProperty === 'realm-master';
+  const unifiedNavProperty = shouldSearchRealm ? 'REALM' : 'DOCS';
+  console.log('unified ', unifiedNavProperty);
+
+  return (
+    <StyledHeaderContainer>
+      <SiteBanner />
+      <>{sidenav && <SidenavMobileMenuDropdown />}</>
+    </StyledHeaderContainer>
+  );
+};
+
+Header.propTypes = {
+  sidenav: PropTypes.bool,
+  eol: PropTypes.bool.isRequired,
+};
+
+export default Header;
