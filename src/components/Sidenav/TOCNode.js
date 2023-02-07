d81994812f940a60cbcf393e41072736d6617507
Seung Park <seung.park@mongodb.com>
2023-02-07 09:47:21 -0500

    separate click for caret

--- src/components/Sidenav/TOCNode.js
+++ src/components/Sidenav/TOCNode.js
@@ -68,6 +68,11 @@ const TOCNode = ({ activeSection, handleClick, level = BASE_NODE_LEVEL, node }) 
     setIsOpen(isActive);
   }, [isActive, isDrawer || hasChildren ? activeSection : null]); // eslint-disable-line react-hooks/exhaustive-deps
 
+  const onCaretClick = (event) => {
+    event.preventDefault();
+    setIsOpen(!isOpen);
+  };
+
   const NodeLink = () => {
     // If title is a plaintext string, render as-is. Otherwise, iterate over the text nodes to properly format titles.
     const formatTextOptions = {
@@ -95,7 +100,7 @@ const TOCNode = ({ activeSection, handleClick, level = BASE_NODE_LEVEL, node }) 
             setIsOpen(!isOpen);
           }}
         >
-          <Icon className={cx(caretStyle)} glyph={iconType} fill={palette.gray.base} />
+          <Icon className={cx(caretStyle)} glyph={iconType} fill={palette.gray.base} onClick={onCaretClick} />
           {isTocIcon && <SyncCloud />}
           {formattedTitle}
         </SideNavItem>
@@ -112,7 +117,9 @@ const TOCNode = ({ activeSection, handleClick, level = BASE_NODE_LEVEL, node }) 
             setIsOpen(!isOpen);
           }}
         >
-          {hasChildren && <Icon className={cx(caretStyle)} glyph={iconType} fill={palette.gray.base} />}
+          {hasChildren && (
+            <Icon className={cx(caretStyle)} glyph={iconType} fill={palette.gray.base} onClick={onCaretClick} />
+          )}
           {isTocIcon && <SyncCloud />}
           {formattedTitle}
         </SideNavItem>
