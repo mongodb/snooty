3cd46a4932e3643eb054afe1c4863e6b078277c6
branberry <brandon.ly@mongodb.com>
2023-02-08 13:43:44 -0600

    [DOP-3290]: Add version and project to determine searchProperty

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { UnifiedNav } from '@mdb/consistent-nav';
@@ -6,7 +6,7 @@ import { SidenavMobileMenuDropdown } from '../Sidenav';
 import SiteBanner from '../Banner/SiteBanner';
 import { useSiteMetadata } from '../../hooks/use-site-metadata';
 import { isBrowser } from '../../utils/is-browser';
-
+import { VersionContext } from '../../context/version-context';
 const StyledHeaderContainer = styled.header`
   grid-area: header;
   position: sticky;
@@ -16,28 +16,27 @@ const StyledHeaderContainer = styled.header`
 
 const Header = ({ sidenav, eol }) => {
   const { project } = useSiteMetadata();
+  const { activeVersions } = useContext(VersionContext);
+  const version = `${project}-${activeVersions[project]}`;
 
   let searchProperty;
-  const searchParams = [];
   if (isBrowser) {
-    const { searchParams, pathname } = new URL(window.location);
+    const { searchParams } = new URL(window.location);
     searchProperty = searchParams.get('searchProperty');
-
-    // when splitting path name, we get empty strings, so we want to filter those out
-    // we also don't want to include 'docs' or 'realm' in the searchParams
-    // docs and realm are already accounted for in the searchProperty
-    const paths = pathname.split('/').filter((s) => !!s && s !== 'docs' && s !== 'realm');
-
-    paths.forEach((pathTerm) => searchParams.push({ value: pathTerm }));
   }
-
+  debugger;
   const shouldSearchRealm = project === 'realm' || searchProperty === 'realm-master';
   const unifiedNavProperty = shouldSearchRealm ? 'REALM' : 'DOCS';
   return (
     <StyledHeaderContainer>
       <SiteBanner />
       <>
-        {!eol && <UnifiedNav position="relative" property={{ name: unifiedNavProperty, searchParams }} />}
+        {!eol && (
+          <UnifiedNav
+            position="relative"
+            property={{ name: unifiedNavProperty, searchParams: [{ param: 'searchProperty', value: version }] }}
+          />
+        )}
         {sidenav && <SidenavMobileMenuDropdown />}
       </>
     </StyledHeaderContainer>
