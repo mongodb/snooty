115ad7acd0dceca08317168407a954d24541b415
branberry <brandon.ly@mongodb.com>
2023-02-22 17:25:33 -0600

    [DOP-3290]: add comment for drivers set

--- src/utils/drivers-set.js
+++ src/utils/drivers-set.js
@@ -1,1 +1,5 @@
+/**
+ * This set corresponds to the repo branch projects that have an entry
+ * in the search filter under category.
+ */
 export const driversSet = new Set(['golang', 'ruby', 'php', 'csharp', 'java', 'node']);
