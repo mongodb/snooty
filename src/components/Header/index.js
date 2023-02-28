f68802dcbba12042eb46feb1572588826dd0d5a9
Brandon Ly <brandonly@lostcoding.com>
2023-02-28 12:18:05 -0600

    [DOP-3290]: Search the current drivers docs site by default (#763)
    * [DOP-3290]: Grab path values for searchParams
    
    * [DOP-3290]: Update path params value
    
    * [DOP-3290]: Update comment
    
    * [DOP-3290]: Add version and project to determine searchProperty
    
    * [DOP-3290]: Add logic to only add searchParam if in drivers page
    
    * [DOP-3290]: Rename variables
    
    * [DOP-3290]: Refactor project manifest to be created only when project includes drivers
    
    * [DOP-3290]: Update consistent-nav library
    
    * [DOP-3290]: Add set for drivers pages
    
    * [DOP-3290]: Remove hardcoded searchProperty
    
    * [DOP-3290]: Remove logs, and comment code
    
    * [DOP-3290]: add comment for drivers set
    
    * [DOP-3290]: Use urlSlug instead of versionSelectorLabel and use snootyMetadata for branch
    
    * [DOP-3290]: Add new line
    
    * [DOP-3290]: Small refactor
    
    * [DOP-3290]: Remove drivers set
    
    * [DOP-3290]: Add search context for search manifests
    
    * [DOP-3290]: Remove search context as it is not needed and simplify header
    
    * [DOP-3290]: Remove unused imports, add check for docs landing
    
    * [DOP-3290]: Use marian manifest hook to get search manifests
    
    * [DOP-3290]: Remove console log
    
    * [DOP-3290]: Use projectManifest as key to check for in searchPropertyMapping obj
    
    * [DOP-3290]: Use projectManifest as key to check for in searchPropertyMapping obj
    
    * [DOP-3290]: Use correct property name
    
    * [DOP-3290]: Address comments left by PR

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,6 +6,8 @@ import { SidenavMobileMenuDropdown } from '../Sidenav';
 import SiteBanner from '../Banner/SiteBanner';
 import { useSiteMetadata } from '../../hooks/use-site-metadata';
 import { isBrowser } from '../../utils/is-browser';
+import useSnootyMetadata from '../../utils/use-snooty-metadata';
+import { useMarianManifests } from '../../hooks/use-marian-manifests';
 
 const StyledHeaderContainer = styled.header`
   grid-area: header;
@@ -16,20 +18,43 @@ const StyledHeaderContainer = styled.header`
 
 const Header = ({ sidenav, eol }) => {
   const { project } = useSiteMetadata();
+  const { branch } = useSnootyMetadata();
+  const { searchPropertyMapping } = useMarianManifests();
 
   let searchProperty;
-  if (isBrowser) {
-    searchProperty = new URL(window.location).searchParams.get('searchProperty');
-  }
 
+  if (isBrowser) {
+    const { searchParams } = new URL(window.location);
+    searchProperty = searchParams.get('searchProperty');
+  }
   const shouldSearchRealm = project === 'realm' || searchProperty === 'realm-master';
   const unifiedNavProperty = shouldSearchRealm ? 'REALM' : 'DOCS';
+
+  const searchParams = [];
+
+  let searchManifestName = project;
+
+  /**
+   * The searchPropertyMapping object will contain a new property
+   * called "projectToSearchMap". This map contains a mapping from
+   * the project name to the true search manifest name in cases where the
+   * project name is NOT the search manifest name.
+   */
+  if (searchPropertyMapping.projectToSearchMap && project in searchPropertyMapping.projectToSearchMap) {
+    searchManifestName = searchPropertyMapping.projectToSearchMap[project];
+  }
+
+  const projectManifest = `${searchManifestName}-${branch}`;
+
+  if (projectManifest in searchPropertyMapping) {
+    searchParams.push({ param: 'searchProperty', value: projectManifest });
+  }
 
   return (
     <StyledHeaderContainer>
       <SiteBanner />
       <>
-        {!eol && <UnifiedNav position="relative" property={{ name: unifiedNavProperty }} />}
+        {!eol && <UnifiedNav position="relative" property={{ name: unifiedNavProperty, searchParams }} />}
         {sidenav && <SidenavMobileMenuDropdown />}
       </>
     </StyledHeaderContainer>
