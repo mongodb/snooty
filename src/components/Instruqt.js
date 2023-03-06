7bf7e22dcc3ddce434b45b77046ba684fc039b8d
rayangler <27821750+rayangler@users.noreply.github.com>
2023-03-06 16:15:45 -0500

    Add fullscreen

--- src/components/Instruqt.js
+++ src/components/Instruqt.js
@@ -1,21 +1,59 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import { css } from '@emotion/react';
+import IconButton from '@leafygreen-ui/icon-button';
+import Icon from '@leafygreen-ui/icon';
+
+const controlsStyle = css`
+  width: 100%;
+  height: 48px;
+  background-color: rgb(28, 38, 57);
+  margin-top: -10px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 0 1rem;
+`;
 
 const Instruqt = ({ nodeData: { argument }, nodeData }) => {
   const embedValue = argument[0].value;
+  const iframeRef = useRef(null);
+
+  const onFullScreen = useCallback(() => {
+    if (iframeRef) {
+      const element = iframeRef.current;
+      if (element.requestFullscreen) {
+        element.requestFullscreen();
+      } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+      }
+    }
+  }, [iframeRef]);
 
   if (!embedValue) {
     return null;
   }
 
   return (
-    <iframe
-      allowFullScreen
-      sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-modals"
-      title={`Instruqt ${embedValue}`}
-      height="640"
-      width="100%"
-      src={`https://play.instruqt.com/embed${embedValue}`}
-    />
+    <>
+      <iframe
+        ref={iframeRef}
+        allowFullScreen
+        sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-modals"
+        title={`Instruqt ${embedValue}`}
+        height="640"
+        width="100%"
+        src={`https://play.instruqt.com/embed${embedValue}`}
+      />
+      <div css={controlsStyle}>
+        <IconButton onClick={onFullScreen}>
+          <Icon glyph="FullScreenEnter" />
+        </IconButton>
+      </div>
+    </>
   );
 };
 
