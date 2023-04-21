7f1a6b6e8ae097060b82c5f8e98c1f2ebd60e7a5
Caesar Bell <caesar.bell@mongodb.com>
2023-04-21 14:52:05 -0400

    DOP-3567 :tada: adds utli for temp key mapping

---
+++ src/utils/get-temp-mapping.js
@@ -0,0 +1,9 @@
+export const setAliasesKeys = {
+  'manual-upcoming': 'manual-v6.0',
+  'manual-manual': 'manual-v6.2',
+  'manual-master': 'manual-v6.3',
+};
+
+export const tempKey = (manifest) => {
+  return setAliasesKeys[manifest] ? setAliasesKeys[manifest] : manifest;
+};
