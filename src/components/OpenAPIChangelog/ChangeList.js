fcf0ba626a97297ef2fc8f51a843ebb9f9a230cb
mmeigs <matt.meigs@mongodb.com>
2023-05-01 10:56:17 -0400

    DOP-3677: OpenAPIChangelog component structure, template (#800)

---
+++ src/components/OpenAPIChangelog/ChangeList.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
+const ChangeList = () => {
+  return <Wrapper></Wrapper>;
+};
+
+export default ChangeList;
