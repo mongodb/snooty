7f660baf8e847b1f363998dc5030d462f6b59058
Matt Meigs <matt.meigs@mongodb.com>
2023-05-10 10:38:20 -0400

    filter POC in place

--- src/components/OpenAPIChangelog/OpenAPIChangelog.js
+++ src/components/OpenAPIChangelog/OpenAPIChangelog.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import { H2 } from '@leafygreen-ui/typography';
 import Button from '@leafygreen-ui/button';
@@ -21,6 +21,44 @@ const ChangelogHeader = styled.div`
 const OpenAPIChangelog = ({ changelog = mockChangelog, diff = mockDiff }) => {
   const [versionMode, setVersionMode] = useState(false);
 
+  const [filteredDiff, setFilteredDiff] = useState(diff);
+  const [filteredChangelog, setFilteredChangelog] = useState(changelog);
+
+  const [selectedFilters, setSelectedFilters] = useState([]);
+
+  useEffect(() => {
+    if (!selectedFilters.length) {
+      setFilteredDiff(diff);
+    } else setFilteredDiff(diff.filter(({ httpMethod, path }) => selectedFilters.includes(`${httpMethod} ${path}`)));
+  }, [selectedFilters, diff]);
+
+  useEffect(() => {
+    if (!selectedFilters.length) {
+      setFilteredChangelog(changelog);
+    } else {
+      const filteredReleases = changelog.filter((release) => {
+        return (
+          release.paths.filter(({ httpMethod, path }) => selectedFilters.includes(`${httpMethod} ${path}`)).length !== 0
+        );
+      });
+      const filteredResources = filteredReleases.map((release) => {
+        return {
+          ...release,
+          paths: release.paths.filter(({ httpMethod, path }) => selectedFilters.includes(`${httpMethod} ${path}`)),
+        };
+      });
+      setFilteredChangelog(filteredResources);
+    }
+  }, [selectedFilters, changelog]);
+
+  const changeFilter = (resource, e) => {
+    if (e?.target?.checked === true) {
+      setSelectedFilters([...selectedFilters, resource]);
+    } else {
+      setSelectedFilters(selectedFilters.filter((rs) => rs !== resource));
+    }
+  };
+
   return (
     <ChangelogPage>
       <ChangelogHeader>
@@ -35,8 +73,25 @@ const OpenAPIChangelog = ({ changelog = mockChangelog, diff = mockDiff }) => {
       <input type="radio" id="compare-two" checked={versionMode} onChange={() => setVersionMode(true)} />
       <label for="compare-two"> Compare Two Versions</label>
 
+      <div></div>
+
+      {/* Placeholder for now to filter a bit */}
+      <input
+        type="checkbox"
+        id="filter-1"
+        onChange={(e) => changeFilter('POST /api/atlas/v2/groups/{groupId}/accessList', e)}
+      />
+      <label for="filter-1"> {'POST /api/atlas/v2/groups/{groupId}/accessList'}</label>
+      <br />
+      <input
+        type="checkbox"
+        id="filter-2"
+        onChange={(e) => changeFilter('PUT /api/atlas/v2/groups/{groupId}/alertConfigs/{alertConfigId}', e)}
+      />
+      <label for="filter-2"> {'PUT /api/atlas/v2/groups/{groupId}/alertConfigs/{alertConfigId}'}</label>
+
       <FiltersPanel />
-      <ChangeList versionMode={versionMode} changes={versionMode ? diff : changelog} />
+      <ChangeList versionMode={versionMode} changes={versionMode ? filteredDiff : filteredChangelog} />
     </ChangelogPage>
   );
 };
