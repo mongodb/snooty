f829c09aba66578d8aa09ff787ad69dda49c0e59
mmeigs <matt.meigs@mongodb.com>
2023-05-25 15:13:03 -0400

    DOP-3731: skeleton (#818)

--- src/components/OpenAPIChangelog/utils/useFetchDiff.js
+++ src/components/OpenAPIChangelog/utils/useFetchDiff.js
@@ -2,17 +2,24 @@ import { useState, useEffect } from 'react';
 import { fetchOADiff } from '../../../utils/realm';
 import { getDiffRequestFormat } from './getDiffRequestFormat';
 
-export const useFetchDiff = ({ resourceVersionOne, resourceVersionTwo, index }) => {
+export const useFetchDiff = (resourceVersionOne, resourceVersionTwo, index, setIsLoading) => {
   const [diff, setDiff] = useState([]);
 
   useEffect(() => {
     if (!resourceVersionOne || !resourceVersionTwo || !index.runId) return;
+    setIsLoading(true);
     const fromAndToDiffString = getDiffRequestFormat(resourceVersionOne, resourceVersionTwo);
 
     fetchOADiff(index.runId, fromAndToDiffString)
-      .then((response) => setDiff(response))
-      .catch((err) => console.error(err));
-  }, [resourceVersionOne, resourceVersionTwo, index.runId]);
+      .then((response) => {
+        setDiff(response);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setIsLoading(false);
+      });
+  }, [resourceVersionOne, resourceVersionTwo, index.runId, setIsLoading]);
 
   return [diff, setDiff];
 };
