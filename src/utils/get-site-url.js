fc517962353b60bca23706eef1a772957e214a87
Cassidy Schaufele <cassidyschaufele@gmail.com>
2023-06-12 08:39:14 -0600

    [fixup] Adding case logic for atlas-cli url

--- src/utils/get-site-url.js
+++ src/utils/get-site-url.js
@@ -5,6 +5,9 @@ const getSiteUrl = (project) => {
     case 'cloud-docs':
       url = 'https://www.mongodb.com/docs/atlas';
       break;
+    case 'atlas-cli':
+      url = 'https://www.mongodb.com/docs/atlas/cli';
+      break;
     case 'mms':
       url = 'https://www.mongodb.com/docs/ops-manager';
       break;
