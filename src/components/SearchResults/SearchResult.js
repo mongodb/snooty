108b1947042f5c002e262c78061bb3ea02e7b68f
Bianca Laube <bianca.laube@mongodb.com>
2023-06-27 09:42:28 -0400

    updated snapshots

--- src/components/SearchResults/SearchResult.js
+++ src/components/SearchResults/SearchResult.js
@@ -14,6 +14,7 @@ const SEARCH_MATCH_STYLE = `background-color: ${palette.yellow.light2};`;
 
 const largeResultTitle = css`
   font-size: ${theme.size.default};
+  color: #016bf8;
   line-height: ${theme.size.medium};
   /* Only add bold on larger devices */
   @media ${theme.screenSize.smallAndUp} {
