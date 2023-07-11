942bc28b0b55f01376ae1c89797c0d6743aaa77d
Kyle Mathews <mathews.kyle@gmail.com>
2023-07-11 09:00:12 -0600

    Cleanup config + remove sitemap plugin from preview builds

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,8 @@ const { generatePathPrefix } = require('./src/utils/generate-path-prefix');
 const { siteMetadata } = require('./src/utils/site-metadata');
 
 const pathPrefix = generatePathPrefix(siteMetadata);
+const isPreview = process.env.GATSBY_IS_PREVIEW === `true`;
+const isNewPlugin = process.env.USE_NEW_PLUGIN === `true`
 
 // TODO: Gatsby v4 will enable code splitting automatically. Delete duplicate components, add conditional for consistent-nav UnifiedNav in DefaultLayout
 // const isFullBuild =
@@ -10,14 +12,7 @@ const pathPrefix = generatePathPrefix(siteMetadata);
 
 const plugins = [
   'gatsby-plugin-emotion',
-  process.env.USE_NEW_PLUGIN === `true` ? 'new' : 'old',
-  // {
-  // resolve: 'gatsby-plugin-layout',
-  // options: {
-  // component: require.resolve(layoutComponentRelativePath),
-  // },
-  // },
-  'gatsby-plugin-sitemap',
+  isNewPlugin ? 'new' : 'old',
   {
     resolve: 'gatsby-plugin-canonical-urls',
     options: {
@@ -26,6 +21,10 @@ const plugins = [
     },
   },
 ];
+
+if (!isPreview) {
+  plugins.push(`gatsby-plugin-sitemap`)
+}
 
 module.exports = {
   plugins,
