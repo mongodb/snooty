cb43afd006dc90e3e6aa90fb22dbe97dc2ecab1e
rayangler <27821750+rayangler@users.noreply.github.com>
2023-07-24 17:55:33 -0400

    Add webhook body log

--- plugins/new/gatsby-node.js
+++ plugins/new/gatsby-node.js
@@ -54,7 +54,8 @@ let manifestMetadata;
 
 const APIBase = process.env.API_BASE || `https://snooty-data-api.mongodb.com`;
 
-exports.sourceNodes = async ({ actions, createNodeId, getNode, createContentDigest, cache }) => {
+exports.sourceNodes = async ({ actions, createNodeId, getNode, createContentDigest, cache, webhookBody }) => {
+  console.log({ webhookBody });
   let hasOpenAPIChangelog = false;
   const { createNode } = actions;
 
