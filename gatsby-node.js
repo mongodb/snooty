caac84e66e923097d4e6a0e07cdec88729343823
Caesar Bell <caesar.bell@mongodb.com>
2023-08-17 14:10:52 -0400

    :heavy_minus_sign: DOP-3540 removes package and check for package during build time

--- gatsby-node.js
+++ gatsby-node.js
@@ -377,19 +377,6 @@ exports.createPages = async ({ actions }) => {
 
 // Prevent errors when running gatsby build caused by browser packages run in a node environment.
 exports.onCreateWebpackConfig = ({ stage, loaders, plugins, actions }) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      module: {
-        rules: [
-          {
-            test: /mongodb-stitch-browser-sdk/,
-            use: loaders.null(),
-          },
-        ],
-      },
-    });
-  }
-
   const providePlugins = {
     Buffer: ['buffer', 'Buffer'],
     process: require.resolve('./stubs/process.js'),
