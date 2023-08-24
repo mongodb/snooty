d39a004d5db5b57c548156576449f61f4d0ce230
Caesar Bell <caesar.bell@mongodb.com>
2023-08-24 16:35:34 -0400

    :sparkles: DOP-3870 adds chatbot ui component wrapper with custom style function

--- src/components/ChatbotUi.js
+++ src/components/ChatbotUi.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import Chatbot from 'mongodb-chatbot-ui';
+import React, { lazy } from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import { theme } from '../theme/docsTheme';
 
 const formWith = (measurement) => {
+  let _measurement = measurement;
+
+  if (typeof measurement !== 'string' && typeof measurement !== 'number') {
+    console.error('using the wrong measurement value');
+    // set it to the original style from the package
+    _measurement = '100%';
+  }
+
+  if (typeof measurement === 'number') {
+    _measurement = `${measurement}px`;
+  }
   return css`
     form {
-      width: ${measurement};
+      width: ${_measurement};
     }
   `;
 };
@@ -15,10 +25,10 @@ const formWith = (measurement) => {
 const StyledChatBotUiContainer = styled.div`
   margin-left: 60px;
   margin-top: 20px;
-  ${formWith('771px')}
+  ${formWith(771)}
 
   @media ${theme.screenSize.upToXLarge} {
-    ${formWith('704px')}
+    ${formWith(704)}
   }
 
   @media ${theme.screenSize.upToMedium} {
@@ -26,10 +36,13 @@ const StyledChatBotUiContainer = styled.div`
   }
 `;
 
+const LazyChatbot = lazy(() => import('mongodb-chatbot-ui'));
+
 const ChatbotUi = () => {
   return (
-    <StyledChatBotUiContainer>
-      <Chatbot />
+    <StyledChatBotUiContainer data-testid="chatbot-ui">
+      {/* We can wrap this in a Suspend to render a loading state if we decided we want that */}
+      <LazyChatbot />
     </StyledChatBotUiContainer>
   );
 };
