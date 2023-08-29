05c99ffc3342dddc15defdcdbae904f02c51bf66
Caesar Bell <caesar.bell@mongodb.com>
2023-08-29 15:54:30 -0400

    :fire: DOP-3870 removes from DocumentBodyPreview

--- src/components/DocumentBodyPreview.js
+++ src/components/DocumentBodyPreview.js
@@ -82,8 +82,6 @@ const DocumentBody = (props) => {
 
   return (
     <>
-      {/* TODO: Will need to update from manually passing in the nodeData when the directive is created in the parser */}
-      <ComponentFactory nodeData={{ type: 'chatbot' }} />
       <SEO pageTitle={pageTitle} siteTitle={siteTitle} />
       <Widgets
         location={location}
