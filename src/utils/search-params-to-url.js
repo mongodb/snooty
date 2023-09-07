be88a474db6a9520f969c011599251d2706dcce0
Seung Park <seung.park@mongodb.com>
2023-09-07 16:27:56 -0400

    update comment

--- src/utils/search-params-to-url.js
+++ src/utils/search-params-to-url.js
@@ -16,6 +16,9 @@ export const searchParamsToURL = (searchQuery, searchFilters, pageNumber = 1, is
  */
 export const searchParamsToMetaURL = (searchQuery, searchFilters, facetSelections) => {
   let queryParams = `?q=${searchQuery}${searchFilters ? `&searchProperty=${searchFilters}` : ''}`;
+  // TODO: DOP-3929
+  // use facet selections to expand facets, restrict count
+  //
   // for (const facetSelection of facetSelections) {
   //   for (const facetKey in facetSelection) {
   //     queryParams += `&facets.${facetKey}=${facetSelection[facetKey]}`
