3b648efc003623cec6f7001e3bfc4529ce45e5d4
branberry <brandon.ly@mongodb.com>
2023-09-08 14:25:53 -0500

    [DOP-3992]: Add video, h3, and text block to MoreWays component

--- src/components/Landing/MoreWays/index.js
+++ src/components/Landing/MoreWays/index.js
@@ -1,3 +1,15 @@
+import { H3 } from '@leafygreen-ui/typography';
+import Video from '../../Video';
+import ComponentFactory from '../../ComponentFactory';
+
 export const MoreWays = ({ nodeData: { children, options, argument }, ...rest }) => {
-  return <div></div>;
+  return (
+    <div>
+      <Video nodeData={{ argument: [{ refuri: options.video_url }] }} />
+      <H3>{argument}</H3>
+      {children.map((child, i) => (
+        <ComponentFactory nodeData={child} key={i} {...rest} />
+      ))}
+    </div>
+  );
 };
