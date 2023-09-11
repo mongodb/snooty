5c8a9e62769d3e6bc5378afd4baca578a4738339
Caesar Bell <caesar.bell@mongodb.com>
2023-09-11 12:21:28 -0400

    :sparkles: DOP-3989 adds a new standalone header that is used next to the cards group

--- src/components/StandaloneHeader.js
+++ src/components/StandaloneHeader.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+import { theme } from '../theme/docsTheme';
 import ComponentFactory from './ComponentFactory';
 import Link from './Link';
+import SectionHeader from './SectionHeader';
+
+const StyledStandaloneContainer = styled.div`
+  align-items: center;
+  column-gap: 16px;
+  display: flex;
+  margin-bottom: 12px;
+
+  @media ${theme.screenSize.mediumAndUp} {
+    margin-bottom: 24px;
+  }
+`;
 
 const StandaloneHeader = ({ nodeData: { argument, options } }) => {
+  const ele = useRef(null);
+  useEffect(() => {
+    // When using the standalone header next to the card-group element, remove card group
+    // margin top styles.
+    const nextSib = ele.current.nextSibling;
+    if (nextSib.classList.contains('card-group')) {
+      nextSib.style.marginTop = '0px';
+    }
+  }, []);
   return (
-    <div>
-      {argument.map((child, i) => (
-        <ComponentFactory nodeData={child} key={i} />
-      ))}
-      <Link to={options.url}>{options.cta}</Link>
-    </div>
+    <StyledStandaloneContainer ref={ele}>
+      <SectionHeader as="h3">
+        {argument.map((child, i) => (
+          <ComponentFactory nodeData={child} key={i} />
+        ))}
+      </SectionHeader>
+      <Link to={options.url} showLinkArrow={true}>
+        {options.cta}
+      </Link>
+    </StyledStandaloneContainer>
   );
 };
 
