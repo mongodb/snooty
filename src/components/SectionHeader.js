b3a442c9ec64fa4430acc75c835cba8d3507d5b5
Caesar Bell <caesar.bell@mongodb.com>
2023-09-11 13:31:03 -0400

    :art: DOP-3989 section header using LG with cx and css for overriding styles

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
+import { css, cx } from '@leafygreen-ui/emotion';
 import { Body } from '@leafygreen-ui/typography';
 
-const CustomStyleHeader = styled(Body)`
+const customStyleHeader = css`
   color: #061621;
   font-size: 24px;
   font-weight: 500;
   margin: 0;
 `;
 
-const SectionHeader = ({ as = 'h1', children }) => {
-  return <CustomStyleHeader as={as}>{children}</CustomStyleHeader>;
+const SectionHeader = ({ children, customStyles, as }) => {
+  return (
+    <Body className={cx([customStyleHeader, customStyles])} as={as}>
+      {children}
+    </Body>
+  );
+};
+SectionHeader.defaultProps = {
+  as: 'h1',
 };
 
 SectionHeader.prototype = {
   as: PropTypes.string,
-  children: PropTypes.oneOfType([PropTypes.element, PropTypes.elementType]),
+  children: PropTypes.oneOfType([PropTypes.element, PropTypes.elementType]).isRequired,
+  customStyles: PropTypes.string,
 };
 
 export default SectionHeader;
