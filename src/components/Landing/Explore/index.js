fb9e4f13270d60d161cfbbd30fb05c66c757abc9
Allison Reinheimer Moore <allison.moore@mongodb.com>
2023-09-16 14:35:59 -0400

    update with Raymunds feedback & fix Explore width

--- src/components/Landing/Explore/index.js
+++ src/components/Landing/Explore/index.js
@@ -7,7 +7,7 @@ import ComponentFactory from '../../ComponentFactory';
 import { theme } from '../../../theme/docsTheme';
 
 const ExploreItem = styled('div')`
-  @media ${theme.screenSize.largeAndUp} {
+  @media ${theme.screenSize.xLargeAndUp} {
     background-image: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="308" height="337" viewBox="0 0 308 337" fill="none"><path d="M183.5 11.2773L183.5 11.2907C183.875 39.1797 204.911 61.9519 230.994 61.9519H322.506C347.71 61.9519 368 84.0794 368 111.024V260.048C368 314.013 327.418 358 277.119 358C226.813 358 185.869 314.402 185.869 260.048V209.976C185.869 182.064 164.447 159.301 138.375 159.301H46.494C21.6488 159.301 1 137.561 1 110.229V11.6747C1 -15.2539 21.6438 -37 46.494 -37H138.006C162.866 -37 183.5 -15.6405 183.5 11.2773Z" stroke="%2300ED64" margin-left="865px" stoke-width="2"/></svg>');
     background-position: right;
     background-repeat: no-repeat;
@@ -16,25 +16,26 @@ const ExploreItem = styled('div')`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  flex: 1 1 auto;
-  grid-column-start: 2;
-  grid-column-end: 14;
+  flex: 1 1 100%;
+  grid-column-start: 1;
+  grid-column-end: 15;
   column-gap: 63px;
-  row-gap: 32px;
-  max-width: 1540px;
-  min-height: 386px;
+  min-height: 337px;
 
   @media ${theme.screenSize.upToLarge} {
     flex-direction: row;
     padding: 2px 52px 0 42px;
+    width: 100%;
   }
 
   @media ${theme.screenSize.LargeAndUp} {
     padding: 24px 52px 0 65px;
+    width: 100%;
   }
 
   @media ${theme.screenSize.xLargeAndUp} {
     padding: 24px 62px 0 65px;
+    width: 100%;
   }
 `;
 
@@ -54,17 +55,15 @@ const blockStyle = css`
   padding-top: 8px;
   font-size: 13px;
   max-width: 682px;
-  pre {
-    //padding-bottom: 100px;
-  }
   @media ${theme.screenSize.upToLarge} {
     padding-bottom: 0px;
     min-width: 0px;
+    flex: 1 0 100%;
     width: 100%;
   }
   @media ${theme.screenSize.xLargeAndUp} {
-    max-width: 400px;
-    flex: 1 0 400px;
+    flex: 1 0 425px;
+    width: 100%;
   }
 `;
 
@@ -72,11 +71,10 @@ const HeaderStyle = styled('div')`
   width: 329px;
   padding-top: 50px;
   padding-bottom: 24px;
-  width: 100%;
 `;
 
 const Explore = ({ nodeData: { children, argument }, ...rest }) => {
-  let title = argument[0]?.value;
+  const title = argument[0]?.value;
   return (
     <ExploreItem>
       <div className={cx(leftCol)}>
@@ -94,6 +92,7 @@ const Explore = ({ nodeData: { children, argument }, ...rest }) => {
               darkMode={true}
               variant={'default'}
               rightGlyph={'Export'}
+              hideExternalIcon={true}
             ></ComponentFactory>
           ))}
         </div>
