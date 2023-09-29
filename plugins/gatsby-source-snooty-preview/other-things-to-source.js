86bd95b41393eea302e3f5bd9431332b6d9330b5
rayangler <27821750+rayangler@users.noreply.github.com>
2023-09-29 11:20:44 -0400

    Don't check project for changelog

--- plugins/gatsby-source-snooty-preview/other-things-to-source.js
+++ plugins/gatsby-source-snooty-preview/other-things-to-source.js
@@ -5,26 +5,10 @@ const { createProductNodes } = require('../utils/products');
 
 // Sources nodes for the preview plugin that are not directly related to data
 // from the Snooty Data API
-exports.sourceNodes = async ({
-  hasOpenAPIChangelog,
-  createNode,
-  createContentDigest,
-  createNodeId,
-  getNodesByType,
-}) => {
+exports.sourceNodes = async ({ hasOpenAPIChangelog, createNode, createContentDigest, createNodeId }) => {
   let db = realmDocumentDatabase;
   await db.connect();
   await createProductNodes({ db, createNode, createNodeId, createContentDigest });
-
-  const nodes = getNodesByType(`SnootyMetadata`);
-  let hasCloudDocsProject = false;
-  for (const { metadata } of nodes) {
-    if (metadata.project === `cloud-docs`) {
-      hasCloudDocsProject = true;
-      break;
-    }
-  }
-
-  if (hasCloudDocsProject && hasOpenAPIChangelog)
+  if (hasOpenAPIChangelog)
     await createOpenAPIChangelogNode({ createNode, createNodeId, createContentDigest, siteMetadata, db });
 };
