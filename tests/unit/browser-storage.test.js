e14ed4f27616fb37b5937fec46b97f1f8b63394c
Maya Raman <maya.raman19@gmail.com>
2023-10-02 11:59:44 -0400

    adding unit test

--- tests/unit/browser-storage.test.js
+++ tests/unit/browser-storage.test.js
@@ -3,14 +3,20 @@ import { render } from '@testing-library/react';
 import { setLocalValue, getLocalValue } from '../../src/utils/browser-storage';
 import { expect, jest, test } from '@jest/globals';
 
-const mockGetItem = jest.spyOn(window.localStorage.getItem('mongodb-docs'), 'getItem');
+Object.defineProperty(window, 'localStorage', {
+  value: {
+    getItem: jest.fn(),
+  },
+});
+
+const mockGetItem = jest.spyOn(window.localStorage, 'getItem');
 
 // mock window.localStorage.getItem so it always throws an error
-mockGetItem.mockImplementation(() => {
+mockGetItem.mockImplementation((key) => {
   throw new Error('Error in test');
 });
 
 test('browser loads if setLocalValue breaks', () => {
-  expect(mockGetItem.toThrow());
-  expect(setLocalValue.not.toThrow());
+  expect(mockGetItem).toThrow();
+  expect(setLocalValue).not.toThrow();
 });
