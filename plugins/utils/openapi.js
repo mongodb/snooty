340bb238e0e51c9d0e672dc3fa4dc136570ccb6d
Anabella Buckvar <anabellabuckvar@gmail.com>
2023-10-03 16:51:50 -0400

    DOP-3903-b cleaned up merge

--- plugins/utils/openapi.js
+++ plugins/utils/openapi.js
@@ -5,7 +5,37 @@ const fetchChangelogData = async (runId, versions, s3Prefix) => {
   try {
     /* Using metadata runId, fetch OpenAPI Changelog full change list */
     const changelogResp = await fetch(`${s3Prefix}/${runId}/changelog.json`);
-    const changelog = await changelogResp.json();
+    const unfilteredChangelog = await changelogResp.json();
+
+    const hideChanges = (changelog) => {
+      const versionUpdate = (version) => {
+        const updatedVersion = { ...version };
+        if (version?.changes) {
+          updatedVersion.changes = version.changes.filter((change) => !change.hideFromChangelog);
+        }
+        return updatedVersion;
+      };
+
+      //pathUpdate takes the array of versions from the specific path passed in and takes each version and runs versionUpdate on it
+      const pathUpdate = (path) => {
+        const updatedPath = { ...path, versions: path.versions.map(versionUpdate) };
+        updatedPath.versions = updatedPath.versions.filter((version) => version.changes?.length);
+        return updatedPath;
+      };
+
+      //dateUpdate takes the array of paths from the specific date section passed in and takes each path and runs pathUpdate on it
+      const dateUpdate = (dateSection) => {
+        const updatedDateSection = { ...dateSection, paths: dateSection.paths.map(pathUpdate) };
+        updatedDateSection.paths = updatedDateSection.paths.filter((path) => path.versions?.length);
+        return updatedDateSection;
+      };
+
+      //changelog is the json file with everything in it. Map at this level takes each date section and runs dateUpdate on it
+      const updatedChangelog = changelog.map(dateUpdate);
+      return updatedChangelog.filter((dateSection) => dateSection.paths?.length);
+    };
+
+    const changelog = hideChanges(unfilteredChangelog);
 
     /* Aggregate all Resources in changelog for frontend filter */
     const resourcesListSet = new Set();
@@ -18,7 +48,23 @@ const fetchChangelogData = async (runId, versions, s3Prefix) => {
     const mostRecentResourceVersions = versions.slice(-2);
     const mostRecentDiffLabel = mostRecentResourceVersions.join('_');
     const mostRecentDiffResp = await fetch(`${s3Prefix}/${runId}/${mostRecentDiffLabel}.json`);
-    const mostRecentDiffData = await mostRecentDiffResp.json();
+    const mostRecentDiffDataUnfiltered = await mostRecentDiffResp.json();
+
+    //nested filtering of Diff changes with hideFromChangelog
+    const hideDiffChanges = (diffData) => {
+      const pathUpdate = (path) => {
+        const updatedPath = { ...path };
+        if (path?.changes) {
+          updatedPath.changes = path.changes.filter((change) => !change.hideFromChangelog);
+        }
+        return updatedPath;
+      };
+
+      const updatedDiffData = diffData.map(pathUpdate);
+      return updatedDiffData.filter((path) => path.changes?.length);
+    };
+
+    const mostRecentDiffData = hideDiffChanges(mostRecentDiffDataUnfiltered);
 
     return {
       changelog,
