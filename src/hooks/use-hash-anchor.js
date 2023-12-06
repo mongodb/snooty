b177be057d0733475610eb87362aa8aa051234d1
Seung Park <seungpark@users.noreply.github.com>
2023-12-06 13:36:16 -0500

    DOP-3708: scroll heading into view if hash exists in URL (#966)

---
+++ src/hooks/use-hash-anchor.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { useLocation } from '@gatsbyjs/reach-router';
+
+const useHashAnchor = (id, ref) => {
+  const { hash } = useLocation();
+
+  useEffect(() => {
+    const hashId = hash?.slice(1);
+    if (!hash || id !== hashId) {
+      return;
+    }
+    setTimeout(() => {
+      window.scrollTo(0, ref.current.offsetTop);
+    }, 40);
+  }, [hash, id, ref]);
+};
+
+export default useHashAnchor;
