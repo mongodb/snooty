242e34168fb2aae95b4bec520ac44dfee515fd20
rayangler <27821750+rayangler@users.noreply.github.com>
2023-12-07 16:15:34 -0500

    Rename file

--- src/components/Widgets/FeedbackWidget/index.js
+++ src/components/Widgets/FeedbackWidget/index.js
@@ -1,7 +1,7 @@
 import { FeedbackProvider, useFeedbackContext } from './context';
 import useFeedbackData from './useFeedbackData';
 import FeedbackForm from './FeedbackForm';
-import FeedbackTab from './FeedbackTab';
+import FeedbackTab from './FeedbackButton';
 import FeedbackFooter from './FeedbackFooter';
 
 export { FeedbackProvider, useFeedbackContext, useFeedbackData, FeedbackForm, FeedbackTab, FeedbackFooter };
