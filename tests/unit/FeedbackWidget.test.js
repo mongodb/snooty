0a95d31c6fbd68b25f3ad2774d714d7ff829deda
Anabella Buckvar <anabellabuckvar@gmail.com>
2023-12-12 15:06:08 -0500

    DOP-4193 removed unused footer and FeedbackFooter code

--- tests/unit/FeedbackWidget.test.js
+++ tests/unit/FeedbackWidget.test.js
@@ -2,12 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { matchers } from '@emotion/jest';
-import {
-  FeedbackProvider,
-  FeedbackForm,
-  FeedbackTab,
-  FeedbackFooter,
-} from '../../src/components/Widgets/FeedbackWidget';
+import { FeedbackProvider, FeedbackForm, FeedbackTab } from '../../src/components/Widgets/FeedbackWidget';
 
 import { tick, mockMutationObserver, mockSegmentAnalytics, setDesktop, setMobile, setTablet } from '../utils';
 import {
@@ -47,7 +42,6 @@ async function mountFormWithFeedbackState(feedbackState = {}, options = {}) {
         <div>
           <FeedbackTab />
           <Heading nodeData={headingData} sectionDepth={1} />
-          <FeedbackFooter />
         </div>
       </FeedbackProvider>
     </>
