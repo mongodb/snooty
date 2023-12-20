c05fad9a4ca03ad421d1ca4813c251d87f3e853e
Maya Raman <maya.raman19@gmail.com>
2023-12-20 13:49:28 -0800

    gatsby node preview

--- plugins/gatsby-source-snooty-preview/other-things-to-source.js
+++ plugins/gatsby-source-snooty-preview/other-things-to-source.js
@@ -2,6 +2,7 @@ const { siteMetadata } = require('../../src/utils/site-metadata');
 const { realmDocumentDatabase } = require('../../src/init/DocumentDatabase.js');
 const { createOpenAPIChangelogNode } = require('../utils/openapi');
 const { createProductNodes } = require('../utils/products');
+const { createDocsetNodes } = require('../utils/docsets');
 
 // Sources nodes for the preview plugin that are not directly related to data
 // from the Snooty Data API
@@ -9,6 +10,7 @@ exports.sourceNodes = async ({ hasOpenAPIChangelog, createNode, createContentDig
   let db = realmDocumentDatabase;
   await db.connect();
   await createProductNodes({ db, createNode, createNodeId, createContentDigest });
+  await createDocsetNodes({ db, createNode, createNodeId, createContentDigest });
   if (hasOpenAPIChangelog)
     await createOpenAPIChangelogNode({ createNode, createNodeId, createContentDigest, siteMetadata, db });
 };
