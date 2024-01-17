b67b5b0e1e35091b1ff3d17047ec8cdfc2f89c39
mmeigs <matt.meigs@mongodb.com>
2024-01-17 15:32:11 -0500

    DOP-4227: Chatbot FAB on all Docs pages (#983)

--- src/components/Widgets/index.js
+++ src/components/Widgets/index.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 import { isBrowser } from '../../utils/is-browser';
+import { theme } from '../../theme/docsTheme';
 import { FeedbackProvider, FeedbackForm, FeedbackButton, useFeedbackData } from './FeedbackWidget';
+import ChatbotFab from './ChatbotWidget/ChatbotFab';
 
-const Widgets = ({ children, pageOptions, pageTitle, publishedBranches, slug, isInPresentationMode }) => {
+const WidgetsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  align-items: end;
+  gap: ${theme.size.small};
+  position: fixed;
+  right: ${theme.size.large};
+  bottom: ${theme.size.large};
+
+  @media ${theme.screenSize.upToSmall} {
+    background-color: white;
+    width: 100%;
+    height: 60px;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0px -1px 0px 0px rgba(0, 0, 0, 0.25);
+    right: 0;
+    bottom: 0;
+    min-width: max-content;
+
+    > button {
+      height: 44px;
+      position: unset;
+    }
+  }
+`;
+
+const Widgets = ({ children, pageOptions, pageTitle, publishedBranches, slug, isInPresentationMode, template }) => {
   const url = isBrowser ? window.location.href : null;
   const hideFeedbackHeader = pageOptions.hidefeedback === 'header';
   const feedbackData = useFeedbackData({
@@ -20,10 +52,14 @@ const Widgets = ({ children, pageOptions, pageTitle, publishedBranches, slug, is
     <FeedbackProvider page={feedbackData} hideHeader={hideFeedbackHeader}>
       {children}
       {!isInPresentationMode && !hideFeedback && (
-        <>
-          <FeedbackButton />
-          <FeedbackForm />
-        </>
+        /* Suspense at this level ensures that widgets will appear simultaneously rather than one-by-one as loaded */
+        <Suspense fallback={null}>
+          <WidgetsContainer className={widgetsContainer}>
+            <FeedbackButton />
+            <FeedbackForm />
+            {template !== 'landing' && <ChatbotFab />}
+          </WidgetsContainer>
+        </Suspense>
       )}
     </FeedbackProvider>
   );
@@ -44,4 +80,5 @@ Widgets.defaultProps = {
   pageOptions: {},
 };
 
+export const widgetsContainer = 'widgets';
 export default Widgets;
