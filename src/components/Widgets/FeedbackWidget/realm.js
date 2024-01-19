743770fbc8ab2a89ccecdb7f501fac38ee105094
rayangler <27821750+rayangler@users.noreply.github.com>
2024-01-19 17:21:56 -0500

    Revert "Clean non-useful logic"
    This reverts commit eb9bdbde370a768290b71db01f1b8a3047c509e8.

--- src/components/Widgets/FeedbackWidget/realm.js
+++ src/components/Widgets/FeedbackWidget/realm.js
@@ -5,6 +5,55 @@ import { isBrowser } from '../../../utils/is-browser';
 const APP_ID = 'feedbackwidgetv3-dgcsv';
 export const app = isBrowser ? Realm.App.getApp(APP_ID) : { auth: {} };
 
+/**
+ * @param {object} storage
+ * @returns The prefix for the storage key used by Realm for localStorage access
+ */
+function parseStorageKey(storage) {
+  if (!storage.keyPart) {
+    return '';
+  }
+  const prefix = parseStorageKey(storage.storage);
+  return prefix + storage.keyPart + ':';
+}
+
+/**
+ * Deletes localStorage data for all users
+ */
+async function deleteLocalStorageData() {
+  const { allUsers } = app;
+  // The accessToken and refreshToken are automatically removed if invalid, but not the following keys
+  const keysToDelete = ['profile', 'providerType'];
+
+  Object.values(allUsers).forEach((user) => {
+    const storageKeyPrefix = parseStorageKey(user.storage);
+    keysToDelete.forEach((key) => {
+      localStorage.removeItem(storageKeyPrefix + key);
+    });
+  });
+}
+
+/**
+ * Makes a call to the Feedback Widget App Service to ensure the current user can succesfully
+ * call out to the app. Otherwise, a new user is created in case credentials are completely invalid.
+ * @returns A valid user
+ */
+// async function validateCurrentUser() {
+//   try {
+//     // Ensure access token is always refreshed on initial login
+//     await app.currentUser.refreshAccessToken();
+//     // Ideally, this would only error in the event that current anonymous credentials are messed up beyond repair
+//     const success = await app.currentUser.callFunction('validateConnection');
+//     if (!success) {
+//       return reassignCurrentUser();
+//     }
+//     return app.currentUser;
+//   } catch (e) {
+//     console.error(e);
+//     return reassignCurrentUser();
+//   }
+// }
+
 // User Authentication & Management
 export async function loginAnonymous() {
   if (!app.currentUser) {
@@ -27,13 +76,13 @@ export const useRealmUser = () => {
   const [user, setUser] = React.useState(app.currentUser);
 
   async function reassignCurrentUser() {
-    // Attempt to remove the user so that localStorage can be cleared out
-    if (app.currentUser) {
-      try {
-        await app.removeUser(app.currentUser);
-      } catch (e) {
-        console.error(e);
-      }
+    const oldUser = app.currentUser;
+    try {
+      await app.removeUser(oldUser);
+    } catch (e) {
+      console.error(e);
+      // Clean up invalid data from local storage to avoid bubbling up local storage sizes for unused users
+      await deleteLocalStorageData();
     }
 
     const newUser = await app.logIn(Realm.Credentials.anonymous());
