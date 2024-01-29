6dac07bde03447a962f79219081177e4f3aac5e9
Matt Meigs <matt.meigs@mongodb.com>
2024-01-29 11:29:43 -0500

    decode base64

--- src/init/DocumentDatabase.js
+++ src/init/DocumentDatabase.js
@@ -88,7 +88,7 @@ class ManifestDocumentDatabase {
   async getAsset(checksum) {
     if (!this.zip) {
       const asset = fs.readFileSync(`assets/${checksum}`);
-      return asset;
+      return atob(asset);
     }
     const result = this.zip.getEntry(`assets/${checksum}`);
     if (result) {
