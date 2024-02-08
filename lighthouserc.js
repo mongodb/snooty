fb863eedd4e1ea0bab5166bc4aab0c66ab7d58cd
Matt Meigs <matt.meigs@mongodb.com>
2024-02-08 16:21:21 -0500

    todo

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,7 +8,7 @@ module.exports = {
     upload: {
       target: 'lhci',
       serverBaseUrl: 'http://localhost:9001',
-      token: '3695a47b-03b2-4bd0-9d9b-53039f3f9d51', // could also use LHCI_TOKEN variable instead
+      token: '3695a47b-03b2-4bd0-9d9b-53039f3f9d51', // TODO: need to use env var
     },
   },
 };
