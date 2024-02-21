1944201051c0715e1fe3c63094f2ef2ffdf33932
rayangler <27821750+rayangler@users.noreply.github.com>
2024-02-21 17:23:41 -0500

    Debug logging

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -5,6 +5,7 @@ export async function handler(event) {
   console.log('This is the Netlify deploy-succeeded trigger function');
   // Has to be relative to where included_file originates from
   const testData = fs.readFileSync(path.resolve(__dirname, '../../build-hook.txt'), 'utf-8');
+  console.log({ testData });
   const parsedTestData = JSON.parse(testData);
   const incomingHookBody = process.env.INCOMING_HOOK_BODY;
   console.log({ event, incomingHookBody, parsedTestData });
