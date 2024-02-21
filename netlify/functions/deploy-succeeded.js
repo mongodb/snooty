b8f729d7401cec2637e4d84907631305de8036fa
rayangler <27821750+rayangler@users.noreply.github.com>
2024-02-21 17:01:26 -0500

    Check file one directory up

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -2,8 +2,9 @@ import fs from 'fs';
 import path from 'path';
 
 export async function handler(event) {
-  const testData = fs.readFileSync(path.resolve(__dirname, './build-hook.txt'));
   console.log('This is the Netlify deploy-succeeded trigger function');
+  // Go back a directory since this is where included files are added
+  const testData = fs.readFileSync(path.resolve(__dirname, '../build-hook.txt'));
   const incomingHookBody = process.env.INCOMING_HOOK_BODY;
   console.log({ event, incomingHookBody, testData });
 }
