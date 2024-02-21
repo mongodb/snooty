94d3dee7f1b11c6720e36b8bf755d74dc7df313a
rayangler <27821750+rayangler@users.noreply.github.com>
2024-02-21 17:10:41 -0500

    Fix relative file path

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -3,8 +3,8 @@ import path from 'path';
 
 export async function handler(event) {
   console.log('This is the Netlify deploy-succeeded trigger function');
-  // Go back a directory since this is where included files are added
-  const testData = fs.readFileSync(path.resolve(__dirname, '../build-hook.txt'));
+  // Has to be relative to where included_file originates from
+  const testData = fs.readFileSync(path.resolve(__dirname, '../../build-hook.txt'));
   const incomingHookBody = process.env.INCOMING_HOOK_BODY;
   console.log({ event, incomingHookBody, testData });
 }
