ec4c26776a0d4ef9d58622728ac66fdd7fda4ff9
rayangler <27821750+rayangler@users.noreply.github.com>
2024-02-21 16:29:08 -0500

    Test getting INCOMING_HOOK_BODY

--- netlify/README.md
+++ netlify/README.md
@@ -1,5 +1,5 @@
 Content in this folder is intended to be used by builds on Netlify only.
 
 The functions folder exists to host custom functions that are automatically triggered when one of Netlify's
-events match the name of the function file. See: https://docs.netlify.com/functions/trigger-on-events/ for 
+events match the name of the function file. See: https://docs.netlify.com/functions/trigger-on-events/ for
 more information.
--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -1,4 +1,9 @@
-export async function handler(event, context) {
+import fs from 'fs';
+import path from 'path';
+
+export async function handler(event) {
+  const testData = fs.readFileSync(path.resolve(__dirname, './build-hook.txt'));
   console.log('This is the Netlify deploy-succeeded trigger function');
-  console.log({ event, context });
+  const incomingHookBody = process.env.INCOMING_HOOK_BODY;
+  console.log({ event, incomingHookBody, testData });
 }
