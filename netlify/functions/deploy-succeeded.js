4ce91e8920b9d0786b7deaaec6d49c064f31a321
rayangler <27821750+rayangler@users.noreply.github.com>
2024-02-22 11:39:51 -0500

    Test calling post-build webhook

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -1,10 +1,9 @@
 import fs from 'fs';
 import path from 'path';
-// import { callPostBuildWebhook } from '../../plugins/gatsby-source-snooty-preview/utils/post-build';
+import { callPostBuildWebhook } from '../../plugins/gatsby-source-snooty-preview/utils/post-build';
 
 export async function handler(event, _context) {
   console.log('This is the Netlify deploy-succeeded trigger function');
-  console.log({ preview: process.env.GATSBY_IS_PREVIEW });
 
   // This file does not currently exist, but should be created on Netlify builds. The INCOMING_HOOK_BODY env var
   // is not automatically passed along, so we use a txt file to save it
@@ -19,5 +18,10 @@ export async function handler(event, _context) {
   console.log({ event, buildHookData, body });
   console.log(Object.keys(event));
 
-  // callPostBuildWebhook(parsedTestData);
+  const resPayload = {
+    netlifyBody: body,
+    ...buildHookData,
+  };
+
+  callPostBuildWebhook(resPayload, 'success');
 }
