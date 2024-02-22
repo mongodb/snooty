3a05d33c2c4c0f2733d84cf0c62340f5cb0c4e2e
rayangler <27821750+rayangler@users.noreply.github.com>
2024-02-22 10:40:53 -0500

    Prevent error

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -4,9 +4,14 @@ import path from 'path';
 
 export async function handler(event, _context) {
   console.log('This is the Netlify deploy-succeeded trigger function');
+
   // This file does not currently exist, but should be created on Netlify builds. The INCOMING_HOOK_BODY env var
   // is not automatically passed along, so we use a txt file to save it
   const buildHookDataString = fs.readFileSync(path.resolve(__dirname, '../../build-hook.txt'), 'utf-8');
+  if (!buildHookDataString) {
+    return;
+  }
+
   const buildHookData = JSON.parse(buildHookDataString);
   const { body } = event;
   console.log({ event, buildHookData, body });
