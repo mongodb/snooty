69e9e567beb95af8b0198ac16833288cf43fb5fd
rayangler <27821750+rayangler@users.noreply.github.com>
2024-02-22 11:50:14 -0500

    Fix status

--- netlify/functions/deploy-succeeded.js
+++ netlify/functions/deploy-succeeded.js
@@ -23,5 +23,5 @@ export async function handler(event, _context) {
     ...buildHookData,
   };
 
-  await callPostBuildWebhook(resPayload, 'success');
+  await callPostBuildWebhook(resPayload, 'completed');
 }
