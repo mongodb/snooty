400338de472a166b687dc551dfee4ddd8c249248
rayangler <27821750+rayangler@users.noreply.github.com>
2024-02-23 17:01:54 -0500

    Change relative path

--- netlify/utils.js
+++ netlify/utils.js
@@ -8,7 +8,7 @@ import path from 'path';
 const parseBuildHookData = () => {
   // This file does not currently exist, but should be created on Netlify builds as part of `npm run build:netlify`.
   // The INCOMING_HOOK_BODY env var is not automatically passed along to functions, so we use a txt file to save it
-  const relativeFilePath = '../../build-hook.txt';
+  const relativeFilePath = '../build-hook.txt';
   const buildHookDataString = fs.readFileSync(path.resolve(__dirname, relativeFilePath), 'utf-8');
   if (!buildHookDataString) {
     console.log('No build hook data found.');
