3281091a3e6118658490a24fc242a304ddc49c24
Matt Meigs <matt.meigs@mongodb.com>
2024-03-12 10:36:38 -0400

    add dotenv to lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,14 +1,20 @@
+const runningEnv = process.env.NODE_ENV || 'production';
+
+require('dotenv').config({
+  path: `.env.${runningEnv}`,
+});
+
 module.exports = {
   ci: {
     collect: {
       staticDistDir: './public',
       startServerCommand: 'npm run serve',
-      url: ['http://localhost:8080', 'http://mongodb.com/docs'],
+      url: ['http://localhost:8080'],
     },
     upload: {
       target: 'lhci',
       serverBaseUrl: 'http://localhost:9001',
-      token: '3695a47b-03b2-4bd0-9d9b-53039f3f9d51', // TODO: need to use env var
+      token: process.env.LHCI_BUILD_TOKEN,
     },
   },
 };
