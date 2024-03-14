127308649ddbc74a4b6b85caa8ad5cdb8b20ddb1
rayangler <27821750+rayangler@users.noreply.github.com>
2024-03-14 14:50:43 -0400

    DOP-4457: Strip away locale code from slugs before localizing them

--- tests/unit/utils/locale.test.js
+++ tests/unit/utils/locale.test.js
@@ -25,8 +25,8 @@ describe('localizePath', () => {
 
   it.each([
     ['/', '/zh-cn/'],
-    ['/page/slug', '/zh-cn/page/slug/'],
-    ['page/slug', 'zh-cn/page/slug/'],
+    ['/page/slug', '/zh-cn/page/slug'],
+    ['page/slug/', 'zh-cn/page/slug/'],
   ])('returns localized path when no code is set', (slug, expectedRes) => {
     // Pretend page exists on translated site
     windowSpy.mockImplementation(() => ({
@@ -40,8 +40,8 @@ describe('localizePath', () => {
 
   it.each([
     ['/', 'ko-kr', '/ko-kr/'],
-    ['/page/slug', 'pt-br', '/pt-br/page/slug/'],
-    ['page/slug', 'zh-cn', 'zh-cn/page/slug/'],
+    ['/page/slug', 'pt-br', '/pt-br/page/slug'],
+    ['page/slug/', 'zh-cn', 'zh-cn/page/slug/'],
   ])('returns localized path when code is set', (slug, code, expectedRes) => {
     const res = localizePath(slug, code);
     expect(res).toEqual(expectedRes);
@@ -49,8 +49,8 @@ describe('localizePath', () => {
 
   it.each([
     ['/', '/'],
-    ['/page/slug', '/page/slug/'],
-    ['page/slug', 'page/slug/'],
+    ['/page/slug', '/page/slug'],
+    ['page/slug/', 'page/slug/'],
   ])('returns the same page slug when English is found by default', (slug, expectedRes) => {
     const res = localizePath(slug);
     expect(res).toEqual(expectedRes);
@@ -58,10 +58,22 @@ describe('localizePath', () => {
 
   it.each([
     ['/', '/'],
-    ['/page/slug', '/page/slug/'],
-    ['page/slug', 'page/slug/'],
+    ['/page/slug', '/page/slug'],
+    ['page/slug/', 'page/slug/'],
   ])('gracefully defaults to English path when invalid language is passed in', (slug, expectedRes) => {
     const res = localizePath(slug, 'beep-boop');
+    expect(res).toEqual(expectedRes);
+  });
+
+  it.each([
+    ['zh-cn/docs', 'zh-cn/docs'],
+    ['/ko-kr/docs/foo', '/zh-cn/docs/foo'],
+    ['/pt-br/', '/zh-cn/'],
+    ['//ko-kr/', '/zh-cn/'],
+    ['pt-br/', 'zh-cn/'],
+    ['pt-br/docs/pt-br', 'zh-cn/docs/pt-br'],
+  ])('handles slugs that already have a locale code', (slug, expectedRes) => {
+    const res = localizePath(slug, 'zh-cn');
     expect(res).toEqual(expectedRes);
   });
 });
