cbfd7db5c7564ff6f1d0fbbcda18ffa7372d34f7
branberry <brandon.ly@mongodb.com>
2024-03-25 10:03:03 -0500

    [DOP-4437]: Remove gatsby bundle analyzer

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,6 @@ const plugins = [
   `gatsby-plugin-image`,
   `gatsby-plugin-sharp`,
   `gatsby-transformer-sharp`, // Needed for dynamic images
-  { resolve: 'gatsby-plugin-webpack-bundle-analyser-v2', options: { analyzerMode: 'static' } },
   {
     resolve: 'gatsby-source-filesystem',
     options: {
