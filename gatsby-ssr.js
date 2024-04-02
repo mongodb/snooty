9ec15de123be50a470683118932b25fd01a2fe2d
Caesar Bell <caesar.bell@mongodb.com>
2024-04-02 13:57:50 -0400

    :package: Don't need the package

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -25,13 +25,8 @@ export const onRenderBody = ({ setHeadComponents }) => {
     />,
     <link rel="preconnect" href="https://fonts.googleapis.com" />,
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />,
-    <link rel="preconnect" href="https://d2va9gm4j17fy9.cloudfront.net" />,
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@100..900&display=swap" rel="stylesheet" />,
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap" rel="stylesheet" />,
-    <link
-      href="https://d2va9gm4j17fy9.cloudfront.net/fonts/euclid-circular/EuclidCircularA-Semibold-WebXL.woff&display=swap"
-      rel="stylesheet"
-    />,
   ]);
 };
 
