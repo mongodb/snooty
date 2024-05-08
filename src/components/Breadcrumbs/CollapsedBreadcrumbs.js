8e2a49602f5c41244c512e0fa990b2cc49696981
Matt Meigs <matt.meigs@mongodb.com>
2024-05-08 09:27:14 -0400

    log and change collapsedBreadcrumbs

--- src/components/Breadcrumbs/CollapsedBreadcrumbs.js
+++ src/components/Breadcrumbs/CollapsedBreadcrumbs.js
@@ -2,10 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Menu, MenuItem } from '@leafygreen-ui/menu';
 import IconButton from '@leafygreen-ui/icon-button';
+import { withPrefix } from 'gatsby';
 import Icon from '@leafygreen-ui/icon';
 import { formatText } from '../../utils/format-text';
 
 const CollapsedBreadcrumbs = ({ crumbs }) => {
+  console.log(
+    'collapsed crumb example turned using withPrefix ',
+    crumbs.length ? `${crumbs[crumbs.length - 1].url} -> ${withPrefix(crumbs[crumbs.length - 1].url)}` : 'none'
+  );
+
   return (
     <React.Fragment>
       <Menu
@@ -18,7 +24,7 @@ const CollapsedBreadcrumbs = ({ crumbs }) => {
         }
       >
         {crumbs.map((crumb, index) => (
-          <MenuItem key={index} href={crumb.url}>
+          <MenuItem key={index} href={withPrefix(crumb.url)}>
             {formatText(crumb.title)}
           </MenuItem>
         ))}
