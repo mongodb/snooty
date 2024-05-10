7f6c305d9549f21d41fe15db5fbe81d2754cf178
Matt Meigs <matt.meigs@mongodb.com>
2024-05-10 10:00:35 -0400

    getGatsbyPreviewLink util

--- src/components/Breadcrumbs/CollapsedBreadcrumbs.js
+++ src/components/Breadcrumbs/CollapsedBreadcrumbs.js
@@ -7,25 +7,14 @@ import { useLocation } from '@gatsbyjs/reach-router';
 import Icon from '@leafygreen-ui/icon';
 import { formatText } from '../../utils/format-text';
 import { isGatsbyPreview } from '../../utils/is-gatsby-preview';
+import { getGatsbyPreviewLink } from '../../utils/get-gatsby-preview-link';
 
 const CollapsedBreadcrumbs = ({ crumbs }) => {
   const location = useLocation();
 
   const menuItems = crumbs.map((crumb, index) => {
     let to = withPrefix(crumb.path);
-    if (isGatsbyPreview()) {
-      // If we're in preview mode, we build the pages of each project and branch of the site within
-      // its own namespace so each author can preview their own pages e.g.
-      // /project1/branch1/doc-path
-      // /project2/branch2/doc-path
-      //
-      // So to navigate with the namespaced site, we add to each link the current project and branch
-      // the user is browsing in.
-      const projectAndBranchPrefix = `/` + location.pathname.split(`/`).slice(1, 3).join(`/`);
-      if (!to.startsWith(projectAndBranchPrefix)) {
-        to = projectAndBranchPrefix + to;
-      }
-    }
+    if (isGatsbyPreview()) to = getGatsbyPreviewLink(to, location);
 
     return (
       <MenuItem key={index} href={to}>
