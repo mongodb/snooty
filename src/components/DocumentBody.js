f3887f4fb7955788a0270059059d4951f7ab280e
Seung Park <seung.park@mongodb.com>
2024-05-24 16:21:14 -0400

    remove test

--- src/components/DocumentBody.js
+++ src/components/DocumentBody.js
@@ -140,10 +140,6 @@ const DocumentBody = (props) => {
           #template-container *:not(:is(code, code *)) {
             font-family: ${fontFamily};
           }
-          // TESTING initial load background
-          // html.dark-theme body {
-          //   background: black
-          // }
         `}
       />
     </>
