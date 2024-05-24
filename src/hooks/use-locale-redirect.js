88da84faca8a2d20464daef1dc7271b9d908fbbd
rayangler <27821750+rayangler@users.noreply.github.com>
2024-05-24 17:53:27 -0400

    Memoize the search for a language

--- src/hooks/use-locale-redirect.js
+++ src/hooks/use-locale-redirect.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { withPrefix } from 'gatsby';
 import { AVAILABLE_LANGUAGES, getCurrLocale, localizePath } from '../utils/locale';
 import { STORAGE_KEY_PREF_LOCALE, getLocalValue } from '../utils/browser-storage';
@@ -8,39 +8,51 @@ import { STORAGE_KEY_PREF_LOCALE, getLocalValue } from '../utils/browser-storage
  * @param {string} slug The raw slug of the site page. Does not include the site's path prefix
  */
 export const useLocaleRedirect = (slug) => {
-  // Determine if a redirect is needed based on browser language
-  useEffect(() => {
-    if (typeof navigator === undefined) {
-      return;
+  // Memoize a user's preferred supported non-English language to avoid the need to check it across multiple pages
+  const preferredLang = useMemo(() => {
+    if (typeof navigator === 'undefined') {
+      return undefined;
     }
 
-    const { languages } = navigator;
+    const normalizedLangs = new Set();
+    // Normalize lang code in case of same language, but different region. For example:
+    // "zh-CN" is used for Simplified Chinese, but "zh-TW" is used for Traditional Chinese.
+    // Both should be treated as "zh" for now. Same applies to different regions for English
+    navigator.languages.forEach((lang) => {
+      const normlizedLang = lang.split('-')[0];
+      normalizedLangs.add(normlizedLang);
+    });
+
+    // Languages should be in order of browser priority
+    for (const lang of normalizedLangs) {
+      // English is highly preferred over other languages, so we don't need to return anything
+      if (lang === 'en') {
+        return undefined;
+      }
+
+      // Check to see if browser's language is one that is already translated
+      const foundLanguage = AVAILABLE_LANGUAGES.find(({ langCode }) => lang === langCode);
+      if (foundLanguage) {
+        return foundLanguage;
+      }
+    }
+  }, []);
+
+  // Determine if a redirect is needed based on browser language
+  useEffect(() => {
     const currLocale = getCurrLocale();
     const storedPrefLocale = getLocalValue(STORAGE_KEY_PREF_LOCALE);
     // Only redirect based on language if site is in English
-    if (currLocale !== 'en-us' || !languages || storedPrefLocale === 'en-us') {
+    if (currLocale !== 'en-us' || storedPrefLocale === 'en-us') {
       return;
     }
 
-    // languages should be in order of browser priority
-    for (const lang of languages) {
-      // Normalize lang code in case of same language, but different region. For example:
-      // "zh-CN" is used for Simplified Chinese, but "zh-TW" is used for Traditional Chinese.
-      // Both should be treated as "zh" for now. Same applies to different regions for English
-      const normalizedLang = lang.split('-')[0];
-      // English is already a preferred language, so we should not attempt to redirect
-      if (normalizedLang === 'en') {
-        break;
-      }
-
-      const foundLanguage = AVAILABLE_LANGUAGES.find(({ langCode }) => normalizedLang === langCode);
-      if (foundLanguage) {
-        // Redirect user to expected page
-        const fullPageSlug = withPrefix(slug);
-        const targetRedirect = localizePath(fullPageSlug, foundLanguage.localeCode);
-        window.location.href = targetRedirect;
-        break;
-      }
+    // preferredLang should be a non-English language
+    if (preferredLang) {
+      // Redirect user to expected page
+      const fullPageSlug = withPrefix(slug);
+      const targetRedirect = localizePath(fullPageSlug, preferredLang.localeCode);
+      window.location.href = targetRedirect;
     }
-  }, [slug]);
+  }, [slug, preferredLang]);
 };
