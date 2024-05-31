8ad5f0397bbf7dfea94e3bb1662b04ce6bf71fdc
Seung Park <seung.park@mongodb.com>
2024-05-31 17:13:52 -0400

    add computer icon and close menu when selecting one item

--- src/components/ActionBar/DarkModeDropdown.js
+++ src/components/ActionBar/DarkModeDropdown.js
@@ -1,10 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import { cx, css } from '@leafygreen-ui/emotion';
+import Box from '@leafygreen-ui/box';
 import Icon from '@leafygreen-ui/icon';
 import IconButton from '@leafygreen-ui/icon-button';
+import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
 import { Menu, MenuItem } from '@leafygreen-ui/menu';
 import { DarkModeContext } from '../../context/dark-mode-context';
 import { theme } from '../../theme/docsTheme';
+import IconComputer from '../icons/Computer';
 
 const IconStyling = css`
   cursor: pointer;
@@ -18,6 +21,17 @@ const MenuStyling = css`
 const DarkModeDropdown = ({ ...props }) => {
   // not using dark mode from LG/provider here to account for case of 'system' dark theme
   const { setDarkModePref, darkModePref } = useContext(DarkModeContext);
+  const { darkMode } = useDarkMode();
+
+  const [open, setOpen] = useState(false);
+
+  const select = useCallback(
+    (selectedPref) => {
+      setDarkModePref(selectedPref);
+      setOpen(false);
+    },
+    [setDarkModePref, setOpen]
+  );
 
   return (
     <Menu
@@ -25,35 +39,42 @@ const DarkModeDropdown = ({ ...props }) => {
       usePortal={false}
       justify={'start'}
       align={'bottom'}
+      open={open}
+      setOpen={setOpen}
       trigger={
-        <IconButton className={cx(IconStyling)} aria-label="Dark Mode Menu" aria-labelledby="Dark Mode Menu">
-          <Icon glyph={darkModePref === 'system' ? 'Laptop' : darkModePref === 'dark-theme' ? 'Moon' : 'Sun'} />
+        // using Box here to prevent warning of Button within Button
+        // since we are using usePortal=false to mitigate sticky header
+        <IconButton as={Box} className={cx(IconStyling)} aria-label="Dark Mode Menu" aria-labelledby="Dark Mode Menu">
+          {darkModePref === 'system' ? (
+            <IconComputer darkMode={darkMode} />
+          ) : (
+            <Icon glyph={darkModePref === 'dark-theme' ? 'Moon' : 'Sun'} />
+          )}
         </IconButton>
       }
     >
       <MenuItem
         active={darkModePref === 'light-theme'}
-        onClick={() => setDarkModePref('light-theme')}
+        onClick={() => select('light-theme')}
         glyph={<Icon glyph={'Sun'} />}
       >
         Light
       </MenuItem>
       <MenuItem
         active={darkModePref === 'dark-theme'}
-        onClick={() => setDarkModePref('dark-theme')}
+        onClick={() => select('dark-theme')}
         glyph={<Icon glyph={'Moon'} />}
       >
         Dark
       </MenuItem>
       <MenuItem
         active={darkModePref === 'system'}
-        onClick={() => setDarkModePref('system')}
-        glyph={<Icon glyph={'Laptop'} />}
+        onClick={() => select('system')}
+        glyph={<IconComputer darkMode={darkMode} />}
       >
         System
       </MenuItem>
     </Menu>
-    // </PortalContainer>
   );
 };
 
