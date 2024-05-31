2cbee10891f0a0e557a74906492efb49e439bfa8
Seung Park <seung.park@mongodb.com>
2024-05-31 17:22:29 -0400

    revert back initial state to useeffect bc ssg...

--- src/context/dark-mode-context.js
+++ src/context/dark-mode-context.js
@@ -25,15 +25,7 @@ const DarkModeContextProvider = ({ children }) => {
   const docClassList = useMemo(() => isBrowser && window?.document?.documentElement?.classList, []);
 
   // darkModePref   {str}   'light-theme' || 'dark-theme' || 'system';
-  const [darkModePref, setDarkModePref] = useState(() => {
-    if (!isBrowser) return LIGHT_THEME_CLASSNAME;
-
-    return docClassList.contains(SYSTEM_THEME_CLASSNAME)
-      ? SYSTEM_THEME_CLASSNAME
-      : docClassList.contains(DARK_THEME_CLASSNAME)
-      ? DARK_THEME_CLASSNAME
-      : LIGHT_THEME_CLASSNAME;
-  });
+  const [darkModePref, setDarkModePref] = useState(() => 'light-theme');
   const loaded = useRef();
 
   // update document class list to apply dark-theme/light-theme to whole document
@@ -64,6 +56,22 @@ const DarkModeContextProvider = ({ children }) => {
     setLocalValue('theme', darkModePref);
     updateDocumentClasslist(darkModePref, darkPref);
   }, [darkModePref, updateDocumentClasslist, darkPref]);
+
+  useEffect(() => {
+    if (!isBrowser) return;
+
+    // NOTE: client side read of darkmode from document classnames
+    // which is derived from local storage (see gatsby-ssr script).
+    // This occurs after component mounts, not during build time
+    setDarkModePref(
+      docClassList.contains(SYSTEM_THEME_CLASSNAME)
+        ? SYSTEM_THEME_CLASSNAME
+        : docClassList.contains(DARK_THEME_CLASSNAME)
+        ? DARK_THEME_CLASSNAME
+        : LIGHT_THEME_CLASSNAME
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <DarkModeContext.Provider value={{ setDarkModePref, darkModePref }}>
