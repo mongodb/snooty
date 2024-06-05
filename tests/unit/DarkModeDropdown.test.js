60185d0d50c1764354f59b2c928ae3f3bdaa4c79
Seung Park <seungpark@users.noreply.github.com>
2024-06-05 10:06:27 -0400

    DOP-4713 unit test (#1111)

---
+++ tests/unit/DarkModeDropdown.test.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { render, act } from '@testing-library/react';
+import LeafyGreenProvider from '@leafygreen-ui/leafygreen-provider';
+import { DarkModeContext } from '../../src/context/dark-mode-context';
+import * as MediaHooks from '../../src/hooks/use-media';
+import DarkModeDropdown from '../../src/components/ActionBar/DarkModeDropdown';
+
+let darkModePref = 'light-theme';
+
+const setDarkModePref = jest.fn((value) => {
+  darkModePref = value;
+});
+
+// mock useMedia
+jest.spyOn(MediaHooks, 'default').mockImplementation(() => ({}));
+
+// mock window.localStorage
+const storage = {};
+jest.spyOn(window.localStorage.__proto__, 'setItem').mockImplementation((key, value) => {
+  storage[key] = value;
+});
+
+const mountDarkModeDropdown = () => {
+  return render(
+    <DarkModeContext.Provider value={{ setDarkModePref, darkModePref }}>
+      <LeafyGreenProvider baseFontSize={16} darkMode={darkModePref === 'dark-theme'}>
+        <DarkModeDropdown />
+      </LeafyGreenProvider>
+    </DarkModeContext.Provider>
+  );
+};
+
+describe('DarkMode Dropdown component', () => {
+  it('renders dark mode dropdown', async () => {
+    // first snapshot of closed menu
+    const elm = mountDarkModeDropdown();
+    expect(elm.asFragment()).toMatchSnapshot();
+    const button = await elm.findByLabelText('Dark Mode Menu');
+    await act(async () => {
+      button.click();
+    });
+    // second snapshot of open menu
+    expect(elm.asFragment()).toMatchSnapshot();
+  });
+
+  // test it changes to system and dark mode
+  it('updates dark mode when selecting a different option', async () => {
+    const elm = mountDarkModeDropdown();
+    const button = await elm.findByLabelText('Dark Mode Menu');
+    await act(async () => {
+      button.click();
+    });
+    const darkModeSelections = await elm.findAllByRole('menuitem');
+    await act(async () => {
+      darkModeSelections[1].click();
+    });
+    expect(darkModePref).toBe('dark-theme');
+    const darkElm = mountDarkModeDropdown();
+    expect(darkElm.asFragment()).toMatchSnapshot();
+  });
+});
