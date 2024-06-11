7593429d024e60f1022b2026fe1ef1b548cc7d56
rayangler <27821750+rayangler@users.noreply.github.com>
2024-06-11 17:31:59 -0400

    Try different things

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -104,6 +104,60 @@ const Heading = ({ sectionDepth, nodeData, ...rest }) => {
           </ul>
         </div>
       )}
+      {isPageTitle && (
+        <div className="sl_opaque">
+          Language Selector
+          <ul>
+            <li>
+              <a className="sl_opaque" href="https://www.mongodb.com/docs/">
+                English
+              </a>
+            </li>
+            <li>
+              <a className="sl_opaque" href="https://www.mongodb.com/pt-br/docs/">
+                Português
+              </a>
+            </li>
+            <li>
+              <a className="sl_opaque" href="https://www.mongodb.com/ko-kr/docs/">
+                한국어
+              </a>
+            </li>
+            <li>
+              <a className="sl_opaque" href="https://www.mongodb.com/zh-cn/docs/">
+                简体中文
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+      {isPageTitle && (
+        <div>
+          Language Selector
+          <ul>
+            <li>
+              <a className="sl_opaque" href="https://www.mongodb.com/docs/">
+                English
+              </a>
+            </li>
+            <li>
+              <a className="sl_opaque" href="https://www.mongodb.com/pt-br/docs/">
+                Português
+              </a>
+            </li>
+            <li>
+              <a className="sl_opaque" href="https://www.mongodb.com/ko-kr/docs/">
+                한국어
+              </a>
+            </li>
+            <li>
+              <a className="sl_opaque" href="https://www.mongodb.com/zh-cn/docs/">
+                简体中文
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </>
   );
 };
