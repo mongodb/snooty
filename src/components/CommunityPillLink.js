fae45eec117d6b5cf456adf1a9ff6fbad77da78a
Caesar Bell <caesar.bell@mongodb.com>
2024-06-12 16:04:16 -0400

    :sparkles: DOP-4662 adds community pill to cards for when using tags

--- src/components/CommunityPillLink.js
+++ src/components/CommunityPillLink.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Badge from '@leafygreen-ui/badge';
+import Badge, { Variant } from '@leafygreen-ui/badge';
 import { getPlaintext } from '../utils/get-plaintext';
 import Link from './Link';
 
-const CommunityPillLink = ({
-  nodeData: {
-    argument,
-    options: { url },
-  },
-}) => {
+const communityPillVariants = {
+  darkGray: Variant.DarkGray,
+  lightGray: Variant.LightGray,
+  red: Variant.Red,
+  yellow: Variant.Yellow,
+  blue: Variant.Blue,
+  green: Variant.Green,
+};
+
+/**
+ * CommunityPillLink component
+ *
+ * @param {Object} param0 - The parameters object
+ * @param {Object} param0.nodeData - Data for the node
+ * @param {String} param0.nodeData.argument - Text to display for Link
+ * @param {Object} [param0.nodeData.options] - Options for the node data
+ * @param {string} [param0.nodeData.options.url] - URL in the options
+ * @param {string} param0.variant - Variant of the community pill link
+ * @param {string} [param0.text='community built'] - Text to display, defaults to 'community built'
+ * @returns {JSX.Element} The rendered CommunityPillLink component
+ */
+const CommunityPillLink = ({ nodeData, variant, text = 'community built' }) => {
+  const { argument, options: { url } = {} } = nodeData || {};
+
   return (
     <div>
-      <Link to={url}>{getPlaintext(argument)}</Link>
-      <Badge variant="lightgray">{'community built'}</Badge>
+      {nodeData && argument && url && <Link to={url}>{getPlaintext(argument)}</Link>}
+      <Badge variant={communityPillVariants[variant] || Variant.LightGray}>{text}</Badge>
     </div>
   );
 };
@@ -22,7 +40,9 @@ CommunityPillLink.propTypes = {
   nodeData: PropTypes.shape({
     argument: PropTypes.arrayOf(PropTypes.object).isRequired,
     options: PropTypes.shape({ url: PropTypes.string.isRequired }),
-  }).isRequired,
+  }),
+  variant: PropTypes.oneOf(Object.values(communityPillVariants)),
+  text: PropTypes.string,
 };
 
 export default CommunityPillLink;
