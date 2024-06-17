f3e750eb1c4bb23518160a0f41b116c851f25152
mmeigs <matt.meigs@mongodb.com>
2024-06-17 10:36:02 -0400

    remove lighthouse code

--- lighthouserc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  ci: {
-    upload: {
-      target: 'lhci',
-      serverBaseUrl: process.env.LIGHTHOUSE_SERVER_URL,
-      token: process.env.LIGHTHOUSE_BUILD_TOKEN,
-    },
-  },
-};
