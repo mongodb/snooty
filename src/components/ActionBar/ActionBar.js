3995b785e3c0a16e20aa2bd29f937609d02573f4
Caesar Bell <caesar.bell@mongodb.com>
2024-06-18 14:16:01 -0400

    :iphone: DOP-4624 adds responsiveness to the action bar

--- src/components/ActionBar/ActionBar.js
+++ src/components/ActionBar/ActionBar.js
@@ -12,25 +12,55 @@ const ActionBarContainer = styled('div')`
   padding-top: ${theme.size.small};
   padding-bottom: ${theme.size.small};
   padding-right: ${theme.size.large};
-  height: 96px;
+  height: auto;
   width: 100%;
   position: sticky;
   top: 0;
+  flex-wrap: wrap;
   z-index: ${theme.zIndexes.header};
   background-color: ${(props) => (props.darkMode ? palette.black : palette.white)};
   border-bottom: 1px solid ${(props) => (props.darkMode ? palette.gray.dark2 : palette.gray.light2)};
+
+  @media ${theme.screenSize.mediumAndUp} {
+    & > div {
+      flex: 0 1 auto;
+    }
+  }
+
+  @media ${theme.screenSize.upToMedium} {
+    justify-content: space-between;
+    padding-right: 0;
+  }
 `;
 
 const ActionBarSearchContainer = styled.div`
   align-items: center;
   display: flex;
   width: 80%;
+
+  @media ${theme.screenSize.upToMedium} {
+    width: 100%;
+  }
+
+  @media ${theme.screenSize.upToSmall} {
+    & > div {
+      padding: ${theme.size.default} 32px;
+    }
+  }
 `;
 
 const ActionsBox = styled('div')`
   display: flex;
   align-items: center;
   column-gap: ${theme.size.default};
+
+  @media ${theme.screenSize.upToMedium} {
+    padding-left: 3rem;
+  }
+
+  @media ${theme.screenSize.upToSmall} {
+    padding-left: 2rem;
+  }
 `;
 
 const ActionBar = ({ ...props }) => {
