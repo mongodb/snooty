0d5374a3ab5164eb6d6927957bfebd640b9a27e0
Caesar Bell <caesar.bell@mongodb.com>
2024-06-25 17:30:13 -0400

    :art: DOP-4780 overrides the weblink-link any styles for dark mode

--- src/components/Footnote/FootnoteReference.js
+++ src/components/Footnote/FootnoteReference.js
@@ -1,7 +1,21 @@
 import React, { useContext } from 'react';
+import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
+import { css } from '@emotion/react';
+import { palette } from '@leafygreen-ui/palette';
 import PropTypes from 'prop-types';
 import { getNestedValue } from '../../utils/get-nested-value';
 import FootnoteContext from './footnote-context';
+
+const refStyles = (darkMode) => css`
+  ${darkMode &&
+  `
+      color: ${palette.blue.light1};
+
+      &:visited {
+        color: ${palette.purple.base};
+      }
+    `}
+`;
 
 /**
  * Component used to show a clickable reference to footnote on page
@@ -9,6 +23,7 @@ import FootnoteContext from './footnote-context';
  */
 const FootnoteReference = ({ nodeData: { id, refname } }) => {
   const { footnotes } = useContext(FootnoteContext);
+  const { darkMode } = useDarkMode();
 
   // the nodeData originates from docutils, and may be incorrect for
   // anonymous footnoteReferences originating from included files -- docutils
@@ -16,8 +31,14 @@ const FootnoteReference = ({ nodeData: { id, refname } }) => {
 
   const ref = refname || id.replace('id', '');
   const uid = refname ? `${refname}-${id}` : id;
+
   return (
-    <a className="footnote-reference header-buffer" href={`#footnote-${ref}`} id={`ref-${uid}`}>
+    <a
+      className="footnote-reference header-buffer"
+      css={refStyles(darkMode)}
+      href={`#footnote-${ref}`}
+      id={`ref-${uid}`}
+    >
       [{getNestedValue([ref, 'label'], footnotes) || ref}]
     </a>
   );
