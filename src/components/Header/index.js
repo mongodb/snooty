625679966bccf47182e5b9cab992c773f8f02b1c
Maya Raman <maya.raman19@gmail.com>
2024-07-23 15:08:41 -0400

    banner height dynamic

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { UnifiedNav } from '@mdb/consistent-nav';
@@ -6,13 +6,13 @@ import { SidenavMobileMenuDropdown } from '../Sidenav';
 import SiteBanner from '../Banner/SiteBanner';
 import { theme } from '../../theme/docsTheme';
 import { AVAILABLE_LANGUAGES, getCurrLocale, onSelectLocale } from '../../utils/locale';
+import { HeaderContext } from './header-context';
 
 const StyledHeaderContainer = styled.header(
   (props) => `
   grid-area: header;
   top: 0;
-  // TODO : CHANGE THIS
-  margin-top: 40px;
+  margin-top: ${props.hasBanner ? theme.header.bannerHeight : '0px'};
   z-index: ${theme.zIndexes.header};
   ${
     props.template === 'landing' || props.template === 'errorpage' || process.env['GATSBY_ENABLE_DARK_MODE'] === 'true'
@@ -26,10 +26,12 @@ const Header = ({ sidenav, eol, template }) => {
   const unifiedNavProperty = 'DOCS';
 
   const enabledLocales = AVAILABLE_LANGUAGES.map((language) => language.localeCode);
+  const { bannerContent } = useContext(HeaderContext);
+
   return (
     <>
       <SiteBanner />
-      <StyledHeaderContainer template={template}>
+      <StyledHeaderContainer template={template} hasBanner={!!bannerContent}>
         <>
           {!eol && (
             <UnifiedNav
