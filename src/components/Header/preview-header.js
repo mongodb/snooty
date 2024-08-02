e104d20c8ecf9e9997eaf16bd711fce9100412b8
mmeigs <matt.meigs@mongodb.com>
2024-08-02 12:14:11 -0400

    preview header styles

--- src/components/Header/preview-header.js
+++ src/components/Header/preview-header.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import { palette } from '@leafygreen-ui/palette';
 import { SidenavMobileMenuDropdown } from '../Sidenav';
 import SiteBanner from '../Banner/SiteBanner';
 import { theme } from '../../theme/docsTheme';
+import { HeaderContext } from './header-context';
 
-const StyledHeaderContainer = styled.header`
+const StyledHeaderContainer = styled.header(
+  (props) => `
   grid-area: header;
-  position: sticky;
   top: 0;
-  z-index: 10;
-`;
+  margin-top: ${props.hasBanner ? theme.header.bannerHeight : '0px'};
+  z-index: ${theme.zIndexes.header};
+  ${props.template === 'landing' || props.template === 'errorpage' ? '' : 'position: sticky;'}
+
+  --breadcrumb-color: ${palette.gray.dark1};
+  .dark-theme & {
+    --breadcrumb-color: ${palette.gray.light1};
+  }
+
+
+  nav {
+    background-
+  }
+  `
+);
 
 const StyledUnifiedNavPlaceholder = styled.nav`
   width: 100%;
   height: ${theme.header.navbarHeight};
   display: flex;
   align-items: center;
-  border-bottom: 1px solid #b8c4c2;
-  background-color: white;
+
+  --background-color: white;
+  --border-color: #b8c4c2;
+  .dark-theme & {
+    --background-color: ${palette.black};
+    --border-color: ${palette.gray.dark2};
+  }
+  background-color: var(--background-color);
+  border-bottom: 1px solid var(--border-color);
 
   @media ${theme.screenSize.upToLarge} {
     height: ${theme.header.navbarMobileHeight};
@@ -34,8 +56,10 @@ const StyledStagingWarning = styled.p`
 `;
 
 const Header = ({ sidenav }) => {
+  const { bannerContent } = useContext(HeaderContext);
+
   return (
-    <StyledHeaderContainer>
+    <StyledHeaderContainer hasBanner={!!bannerContent}>
       <SiteBanner />
       <>
         <StyledUnifiedNavPlaceholder>
