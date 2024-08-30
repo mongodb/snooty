2b7086914943fa7d5fa01bab74ce2232e124c595
rayangler <27821750+rayangler@users.noreply.github.com>
2024-08-30 14:57:28 -0400

    Make decorative line more flexible

--- src/components/MethodSelector/MethodSelector.js
+++ src/components/MethodSelector/MethodSelector.js
@@ -1,42 +1,36 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { RadioBox, RadioBoxGroup } from '@leafygreen-ui/radio-box-group';
 import { css, cx } from '@leafygreen-ui/emotion';
 import { palette } from '@leafygreen-ui/palette';
 import { theme } from '../../theme/docsTheme';
 import { getLocalValue, setLocalValue } from '../../utils/browser-storage';
 import { reportAnalytics } from '../../utils/report-analytics';
-import { SidenavContext } from '../Sidenav';
 import MethodOptionContent from './MethodOptionContent';
 
 const STORAGE_KEY = 'methodSelectorId';
+// Padding to help selected option's box shadow stay visible while keeping overflow hidden for container
+const OPTIONS_GRID_PADDING = '3px';
+const OPTIONS_GAP = theme.size.default;
 
-/**
- * Helper function to determine if options will wrap or not
- * @param {number} count
- * @returns {string}
- */
-const whenWrappingDesktop = (count) => {
-  // Hardcoding this will make it easier to have accurate media queries, without relying on client-side JS and potentially
-  // creating content layout shifts.
-  // Only wraps when there's 4+ options are found
-  const singleRowScreenSizeBP = {
-    4: 1028,
-    5: 1134,
-    6: 1240,
-  };
-  return `@media only screen and (min-width: ${theme.breakpoints.large + 1}px) and (max-width: ${
-    singleRowScreenSizeBP[count] - 1
-  }px)`;
-};
+// Provides enough height for 2 rows of method options. Allows the decorative line to be hidden
+// when the options start wrapping
+const optionsContainer = css`
+  @media ${theme.screenSize.mediumAndUp} {
+    max-height: 120px;
+    overflow: hidden;
+  }
+`;
 
 const radioBoxGroupStyle = (count) => css`
   // Use grid to help keep all options the same size while allowing for stretching
   display: grid;
   grid-template-columns: repeat(${count > 3 ? 2 : 1}, minmax(0, 1fr));
-  gap: ${theme.size.default};
+  gap: ${OPTIONS_GAP};
   margin-bottom: ${theme.size.medium};
   // Force component to hit content's max width on pages that have minimal content
   max-width: 100vw;
+  padding: ${OPTIONS_GRID_PADDING};
+  padding-bottom: 0px;
 
   @media ${theme.screenSize.mediumAndUp} {
     grid-template-columns: repeat(auto-fit, minmax(0, 1fr));
@@ -48,7 +42,7 @@ const radioBoxGroupStyle = (count) => css`
   }
 `;
 
-const radioBoxStyle = (isSelected, count, isSidenavCollapsed) => css`
+const radioBoxStyle = css`
   height: 48px;
   margin: 0;
 
@@ -62,53 +56,56 @@ const radioBoxStyle = (isSelected, count, isSidenavCollapsed) => css`
   :not(:last-of-type) {
     margin: 0;
   }
-
-  @media ${theme.screenSize.mediumAndUp} {
-    ::after {
-      content: '';
-      height: 0;
-      width: 0;
-      border-left: 11px solid transparent;
-      border-right: 11px solid transparent;
-      border-bottom: 11px solid ${isSelected ? palette.green.dark1 : 'transparent'};
-      position: absolute;
-      left: 50%;
-      transform: translateX(-50%);
-      bottom: -${theme.size.medium};
-    }
-  }
-
-  ${!isSidenavCollapsed &&
-  `
-    ${whenWrappingDesktop(count)} {
-      ::after {
-        content: none;
-      }
-    }
-  `}
 `;
 
-const hrStyle = (count, isSidenavCollapsed) => css`
+const lineStyle = css`
   display: none;
 
   @media ${theme.screenSize.mediumAndUp} {
-    border: 1.5px solid ${palette.green.dark1};
-    margin: ${theme.size.medium} 0 ${theme.size.default};
     display: block;
   }
+`;
 
-  ${!isSidenavCollapsed &&
-  `
-    ${whenWrappingDesktop(count)} {
-      display: none;
-    }
-  `}
+/**
+ * Handles the CSS calculations for the triangle to point to the selected method. Assumes the calculation
+ * starts from the lefthand side, relative to an element that is the full width of the content column.
+ * @param {number} count
+ * @param {number} selectedIdx
+ * @returns {string}
+ */
+const trianglePosCalc = (count, selectedIdx) => {
+  const numGaps = `${count - 1} * ${OPTIONS_GAP}`;
+  const fullHorizontalPadding = `${OPTIONS_GRID_PADDING} * 2`;
+  const widthOfOneOption = `(100% - ${fullHorizontalPadding} - ${numGaps}) / ${count}`;
+  return `calc(${OPTIONS_GRID_PADDING} + (${widthOfOneOption} / 2) + ${selectedIdx} * (${OPTIONS_GAP} + ${widthOfOneOption}))`;
+};
+
+const triangleStyle = (count, selectedIdx = 0) => css`
+  position: relative;
+
+  ::before {
+    content: '';
+    height: 0;
+    width: 0;
+    border-left: 11px solid transparent;
+    border-right: 11px solid transparent;
+    border-bottom: 11px solid ${palette.green.dark1};
+    position: absolute;
+    left: ${trianglePosCalc(count, selectedIdx)};
+    bottom: 0px;
+    transform: translateX(-50%);
+  }
+`;
+
+const hrStyle = css`
+  border: 1.5px solid ${palette.green.dark1};
+  margin: 0;
 `;
 
 const MethodSelector = ({ nodeData: { children } }) => {
   const optionCount = children.length;
   const [selectedMethod, setSelectedMethod] = useState(children[0]?.options?.id);
-  const { isCollapsed: isSidenavCollapsed } = useContext(SidenavContext);
+  const [selectedIdx, setSelectedIdx] = useState(0);
 
   // Load method ID saved from last session, if applicable.
   useEffect(() => {
@@ -120,36 +117,40 @@ const MethodSelector = ({ nodeData: { children } }) => {
 
     if (savedMethodId && validOptions.includes(savedMethodId)) {
       setSelectedMethod(savedMethodId);
+      setSelectedIdx(validOptions.indexOf(savedMethodId));
     }
   }, [children]);
 
   return (
     <>
-      <RadioBoxGroup
-        className={cx(radioBoxGroupStyle(children.length))}
-        size={'full'}
-        onChange={({ target: { defaultValue } }) => {
-          setSelectedMethod(defaultValue);
-          setLocalValue(STORAGE_KEY, defaultValue);
-          reportAnalytics('MethodOptionSelected', {
-            methodOption: defaultValue,
-          });
-        }}
-      >
-        {children.map(({ options: { title, id } }) => {
-          return (
-            <RadioBox
-              key={id}
-              className={cx(radioBoxStyle(selectedMethod === id, optionCount, isSidenavCollapsed))}
-              value={id}
-              checked={selectedMethod === id}
-            >
-              {title}
-            </RadioBox>
-          );
-        })}
-      </RadioBoxGroup>
-      <hr className={cx(hrStyle(optionCount, isSidenavCollapsed))} />
+      <div className={optionsContainer}>
+        <RadioBoxGroup
+          className={cx(radioBoxGroupStyle(children.length))}
+          size={'full'}
+          onChange={({ target: { defaultValue } }) => {
+            const [id, idx] = defaultValue.split('-');
+            setSelectedMethod(id);
+            setSelectedIdx(idx);
+            setLocalValue(STORAGE_KEY, id);
+            reportAnalytics('MethodOptionSelected', {
+              methodOption: defaultValue,
+            });
+          }}
+        >
+          {children.map(({ options: { title, id } }, index) => {
+            return (
+              <RadioBox key={id} className={cx(radioBoxStyle)} value={`${id}-${index}`} checked={selectedMethod === id}>
+                {title}
+              </RadioBox>
+            );
+          })}
+        </RadioBoxGroup>
+        {/* Keep separate div for triangle to allow for relative positioning */}
+        <div className={cx(lineStyle)}>
+          <div className={triangleStyle(optionCount, selectedIdx)} />
+          <hr className={cx(hrStyle)} />
+        </div>
+      </div>
       {children.map((child, index) => {
         if (child.name !== 'method-option') return null;
         return <MethodOptionContent key={index} nodeData={child} selectedMethod={selectedMethod} />;
