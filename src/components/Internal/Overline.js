8d40dbb79ea7d81d386ad1a85dfdc799b88b77e1
rayangler <27821750+rayangler@users.noreply.github.com>
2024-09-05 20:57:39 -0400

    Remove unused Overline CSS

--- src/components/Internal/Overline.js
+++ src/components/Internal/Overline.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { css, cx } from '@leafygreen-ui/emotion';
-import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
 import { Overline as LGOverline } from '@leafygreen-ui/typography';
-import { palette } from '@leafygreen-ui/palette';
 
 const overlineBaseStyling = css`
   margin-top: 48px;
@@ -11,16 +9,8 @@ const overlineBaseStyling = css`
   color: var(--font-color-light);
 `;
 
-const overlineLightStyling = css`
-  color: ${palette.gray.base};
-  ${overlineBaseStyling}
-`;
-
 const Overline = ({ className, children }) => {
-  const { darkMode } = useDarkMode();
-  return (
-    <LGOverline className={cx(darkMode ? overlineBaseStyling : overlineLightStyling, className)}>{children}</LGOverline>
-  );
+  return <LGOverline className={cx(overlineBaseStyling, className)}>{children}</LGOverline>;
 };
 
 Overline.propTypes = {
