0332fcec70387a46966f96a80bc648d85a876e88
biancalaube <bianca.laube@gmail.com>
2024-09-19 13:30:52 -0400

    cleaned up build.sh file

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
+# variables that need to be changed based on the content repo you're working on -------------------------------------------
+TESTING_CONTENT_REPO=docs-landing # name of content repo
+ORGANIZATION=mongodb # name of org, usually mongodb or 10gen
+# -------------------------------------------------------------------------------------------------------------------------
 PARSER_VERSION=0.17.0
-TESTING_CONTENT_REPO=docs-landing
 
 # This make command curls the examples for certain repos.
 # If the rule doesn't exist, the error doesn't interrupt the build process.
 # make examples - we don't need this for docs-landing, but have it here for when we change repos
 
+# cloning the content repo
 echo "building docs-landing (temp, bianca's fork)"
-git clone https://github.com/mongodb/${TESTING_CONTENT_REPO}.git
+git clone https://github.com/${ORGANIZATION}/${TESTING_CONTENT_REPO}.git
 
 
 # running the parser 
@@ -20,17 +24,12 @@ fi
 echo "======================================================================================================================================================================="
 echo "========================================================================== Running parser... =========================================================================="
 ./snooty-parser/snooty/snooty build $(pwd)/${TESTING_CONTENT_REPO} --output=./bundle.zip
-#  ./snooty-parser/snooty/snooty build /Users/bianca.laube/Documents/repos/docs-content-repos/docs-landing --output=./bundle.zip
 echo "========================================================================== Parser complete ============================================================================"
 echo "======================================================================================================================================================================="
 
-# putting docs-landing as the path
+# putting set conent-repo as the path
 echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip
 export GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip
 
-# do i need to set anymore variables
-
 # run the site
 npm run build:no-prefix
-
-# make sure correct npm version ? 
