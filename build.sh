adeb0d5b92258d0d49c00921b0538f6016e3eefd
biancalaube <bianca.laube@gmail.com>
2024-09-19 11:50:17 -0400

    trying a variable

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 PARSER_VERSION=0.17.0
+TESTING_CONTENT_REPO=docs-landing
 
 # This make command curls the examples for certain repos.
 # If the rule doesn't exist, the error doesn't interrupt the build process.
 # make examples - we don't need this for docs-landing, but have it here for when we change repos
 
 echo "building docs-landing (temp, bianca's fork)"
-git clone https://github.com/biancalaube/docs-landing.git
+git clone https://github.com/biancalaube/${TESTING_CONTENT_REPO}.git
 
 
 # running the parser 
@@ -18,20 +19,16 @@ fi
 
 echo "======================================================================================================================================================================="
 echo "========================================================================== Running parser... =========================================================================="
-./snooty-parser/snooty/snooty build $(pwd)/docs-landing --output=./bundle.zip
+./snooty-parser/snooty/snooty build $(pwd)/${TESTING_CONTENT_REPO} --output=./bundle.zip
 #  ./snooty-parser/snooty/snooty build /Users/bianca.laube/Documents/repos/docs-content-repos/docs-landing --output=./bundle.zip
 echo "========================================================================== Parser complete ============================================================================"
 echo "======================================================================================================================================================================="
 
 # putting docs-landing as the path
 echo GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip
-# echo GATSBY_SITE=landing
-# echo GATSBY_PARSER_USER=biancalaube
-# echo GATSBY_PARSER_BRANCH=main
+
 export GATSBY_MANIFEST_PATH=$(pwd)/bundle.zip
-# export GATSBY_SITE=landing
-# export GATSBY_PARSER_USER=biancalaube
-# export GATSBY_PARSER_BRANCH=main
+
 # do i need to set anymore variables
 
 # run the site
