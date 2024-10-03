f25dc0d29c36efc1b45cba24ba8052e2ceaf3f6d
rayangler <27821750+rayangler@users.noreply.github.com>
2024-10-03 10:32:57 -0400

    DOP-5017: Remove Gatsby Cloud references and logic (#1265)

--- src/utils/is-gatsby-preview.js
+++
@@ -1,6 +0,0 @@
-/**
- * Returns `true` if the build is a preview build for Gatsby Cloud.
- */
-const isGatsbyPreview = () => process.env.GATSBY_IS_PREVIEW === 'true';
-
-module.exports = { isGatsbyPreview };
