cf8c524c327ed748e4f0e4f5a82200366904b62e
mmeigs <matt.meigs@mongodb.com>
2024-10-31 15:21:44 -0400

    do not use parsing logic for product dropdown urls in dotcomstg

--- plugins/utils/products.js
+++ plugins/utils/products.js
@@ -1,10 +1,12 @@
+const { siteMetadata } = require('../../gatsby-config');
 const { baseUrl } = require('../../src/utils/base-url');
 
 const createProductNodes = async ({ db, createNode, createNodeId, createContentDigest }) => {
   // Get all MongoDB products for the sidenav
   const products = await db.fetchAllProducts();
   products.forEach((product) => {
-    const url = baseUrl(product.baseUrl + product.slug);
+    let url = baseUrl(product.baseUrl + product.slug);
+    if (siteMetadata.snootyEnv === 'dotcomstg') url = product.baseUrl + product.slug;
 
     createNode({
       children: [],
