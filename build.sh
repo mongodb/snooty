776d249c4190387c3bee3d9544f1eefc452ed978
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2024-11-25 15:09:52 -0500

    echoing

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ PARSER_VERSION=$4 # version of the parser to download
 # make examples - we don't need this for docs-landing, but have it here for when we change repos
 
 # cloning the content repo
-echo "Cloning content repo: ${TESTING_REPO_NAME}, ${testing_key}"
-echo " this is the ${testing_key}"
+echo "Cloning content repo: ${TESTING_REPO_NAME}, ${TESTING_KEY}"
+echo " this is the fifth arg $5"
 git clone -b ${TESTING_BRANCH_NAME} https://github.com/${TESTING_ORGANIZATION}/${TESTING_REPO_NAME}.git 
 
 
