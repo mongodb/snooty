37afd9c7f3f769083904be68a66e540d87e01d27
rayangler <27821750+rayangler@users.noreply.github.com>
2024-12-03 17:17:43 -0500

    Banner by text

--- src/components/Banner/SiteBanner/index.js
+++ src/components/Banner/SiteBanner/index.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import styled from '@emotion/styled';
+import { palette } from '@leafygreen-ui/palette';
+import { css } from '@leafygreen-ui/emotion';
 import { HeaderContext } from '../../Header/header-context';
 import { SNOOTY_REALM_APP_ID } from '../../../build-constants';
 import { useSiteMetadata } from '../../../hooks/use-site-metadata';
@@ -7,6 +9,7 @@ import { theme } from '../../../theme/docsTheme';
 import { isBrowser } from '../../../utils/is-browser';
 import { normalizePath } from '../../../utils/normalize-path';
 import { fetchBanner } from '../../../utils/realm';
+import BrandingShape from './BrandingShape';
 
 const getBannerSource = (src) => {
   if (src == null || src === '') return null;
@@ -20,6 +23,8 @@ const StyledBannerContainer = styled.a`
   width: 100%;
   position: absolute;
   z-index: ${theme.zIndexes.header};
+  color: white;
+  text-decoration: none;
 `;
 
 const StyledBannerContent = styled.div(
@@ -27,7 +32,12 @@ const StyledBannerContent = styled.div(
     background-image: url(${getBannerSource(props.imgPath)});
     background-position: center;
     background-size: cover;
+    background-color: ${props.bgColor};
     height: 100%;
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    padding: 0 11px;
 
     @media ${theme.screenSize.upToMedium} {
       background-image: url(${getBannerSource(props.tabletImgPath)});
@@ -39,6 +49,44 @@ const StyledBannerContent = styled.div(
   `
 );
 
+const bannerTextStyle = css`
+  align-self: center;
+`;
+
+const pillContainer = css`
+  display: grid;
+  justify-items: center;
+  align-items: center;
+`;
+
+// Forces components to be in the same cell to create an overlap
+const gridCell = css`
+  grid-row: 1;
+  grid-column: 1;
+`;
+
+const brandingContainer = css`
+  ${gridCell}
+  height: 40px;
+`;
+
+const pillStyle = css`
+  ${gridCell}
+  color: ${palette.green.dark3};
+  font-weight: 600;
+  line-height: 16px;
+  font-size: 12px;
+  background-color: ${palette.green.base};
+  border: 1px solid ${palette.green.dark2};
+  border-radius: 6px;
+  height: 22px;
+  padding: 3px 8px;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const SiteBanner = () => {
   const { bannerContent, setBannerContent } = useContext(HeaderContext);
   const { snootyEnv } = useSiteMetadata();
@@ -47,6 +95,15 @@ const SiteBanner = () => {
     const fetchBannerContent = async () => {
       try {
         setBannerContent(await fetchBanner(snootyEnv));
+        // TODO-5132: Remove and test with App Services when done
+        const bannerContentMock = {
+          text: 'Join us at AWS re:Invent 2024! Learn how to use MongoDB for AI use cases.',
+          altText: 'Hello World!',
+          pillText: 'LEARN MORE',
+          bgColor: palette.green.dark3,
+          url: 'https://www.mongodb.com/docs/',
+        };
+        setBannerContent(bannerContentMock);
       } catch (err) {
         console.error(err);
       }
@@ -66,7 +123,20 @@ const SiteBanner = () => {
         imgPath={bannerContent.imgPath}
         tabletImgPath={bannerContent.tabletImgPath ?? bannerContent.mobileImgPath}
         mobileImgPath={bannerContent.mobileImgPath}
-      />
+        bgColor={bannerContent.bgColor}
+      >
+        {bannerContent.text && (
+          <>
+            <span className={bannerTextStyle}>{bannerContent.text}</span>
+            <div className={pillContainer}>
+              <div className={brandingContainer}>
+                <BrandingShape size={'desktop'} />
+              </div>
+              {bannerContent.pillText && <span className={pillStyle}>{bannerContent.pillText}</span>}
+            </div>
+          </>
+        )}
+      </StyledBannerContent>
     </StyledBannerContainer>
   );
 };
