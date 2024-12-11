bc3d67b4099ccbb3063977e66de7f93e26554558
Seung Park <seung.park@mongodb.com>
2024-12-11 15:58:48 -0500

    make tabs selectors interactive on offline builds

---
+++ src/utils/head-scripts/offline-ui/index.js
@@ -0,0 +1,18 @@
+import bindTabUI from './tabs';
+import updateSidenavHeight from './sidenav';
+import bindTabsSelectorsUI from './tabs-selectors';
+// TODO: add script for tab selectors
+const OFFLINE_UI_CLASSNAME = 'snooty-offline-ui';
+
+const getScript = ({ key, fn }) => (
+  <script
+    className={OFFLINE_UI_CLASSNAME}
+    key={key}
+    type="text/javascript"
+    dangerouslySetInnerHTML={{ __html: `!${fn}()` }}
+  />
+);
+
+export const OFFLINE_HEAD_SCRIPTS = [bindTabUI, updateSidenavHeight, bindTabsSelectorsUI].map((fn, idx) =>
+  getScript({ key: `offline-docs-ui-${idx}`, fn })
+);
