6144287b0dd396cd9e21995aaf50e526cc281df8
mmeigs <matt.meigs@mongodb.com>
2025-03-03 09:27:43 -0500

    DOP:5084 - See Also indentation rather than LG Callout (#1356)

--- src/utils/get-plaintext.ts
+++ src/utils/get-plaintext.ts
@@ -3,11 +3,14 @@
  * Returns plaintext string indicating the nested title.
  */
 
-const getPlaintext = (nodeArray) => {
-  const extractText = (title, node) => {
-    if (node.type === 'text') {
+import { Node } from '../types/ast';
+import { isParentNode, isTextNode } from '../types/ast-utils';
+
+export const getPlaintext = (nodeArray: Node[]) => {
+  const extractText = (title: string, node: Node): string => {
+    if (isTextNode(node)) {
       return title + node.value;
-    } else if (node.children) {
+    } else if (isParentNode(node)) {
       return title + node.children.reduce(extractText, '');
     }
     return title;
@@ -15,5 +18,3 @@ const getPlaintext = (nodeArray) => {
 
   return nodeArray && nodeArray.length > 0 ? nodeArray.reduce(extractText, '') : '';
 };
-
-module.exports = { getPlaintext };
