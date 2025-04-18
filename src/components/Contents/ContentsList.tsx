e6ddd3ccc01b64d6d4ee28286b27b046d45326d9
mmeigs <matt.meigs@mongodb.com>
2025-04-18 11:10:00 -0400

    ContentsList styles

--- src/components/Contents/ContentsList.tsx
+++ src/components/Contents/ContentsList.tsx
@@ -5,8 +5,8 @@ import { palette } from '@leafygreen-ui/palette';
 import Box from '@leafygreen-ui/box';
 import Icon from '@leafygreen-ui/icon';
 
-import { Label } from '../Select';
 import useScreenSize from '../../hooks/useScreenSize';
+import { theme } from '../../theme/docsTheme';
 
 const List = styled('ul')`
   list-style-type: none;
@@ -15,6 +15,17 @@ const List = styled('ul')`
 
 const mobileLabelStyles = css`
   font-weight: 400;
+  font-size: 13px;
+  margin: 0;
+`;
+
+const desktopLabelStyles = css`
+  font-size: ${theme.fontSize.small};
+  line-height: ${theme.fontSize.default};
+  font-weight: bolder;
+  letter-spacing: 0;
+  /* TODO: Remove !important when mongodb-docs.css is removed */
+  margin: 0 0 12px !important;
 `;
 
 const mobileListStyles = css`
@@ -29,8 +40,7 @@ const mobileListStyles = css`
   }
 `;
 
-const StyledLabel = styled(Label)`
-  font-weight: 500;
+const StyledLabel = styled('p')`
   --label-color: ${palette.gray.dark2};
   .dark-theme & {
     --label-color: ${palette.gray.light1};
@@ -40,8 +50,8 @@ const StyledLabel = styled(Label)`
 const collapsibleStyles = css`
   border-top: 1px solid ${palette.gray.light2};
   border-bottom: 1px solid ${palette.gray.light2};
-  margin-top: 32px;
   padding: 5px 0;
+  margin-bottom: 24px;
 `;
 
 const headerStyles = css`
@@ -79,7 +89,12 @@ const ContentsList = ({ children, label }: { children: ReactNode; label: string;
         className={cx('collapsible', collapsibleStyles)}
       >
         <Box className={cx(headerStyles)} onClick={() => setOpen(!open)}>
-          <Icon className={cx(iconStyles)} glyph={open ? 'CaretDown' : 'CaretRight'} />
+          <Icon
+            className={cx(iconStyles)}
+            glyph={open ? 'CaretDown' : 'CaretRight'}
+            color={palette.gray.dark1}
+            fill={palette.gray.dark1}
+          />
           <StyledLabel className={cx(mobileLabelStyles)}>{label}</StyledLabel>
         </Box>
         <Box className={cx(listContainerStyles)}>
@@ -91,7 +106,7 @@ const ContentsList = ({ children, label }: { children: ReactNode; label: string;
   
   return (
     <>
-      <StyledLabel>{label}</StyledLabel>
+      <StyledLabel className={cx(desktopLabelStyles)}>{label}</StyledLabel>
       <List>{children}</List>
     </>
   );
