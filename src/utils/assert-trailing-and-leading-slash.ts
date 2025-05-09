6a54688f6bcada38df96ae63e4a3cba2ae5045ce
biancalaube <bianca.laube@gmail.com>
2025-05-09 15:09:40 -0400

    pathing

--- src/utils/assert-trailing-and-leading-slash.ts
+++ src/utils/assert-trailing-and-leading-slash.ts
@@ -1,8 +1,8 @@
 import { assertLeadingSlash } from './assert-leading-slash';
 import { assertTrailingSlash } from './assert-trailing-slash';
 
-const assertLeadingAndTrailingSlash = (url: string) => {
+export const assertLeadingAndTrailingSlash = (url: string) => {
   return assertLeadingSlash(assertTrailingSlash(url));
 };
 
-export default assertLeadingAndTrailingSlash;
+// export default assertLeadingAndTrailingSlash;
--- toc_data/toc.ts
+++ toc_data/toc.ts
@@ -1,6 +1,6 @@
 import type { TocItem } from '../src/components/UnifiedSidenav/UnifiedConstants';
 
-const COMMANDLINE_TOOLS = 'docs-commandline-tools';
+const COMMANDLINE_TOOLS = 'database-tools';
 
 // each url has to be unique with the prefix!!!!
 export const tocData = (): TocItem[] => {
@@ -21,7 +21,7 @@ export const tocData = (): TocItem[] => {
               items: [
                 {
                   label: 'Compatibility & Installation',
-                  url: '/docs/database-tools/mongodump/',
+                  url: '/docs/database-tools/mongorestore/mongorestore-compatibility-and-installation/',
                 },
                 {
                   label: 'Behavior',
