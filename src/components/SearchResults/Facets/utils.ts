b99e5ddb7281d75ad3071a50948b63b5bfb8efaf
mmeigs <matt.meigs@mongodb.com>
2025-06-05 15:07:13 -0400

    Migrate SearchFilters and Facets/utils, renamed FacetGroup, FacetValue, Facets, and FacetVersionGroup

--- src/components/SearchResults/Facets/utils.ts
+++ src/components/SearchResults/Facets/utils.ts
@@ -2,10 +2,8 @@
  *
  * @param {obj} facet - contains key and id properties as returned by /v2/status ie.
  * https://docs-search-transport.mongodb.com/v2/status
- *
- * @returns {str} blue | green
  */
-export const getFacetTagVariant = (facet) => {
+export const getFacetTagVariant = (facet: { key: string; id: string }) => {
   if (facet.key === 'target_product') {
     return 'green';
   }
