88f0fffabd27783d8fd308b07bc848cdabd209e5
ASteinheiser <me@iamandrew.io>
2025-06-17 11:58:17 -0700

    update type name

--- src/components/MultiPageTutorials/constants.ts
+++ src/components/MultiPageTutorials/constants.ts
@@ -1,6 +1,6 @@
-import { type PageOptionsKeys } from '../../context/page-context';
+import { type PageOptionsKey } from '../../context/page-context';
 
-export const PAGE_OPTION_NAME: PageOptionsKeys = 'multi_page_tutorial_settings';
-export const OPTION_KEY_SHOW_NEXT_TOP: PageOptionsKeys = 'show_next_top';
-export const OPTION_KEY_TIME_REQUIRED: PageOptionsKeys = 'time_required';
+export const PAGE_OPTION_NAME: PageOptionsKey = 'multi_page_tutorial_settings';
+export const OPTION_KEY_SHOW_NEXT_TOP: PageOptionsKey = 'show_next_top';
+export const OPTION_KEY_TIME_REQUIRED: PageOptionsKey = 'time_required';
 export const LINK_TITLE = 'Next Step';
