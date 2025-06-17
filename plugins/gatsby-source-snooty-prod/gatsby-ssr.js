7dec112c5c682a9e2fef10981ad82757343ea01b
Zach Winters <zach.winters@mongodb.com>
2025-06-17 17:27:01 -0400

    DOP-5610 add track js

--- plugins/gatsby-source-snooty-prod/gatsby-ssr.js
+++ plugins/gatsby-source-snooty-prod/gatsby-ssr.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { TrackJS } from 'trackjs';
 import { GlobalProviders } from './src/global-context-providers';
+
+console.log('INSTALLING Testing TrackJS!');
+
+// TrackJS is a tool that allows us to track errors and performance issues in our code.
+TrackJS.install({
+  token: 'c3fccd861d9b4238bfe1af83ebdec219',
+  // for more configuration options, see https://docs.trackjs.com
+});
 
 export function wrapRootElement({ element }) {
   return <GlobalProviders element={element} />;
