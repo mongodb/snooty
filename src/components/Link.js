027f74e9779ef0913c5dc37314ebdf95a255c2a0
biancalaube <bianca.laube@gmail.com>
2025-06-27 11:09:43 -0400

    clean up

--- src/components/Link.js
+++ src/components/Link.js
@@ -135,7 +135,6 @@ const Link = ({
   // If contentSite, that means we are coming from the UnifiedSideNav and not the old SideNav
   if (contentSite) {
     // For an external links, inside the unified toc
-    console.log('before relative to', to);
     if (!isRelativeUrl(to)) {
       return (
         <LGLink
@@ -152,8 +151,6 @@ const Link = ({
       );
     }
 
-    console.log('after relative to', to);
-
     if (!to.startsWith('/')) to = `/${to}`;
 
     // Ensure trailing slash
@@ -163,7 +160,6 @@ const Link = ({
     // allows our custom 404 page to render.
     if (project === contentSite || isUnifiedTOCInDevMode === true) {
       // Get rid of the path contentSite in link for internal links
-      console.log('inside gatsby link', project, contentSite, isUnifiedTOCInDevMode);
       const editedTo = assertLeadingAndTrailingSlash(to.replace(pathPrefix, ''));
 
       return (
@@ -179,8 +175,6 @@ const Link = ({
         </GatsbyLink>
       );
     }
-
-    console.log('to', to, hideExternalIconProp, l1List);
 
     // On the Unified SideNav but linking to a different content site
     return (
