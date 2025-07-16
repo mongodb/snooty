4f567bbf2928d4e5b2e78b1b38643639c6769c01
biancalaube <bianca.laube@gmail.com>
2025-07-16 17:09:41 -0400

    merging with DOP-5877

--- src/components/UnifiedSidenav/UnifiedConstants.ts
+++ src/components/UnifiedSidenav/UnifiedConstants.ts
@@ -1,24 +1,3 @@
-export interface BreadCrumb {
-  path: string;
-  title: string;
-}
-
-export interface TocItem {
-  label: string;
-  url?: string;
-  group?: boolean;
-  contentSite: string;
-  collapsible?: boolean;
-  breadcrumbs?: BreadCrumb[];
-  showSubNav?: boolean;
-  versionDropdown?: boolean;
-  items?: TocItem[];
-}
-
-export interface ActiveVersions {
-  [project: string]: string;
-}
-
 // DOP-5379: Move this somewhere that makes more sense when toc.ts is more finalized
 export enum DocSites {
   CLOUD_DOCS = 'cloud-docs',
@@ -26,5 +5,4 @@ export enum DocSites {
   CSHARP = 'csharp',
   CHARTS = 'charts',
   C2C = 'cluster-sync',
-  ATLAS_CLI = 'atlas-cli',
 }
