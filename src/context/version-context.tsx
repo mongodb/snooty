0b1ab719b04aa7ceba6a2d36092ca95557c5362a
biancalaube <bianca.laube@gmail.com>
2025-08-01 09:37:34 -0400

    parser branch version  issues

--- src/context/version-context.tsx
+++ src/context/version-context.tsx
@@ -53,8 +53,8 @@ const findBranchByGit = (gitBranchName: string, branches?: BranchData[]) => {
   if (!branches || !branches.length) {
     return;
   }
-
-  return branches.find((b) => b.gitBranchName === gitBranchName);
+  const useUrl = branches.find((b) => b.urlSlug === gitBranchName);
+  return useUrl ? useUrl : branches.find((b) => b.gitBranchName === gitBranchName);
 };
 
 // version state reducer helper fn
