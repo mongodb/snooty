3d38984ef64dc26b87c41123064db47fea656ea0
Caesar Bell <caesar.jd.bell@gmail.com>
2025-08-06 17:09:17 -0400

    DOP-6049 adds the column component

---
+++ src/components/MultiColumn/Column.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { css, cx } from '@leafygreen-ui/emotion';
+import { H3 } from '@leafygreen-ui/typography';
+import { ColumnNode } from '../../types/ast';
+import ComponentFactory from '../ComponentFactory';
+
+const Column = ({ nodeData: { children, options } }: { nodeData: ColumnNode }) => {
+  return (
+    <div
+      className={cx(css`
+        flex: 1;
+
+        ul {
+          list-style: none;
+          padding-left: 24px;
+          margin: 0;
+          position: relative;
+
+          li {
+            &::before {
+              content: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 10 10" fill="none"><circle cx="5" cy="5" r="4.25" stroke="%2300ED64" stroke-width="1.5"/></svg>');
+              position: absolute;
+              left: 8px;
+              transform: translateY(20%);
+            }
+          }
+        }
+      `)}
+    >
+      {options?.title && (
+        <H3
+          className={cx(css`
+            color: #fff;
+            margin-bottom: 27px;
+          `)}
+        >
+          {options.title}
+        </H3>
+      )}
+      {children.map((child, i) => (
+        <ComponentFactory nodeData={child} key={i} />
+      ))}
+    </div>
+  );
+};
+
+export default Column;
