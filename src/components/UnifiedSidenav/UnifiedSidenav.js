aabb2a499d7d7b754e24274bd55fdd95d0c38959
biancalaube <bianca.laube@gmail.com>
2025-08-25 14:33:33 -0400

    making variable work

--- src/components/UnifiedSidenav/UnifiedSidenav.js
+++ src/components/UnifiedSidenav/UnifiedSidenav.js
@@ -224,33 +224,35 @@ export function UnifiedSidenav({ slug }) {
   // Hide the Sidenav with css while keeping state as open/not collapsed.
   // This prevents LG's SideNav component from being seen in its collapsed state on mobile
   // return isValidVersion ? (
-  <div
-    className={cx(SidenavContainer({ ...topValues }))}
-    style={{ '--scroll-y': `${viewport.scrollY}px` }}
-    id={SIDE_NAV_CONTAINER_ID}
-  >
-    <AccordionNavPanel
-      showDriverBackBtn={showDriverBackBtn}
-      setShowDriverBackBtn={setShowDriverBackBtn}
-      slug={slug}
-      currentL2s={currentL2s}
-      setCurrentL1={setCurrentL1}
-      setCurrentL2s={setCurrentL2s}
-      hideMobile={hideMobile}
-      currentL1={currentL1}
-      tree={tree}
-    />
-    <DoublePannedNav
-      showDriverBackBtn={showDriverBackBtn}
-      setShowDriverBackBtn={setShowDriverBackBtn}
-      tree={tree}
-      slug={slug}
-      currentL2s={currentL2s}
-      setCurrentL1={setCurrentL1}
-      setCurrentL2s={setCurrentL2s}
-      currentL1={currentL1}
-    />
-  </div>;
+  return (
+    <div
+      className={cx(SidenavContainer({ ...topValues }))}
+      style={{ '--scroll-y': `${viewport.scrollY}px` }}
+      id={SIDE_NAV_CONTAINER_ID}
+    >
+      <AccordionNavPanel
+        showDriverBackBtn={showDriverBackBtn}
+        setShowDriverBackBtn={setShowDriverBackBtn}
+        slug={slug}
+        currentL2s={currentL2s}
+        setCurrentL1={setCurrentL1}
+        setCurrentL2s={setCurrentL2s}
+        hideMobile={hideMobile}
+        currentL1={currentL1}
+        tree={tree}
+      />
+      <DoublePannedNav
+        showDriverBackBtn={showDriverBackBtn}
+        setShowDriverBackBtn={setShowDriverBackBtn}
+        tree={tree}
+        slug={slug}
+        currentL2s={currentL2s}
+        setCurrentL1={setCurrentL1}
+        setCurrentL2s={setCurrentL2s}
+        currentL1={currentL1}
+      />
+    </div>
+  );
   // ) : (
   //   <div />
   // );
