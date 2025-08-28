09cd89c13cd63a49678d3846b69debf0dc3695e2
biancalaube <bianca.laube@gmail.com>
2025-08-28 10:48:59 -0400

    Remove is current page log

--- src/utils/is-current-page.js
+++ src/utils/is-current-page.js
@@ -1,5 +1,4 @@
 export const isCurrentPage = (currentUrl, slug) => {
-  console.log('is current page', currentUrl, slug);
   const trimSlashes = (str) => str.replace(/^\/|\/$/g, '');
   if (!currentUrl || !slug) return false;
   return trimSlashes(currentUrl) === trimSlashes(slug);
