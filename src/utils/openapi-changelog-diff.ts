062e09ef5e582621f21c6f74d1c8056880dc3221
mmeigs <matt.meigs@mongodb.com>
2025-09-08 10:53:36 -0400

    Use Nextjs API for open api changelog diff fetch

---
+++ src/utils/openapi-changelog-diff.ts
@@ -0,0 +1,12 @@
+import { OpenAPIChangelogDiffSection, SnootyEnv } from '../types/data';
+
+export const fetchOpenAPIChangelogDiff = async (
+  diffString: string,
+  snootyEnv: SnootyEnv
+): Promise<OpenAPIChangelogDiffSection[]> => {
+  const isStaging = snootyEnv === 'staging' || snootyEnv === 'development';
+  const res = await fetch(
+    `${process.env.GATSBY_NEXT_API_BASE_URL}/openapi/diff?diff=${diffString}${isStaging ? `&staging=true` : ''}`
+  );
+  return res.json();
+};
