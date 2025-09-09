e0ff25ac028e132d494ed0e9a19dbe3ea6e92a6d
Bianca <76073842+biancalaube@users.noreply.github.com>
2025-09-09 15:32:55 -0400

    DOP-6058:ToC should scroll down to the selected node (#1524)
    Co-authored-by: branberry <brandon.ly@mongodb.com>
    Co-authored-by: Brandon Ly <brandonly@lostcoding.com>
    Co-authored-by: Zach Winters <zachwinters1@gmail.com>
    Co-authored-by: Caesar Bell <caesar.jd.bell@gmail.com>
    Co-authored-by: Seung Park <seungpark@users.noreply.github.com>

--- src/components/UnifiedSidenav/UnifiedSidenav.js
+++ src/components/UnifiedSidenav/UnifiedSidenav.js
@@ -16,6 +16,7 @@ import { assertLeadingSlash } from '../../utils/assert-leading-slash';
 import { removeTrailingSlash } from '../../utils/remove-trailing-slash';
 import { removeLeadingSlash } from '../../utils/remove-leading-slash';
 import { isBrowser } from '../../utils/is-browser';
+import { loadHashIntoView } from '../../utils/load-hash-into-view';
 import { isActiveTocNode, removeAnchor } from './UnifiedTocNavItems';
 import { DoublePannedNav } from './DoublePannedNav';
 import { AccordionNavPanel } from './AccordionNav';
@@ -214,10 +215,7 @@ export function UnifiedSidenav({ slug }) {
   useEffect(() => {
     if (!isBrowser) return;
     if (hash) {
-      const el = document.querySelector(CSS.escape(removeTrailingSlash(hash)));
-      if (el) {
-        el.scrollIntoView({ behavior: 'smooth' });
-      }
+      loadHashIntoView(hash);
     }
   }, [hash]);
 
