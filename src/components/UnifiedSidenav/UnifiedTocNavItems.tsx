1e65935d1ffad6ea21cef65e2765a6e58bc18188
Bianca <76073842+biancalaube@users.noreply.github.com>
2025-10-02 12:48:00 -0400

    DOP-6267: Fix duplication error (#1558)

--- src/components/UnifiedSidenav/UnifiedTocNavItems.tsx
+++ src/components/UnifiedSidenav/UnifiedTocNavItems.tsx
@@ -10,6 +10,7 @@ import { isCurrentPage } from '../../utils/is-current-page';
 import { theme } from '../../theme/docsTheme';
 import { isUnifiedTOCInDevMode } from '../../utils/is-unified-toc-dev';
 import { VersionContext } from '../../context/version-context';
+import { tocItemKey } from '../../utils/create-toc-key';
 import { l1ItemStyling, groupHeaderStyling, l2ItemStyling } from './styles/SideNavItem';
 import { UnifiedVersionDropdown } from './UnifiedVersionDropdown';
 import { TocItem } from './types';
@@ -113,7 +114,7 @@ export const UnifiedTocNavItem = ({
               <UnifiedTocNavItem
                 {...tocItem}
                 level={level}
-                key={tocItem.newUrl + tocItem.label}
+                key={tocItemKey(tocItem)}
                 slug={slug}
                 isStatic={false}
                 isAccordion={isAccordion}
@@ -134,7 +135,7 @@ export const UnifiedTocNavItem = ({
           <UnifiedTocNavItem
             {...tocItem}
             level={level}
-            key={tocItem.newUrl + tocItem.label}
+            key={tocItemKey(tocItem)}
             slug={slug}
             isStatic={false}
             isAccordion={isAccordion}
@@ -157,7 +158,7 @@ export const UnifiedTocNavItem = ({
             <UnifiedTocNavItem
               {...tocItem}
               level={level}
-              key={tocItem.newUrl + tocItem.label}
+              key={tocItemKey(tocItem)}
               slug={slug}
               isAccordion={isAccordion}
               setCurrentL2s={setCurrentL2s}
@@ -308,7 +309,7 @@ export const CollapsibleNavItem = ({
           <UnifiedTocNavItem
             {...item}
             level={level + 1}
-            key={item.newUrl + item.label}
+            key={tocItemKey(item)}
             slug={slug}
             setShowDriverBackBtn={setShowDriverBackBtn}
             setCurrentL2s={setCurrentL2s}
