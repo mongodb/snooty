6799673e1423c2d49bc3f4b4979f06384f38ea69
Bianca <76073842+biancalaube@users.noreply.github.com>
2026-02-13 11:33:06 -0500

    DOP-6429: Fixing dissmissable skills card bug (#1601)

--- src/components/DismissibleSkillsCard.tsx
+++ src/components/DismissibleSkillsCard.tsx
@@ -91,9 +91,9 @@ const DismissibleSkillsCard = ({ skill, url, slug }: { skill: string; url: strin
     reportDismissibleSkillsCard(skill, url, event.currentTarget as HTMLElement);
   };
 
-  const onClose = (event: React.MouseEvent) => {
+  const onClose = (event?: React.MouseEvent) => {
     if (isBrowser) {
-      reportDismissibleSkillsCard(skill, url, event.currentTarget as HTMLElement);
+      reportDismissibleSkillsCard(skill, url, event?.currentTarget as HTMLElement);
       // Add to document classnames
       const docClassList = window.document.documentElement.classList;
       docClassList.add(shownClassname);
@@ -126,7 +126,7 @@ const DismissibleSkillsCard = ({ skill, url, slug }: { skill: string; url: strin
           <SkillsBadgeIcon />
           <Subtitle className={titleStyles}>Earn a Skill Badge</Subtitle>
         </Box>
-        <CloseButton onClick={() => onClose} />
+        <CloseButton onClick={onClose} />
         <Body>Master "{skill}" for free!</Body>
         <Link arrowAppearance={'persist'} baseFontSize={13} href={url} onClick={onLinkClick} hideExternalIcon>
           Learn more
