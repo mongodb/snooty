635f3c641baf0f1914c3be62212047b9214b5155
mmeigs <matt.meigs@mongodb.com>
2026-04-02 12:19:32 -0400

    clean

--- src/utils/get-page-url.ts
+++ src/utils/get-page-url.ts
@@ -2,8 +2,6 @@ import { isBrowser } from './is-browser';
 import { stripLocale } from './locale';
 
 export const getPageUrl = () => {
-  console.log('window.location.pathname', window.location.pathname);
-  console.log('isBrowser', isBrowser);
   if (isBrowser) return stripLocale(window.location.pathname);
   else return '';
 };
